fd7d8c2023fed1b6ce72be12ab6d3e916fbb22c2
Bert Kleewein <bertk@microsoft.com>
2016-12-07 10:48:47 -0800

    fix signing issues

--- shared/NuGet/make_nuget_package.ps1
+++ shared/NuGet/make_nuget_package.ps1
@@ -21,15 +21,15 @@ ls Microsoft.Azure.Devices.Shared.*.nupkg | % { del $_ }
 
 # Get the assembly versions from both files, make sure they match, and use that as the package version
 $dotNetFile = "..\Microsoft.Azure.Devices.Shared\Properties\AssemblyInfo.cs"
-$winRTNetFile = "..\Microsoft.Azure.Devices.Shared.WinRT\Properties\AssemblyInfo.cs"
+$dotNetUWPFile = "..\Microsoft.Azure.Devices.Shared.UWP\Properties\AssemblyInfo.cs"
 $dotNetPCLFile = "..\Microsoft.Azure.Devices.Shared.PCL\Properties\AssemblyInfo.cs"
 
 $v1 = GetAssemblyVersionFromFile($dotNetFile)
-$v2 = GetAssemblyVersionFromFile($winRTNetFile)
+$v2 = GetAssemblyVersionFromFile($dotNetUWPFile)
 $v3 = GetAssemblyVersionFromFile($dotNetPCLFile)
 
 if($v1 -ne $v2) {
-    Write-Host "Error: Mismatching assembly versions in files $dotNetFile and $winRTNetFile. Check AssemblyInformationalVersion attribute in each file." -foregroundcolor "red"
+    Write-Host "Error: Mismatching assembly versions in files $dotNetFile and $dotNetUWPFile. Check AssemblyInformationalVersion attribute in each file." -foregroundcolor "red"
     return
 }
 
