da000d868b0898412478335b1a8a20f5fe4e526d
jasminel <jasminel@microsoft.com>
2016-12-12 12:00:22 -0800

    fix typo

--- device/samples/DeviceClientMethodAmqpSample/Properties/AssemblyInfo.cs
+++ device/samples/DeviceClientMethodAmqpSample/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
 [assembly: AssemblyProduct("DeviceClientMethodAmqpSample")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
