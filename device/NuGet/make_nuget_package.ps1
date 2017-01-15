316e0a7e6a60404dfd4808b7f3e396fe56745bac
yfakariya <yfakariya@gmail.com>
2017-01-16 08:13:22 +0900

    Add .NET Standard build for script and nuspec.
    .NET Standard builds use latest packages including non-signed version of DotNetty.

--- device/NuGet/make_nuget_package.ps1
+++ device/NuGet/make_nuget_package.ps1
@@ -25,10 +25,12 @@ ls Microsoft.Azure.Devices.Client.*.nupkg | % { del $_ }
 $dotNetFile = "..\Microsoft.Azure.Devices.Client\Properties\AssemblyInfo.cs"
 $uwpNetFile = "..\Microsoft.Azure.Devices.Client.UWP\Properties\AssemblyInfo.cs"
 $dotNetPCLFile = "..\Microsoft.Azure.Devices.Client.PCL\Properties\AssemblyInfo.cs"
+$dotNetStandardFile = "..\Microsoft.Azure.Devices.Client.NetStandard\Properties\AssemblyInfo.cs"
 
 $v1 = GetAssemblyVersionFromFile($dotNetFile)
 $v2 = GetAssemblyVersionFromFile($uwpNetFile)
 $v3 = GetAssemblyVersionFromFile($dotNetPCLFile)
+$v4 = GetAssemblyVersionFromFile($dotNetStandardFile)
 
 if($v1 -ne $v2) {
     Write-Host "Error: Mismatching assembly versions in files $dotNetFile and $uwpNetFile. Check AssemblyInformationalVersion attribute in each file." -foregroundcolor "red"
@@ -37,6 +39,11 @@ if($v1 -ne $v2) {
 
 if($v1 -ne $v3) {
     Write-Host "Error: Mismatching assembly versions in files $dotNetFile and $dotNetPCLFile. Check AssemblyInformationalVersion attribute in each file." -foregroundcolor "red"
+    return
+}
+
+if($v1 -ne $v4) {
+    Write-Host "Error: Mismatching assembly versions in files $dotNetFile and $dotNetStandardFile. Check AssemblyInformationalVersion attribute in each file." -foregroundcolor "red"
     return
 }
 
