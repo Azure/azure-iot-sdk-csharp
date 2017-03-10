197dc56190d9272cf89f7b150df94323f44ef868
jasminel <jasminel@microsoft.com>
2017-03-10 11:23:58 -0800

    convert Microsoft.Azure.Devices.Shared project to .net standard 1.3

---
+++ shared/Microsoft.Azure.Devices.Shared.Standard/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Azure.Devices.Shared.Standard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Microsoft.Azure.Devices.Shared.Standard")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if (RELEASE_DELAY_SIGN)
+[assembly: AssemblyDelaySignAttribute(true)]
+[assembly: AssemblyKeyFileAttribute("35MSSharedLib1024.snk")]
+#endif
+
+// Version information for an assembly follows semantic versioning 1.0.0 (because
+// NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
+[assembly: AssemblyInformationalVersion("1.0.0")]
