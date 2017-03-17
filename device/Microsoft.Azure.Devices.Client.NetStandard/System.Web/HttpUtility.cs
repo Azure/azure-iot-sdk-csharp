1c9b9d85c2533a9a20b2dedbc5b1402c8e527f4c
Tamer Awad <tameraw@microsoft.com>
2017-03-17 14:42:48 -0700

    Updated System.Web files with proper Licence header.

--- device/Microsoft.Azure.Devices.Client.NetStandard/System.Web/HttpUtility.cs
+++ device/Microsoft.Azure.Devices.Client.NetStandard/System.Web/HttpUtility.cs
@@ -1,6 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Specialized;
 using System.Globalization;
@@ -10,7 +9,7 @@ using System.Web.Util;
 
 namespace System.Web
 {
-    internal sealed class HttpUtility
+    public sealed class HttpUtility
     {
         private sealed class HttpQSCollection : NameValueCollection
         {
