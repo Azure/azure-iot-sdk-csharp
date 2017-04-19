5d1991d393924f40c26fd9c960a874ea53d50233
Ilias Tsigkogiannis <iliast@microsoft.com>
2017-04-19 03:45:12 -0700

    Updated values for temperature

--- device/samples/DeviceClientMqttSample/Program.cs
+++ device/samples/DeviceClientMqttSample/Program.cs
@@ -16,7 +16,7 @@ namespace Microsoft.Azure.Devices.Client.Samples
         //  "HostName=<iothub_host_name>;CredentialType=SharedAccessSignature;DeviceId=<device_id>;SharedAccessSignature=SharedAccessSignature sr=<iot_host>/devices/<device_id>&sig=<token>&se=<expiry_time>";
         private const string DeviceConnectionString = "<replace>";
         private static int MESSAGE_COUNT = 5;
-        private const int TEMPERATURE_THRESHOLD = 28;
+        private const int TEMPERATURE_THRESHOLD = 30;
         private static String deviceId = "MyCSharpDevice";
         private static float temperature;
         private static float humidity;
@@ -59,7 +59,7 @@ namespace Microsoft.Azure.Devices.Client.Samples
 
             for (int count = 0; count < MESSAGE_COUNT; count++)
             {
-                temperature = rnd.Next(20, 30);
+                temperature = rnd.Next(20, 35);
                 humidity = rnd.Next(60, 80);
                 dataBuffer = string.Format("{{\"deviceId\":\"{0}\",\"messageId\":{1},\"temperature\":{2},\"humidity\":{3}}}", deviceId, count, temperature, humidity);
                 Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
