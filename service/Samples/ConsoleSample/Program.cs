d98890b66b44fea4897a3493a9a35cc4785a8ec6
sergaz-msft <sergaz@microsoft.com>
2017-05-04 16:05:37 +0100

    Service\ConsoleSample: Add RemoveDevice function and change all to Async Tasks.

--- service/Samples/ConsoleSample/Program.cs
+++ service/Samples/ConsoleSample/Program.cs
@@ -9,26 +9,36 @@ namespace ConsoleSample
 {
     class Program
     {
-        const string connectionString = "...";
+        const string connectionString = "<replace_with_iothub_connection_string>";
+        const string deviceID = "new_device";
         static void Main(string[] args)
         {
-            //SendMessage();
-            AddDevice();
+            AddDevice().Wait();
+            //SendMessage().Wait();
+            //RemoveDevice().Wait();
         }
 
-        static void SendMessage()
+        static async Task SendMessage()
         {
             var serviceClient = ServiceClient.CreateFromConnectionString(connectionString, TransportType.Amqp);
             var str = "Hello, Cloud!";
             var message = new Message(Encoding.ASCII.GetBytes(str));
-            serviceClient.SendAsync("GarageDoorSensor", message).Wait();
-            Console.WriteLine("done");
+            await serviceClient.SendAsync(deviceID, message);
+            Console.WriteLine("C2D Message Sent");
         }
 
-        static void AddDevice()
+        static async Task AddDevice()
         {
             RegistryManager manager = RegistryManager.CreateFromConnectionString(connectionString);
-            manager.AddDeviceAsync(new Device("new_device"));
+            await manager.AddDeviceAsync(new Device(deviceID));
+            Console.WriteLine("Device Added");
+        }
+
+        static async Task RemoveDevice()
+        {
+            RegistryManager manager = RegistryManager.CreateFromConnectionString(connectionString);
+            await manager.RemoveDeviceAsync(deviceID);
+            Console.WriteLine("Device Removed");
         }
     }
 }
