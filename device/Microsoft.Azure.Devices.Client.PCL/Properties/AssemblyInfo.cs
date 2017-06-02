7fc2476a1a1bf26de4510b7a57c1c6a17d4bc90e
jasminel <jasminel@microsoft.com>
2017-06-02 06:57:22 -0700

    Bump package versions for release 2017-6-2

--- device/Microsoft.Azure.Devices.Client.PCL/Properties/AssemblyInfo.cs
+++ device/Microsoft.Azure.Devices.Client.PCL/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.CompilerServices;
 
 // Version information for an assembly follows semantic versioning 1.0.0 (because
 // NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
-[assembly: AssemblyInformationalVersion("1.2.11")]
+[assembly: AssemblyInformationalVersion("1.2.12")]
