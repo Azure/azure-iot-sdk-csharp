9e866d93d81389e43561a6de987af349d4c1b867
Ryan Johnson <ryjoh@microsoft.com>
2017-08-25 14:30:17 -0700

    Bump package versions for release 2017-8-25

--- device/Microsoft.Azure.Devices.Client/IotHubConnection.cs
+++ device/Microsoft.Azure.Devices.Client/IotHubConnection.cs
@@ -414,10 +414,10 @@ namespace Microsoft.Azure.Devices.Client
 #if WINDOWS_UWP
             // System.Reflection.Assembly.GetExecutingAssembly() does not exist for UWP, therefore use a hard-coded version name
             // (This string is picked up by the bump_version script, so don't change the line below)
-            var UWPAssemblyVersion = "1.4.1";
+            var UWPAssemblyVersion = "1.4.2";
             linkSettings.AddProperty(IotHubAmqpProperty.ClientVersion, UWPAssemblyVersion);
 #elif PCL
-            string PCLAssemblyVersion = "Microsoft.Azure.Devices.Client/1.4.1";
+            string PCLAssemblyVersion = "Microsoft.Azure.Devices.Client/1.4.2";
             linkSettings.AddProperty(IotHubAmqpProperty.ClientVersion, PCLAssemblyVersion);
 #else
             linkSettings.AddProperty(IotHubAmqpProperty.ClientVersion, Utils.GetClientVersion());
