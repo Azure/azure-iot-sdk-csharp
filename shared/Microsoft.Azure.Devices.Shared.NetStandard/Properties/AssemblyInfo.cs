63d6fae69c81009f3f2098e4f7693cd2991363b6
Cristian Pop <Cristian.Pop@microsoft.com>
2017-12-21 21:49:13 -0800

    Bump package versions for release 2017-12-21

--- shared/Microsoft.Azure.Devices.Shared.NetStandard/Properties/AssemblyInfo.cs
+++ shared/Microsoft.Azure.Devices.Shared.NetStandard/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Reflection;
 
 // Version information for an assembly follows semantic versioning 1.0.0 (because
 // NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
-[assembly: AssemblyInformationalVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
