aa4d6903f9ff82a4e997642abffdd8a23989b905
Dominic Betts <dobett@microsoft.com>
2018-04-27 11:29:11 +0100

    Add initial set of Quickstart code

---
+++ Quickstarts/back-end-application/BackEndApplication.cs
@@ -0,0 +1,41 @@
+﻿// This application uses the Azure IoT Hub service SDK for .NET
+// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/service
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace back_end_application
+{
+    class BackEndApplication
+    {
+        static ServiceClient serviceClient;
+        
+        // Connection string for your IoT Hub
+        // az iot hub show-connection-string --hub-name {your iot hub name}
+        static string connectionString = "{Your service connection string here}";
+
+        // Invoke the direct method on the device, passing the payload
+        private static async Task InvokeMethod()
+        {
+            var methodInvocation = new CloudToDeviceMethod("SetTelemetryInterval") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson("10");
+
+            // Invoke the direct method asynchronously and get the response from the simulated device.
+            var response = await serviceClient.InvokeDeviceMethodAsync("MyDotnetDevice", methodInvocation);
+
+            Console.WriteLine("Response status: {0}, payload:", response.Status);
+            Console.WriteLine(response.GetPayloadAsJson());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub Quickstarts #2 - Back-end application.\n");
+
+            // Create a ServiceClient to communicate with service-facing endpoint on your hub.
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            InvokeMethod().Wait();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
