a3ae9ec3975972f5f83d896b87836e8a306db11a
Dominic Betts <dobett@microsoft.com>
2018-04-27 11:36:47 +0100

    Initial commit of tutorial code

---
+++ Tutorials/Routing/SimulatedDevice/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string myDeviceId = "Contoso-Test-Device";
+        static string iotHubUri = "{your hub name}.azure-devices.net";
+        // This is the primary key for the device. This is in the portal. 
+        // Find your IoT hub in the portal > IoT devices > select your device > copy the key. 
+        static string deviceKey = "{your device key}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n"); 
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(myDeviceId, deviceKey), TransportType.Mqtt);
+            deviceClient.ProductInfo = "Contoso_Simulated-CSharp";
+            SendDeviceToCloudMessagesAsync();
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadLine();
+        }
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                string infoString;
+                string levelValue;
+
+                if (rand.NextDouble() > 0.7)
+                {
+                    if (rand.NextDouble() > 0.5)
+                    {
+                        levelValue = "critical";
+                        infoString = "This is a critical message.";
+                    }
+                    else
+                    {
+                        levelValue = "storage";
+                        infoString = "This is a storage message.";
+                    }
+                }
+                else
+                {
+                    levelValue = "normal";
+                    infoString = "This is a normal message.";
+                }
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = myDeviceId,
+                    temperature = currentTemperature,
+                    humidity = currentHumidity,
+                    pointInfo = infoString
+                };
+                var telemetryDataString = JsonConvert.SerializeObject(telemetryDataPoint);
+
+                //set the body of the message to the serialized value of the telemetry data
+                var message = new Message(Encoding.ASCII.GetBytes(telemetryDataString));
+                message.Properties.Add("level", levelValue);
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, telemetryDataString);
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
