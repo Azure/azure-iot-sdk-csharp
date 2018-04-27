d961a7fab1737e4628c4d7e1a4491719a9736f93
Dominic Betts <dobett@microsoft.com>
2018-04-27 11:29:11 +0100

    Add initial set of Quickstart code

---
+++ Quickstarts/simulated-device-2/SimulatedDevice.cs
@@ -0,0 +1,93 @@
+﻿// This application uses the Azure IoT Hub device SDK for .NET
+// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples
+
+using System;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulated_device
+{
+    class SimulatedDevice
+    {
+        static DeviceClient deviceClient;
+
+        // The device connection string to authenticate the device with your IoT hub.
+        // Using the Azure CLI:
+        // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
+        static string connectionString = "{Your device connection string here}";
+
+        static int telemetryInterval = 1; // Seconds
+
+        // Handle the direct method call
+        static Task<MethodResponse> SetTelemetryInterval(MethodRequest methodRequest, object userContext)
+        {
+            var data = Encoding.UTF8.GetString(methodRequest.Data);
+
+            // Check the payload is a single integer value
+            if (Int32.TryParse(data, out telemetryInterval))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Telemetry interval set to {0} seconds", data);
+                Console.ResetColor();
+
+                // Acknowlege the direct method call with a 200 success message
+                string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+            }
+            else
+            {
+                // Acknowlege the direct method call with a 400 error message
+                string result = "{\"result\":\"Invalid parameter\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 400));
+            }
+        }
+
+        // Async method to send simulated telemetry
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            // Initial telemetry values
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                // Create JSON message
+                var telemetryDataPoint = new
+                {
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                // Add a custom application property to the message.
+                // An IoT hub can filter on these properties without access to the message body.
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                // Send the tlemetry message
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(telemetryInterval * 1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub Quickstarts #2 - Simulated device. Ctrl-C to exit.\n");
+
+            // Connect to the IoT hub using the MQTT protocol
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+
+            // Create a handler for the direct method call
+            deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+    }
+}
