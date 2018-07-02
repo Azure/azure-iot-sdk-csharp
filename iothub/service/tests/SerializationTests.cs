617b96d9f1e70843985fa90bec96a414d5c43030
John Taylor <john.taylor@doverfs.com>
2018-07-02 13:26:45 -0500

    Added file header

--- iothub/service/tests/SerializationTests.cs
+++ iothub/service/tests/SerializationTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Devices.Shared;
+﻿ // Copyright (c) Microsoft. All rights reserved.
+ // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.Azure.Devices.Shared;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System;
