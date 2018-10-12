13edea3108c3f2e29738da6087c0c99628bab8bb
Abhipsa Misra <abmisr@microsoft.com>
2018-10-12 13:04:06 -0700

    Provisioning Service Client v2 - Initial

---
+++ provisioning/service/src/Authentication/ProvisioningServiceClientCredentials.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Common;
+using Microsoft.Rest;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Provisioning.Service
+{
+    /// <summary>
+    /// Base credentials class.
+    /// </summary>
+    public abstract class ProvisioningServiceClientCredentials : ServiceClientCredentials
+    {
+        private ProductInfo _productInfo = new ProductInfo();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add(HttpRequestHeader.Authorization.ToString(), GetSasToken());
+            request.Headers.Add("User-Agent", _productInfo.ToString());
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+
+        /// <summary>
+        /// Return the SAS token
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string GetSasToken();
+    }
+}
---
+++ provisioning/service/src/Authentication/SharedAccessKeyCredentials.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Common;
+using Microsoft.Rest;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Provisioning.Service
+{
+    /// <summary>
+    /// Shared Access Key Signature class.
+    /// </summary>
+    public class SharedAccessKeyCredentials : ProvisioningServiceClientCredentials
+    {
+        private static ServiceConnectionString _provisioningConnectionString;
+
+        /// <summary>
+        /// Create a new instance of <code>SharedAccessKeyCredentials</code> using
+        /// the Provisioning Service Connection String
+        /// </summary>
+        public SharedAccessKeyCredentials(ServiceConnectionString serviceConnectionString)
+        {
+            _provisioningConnectionString = serviceConnectionString;
+        }
+
+        /// <summary>
+        /// Return the SAS token
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetSasToken()
+        {
+            return _provisioningConnectionString.GetSasToken();
+        }
+    }
+}
