cefe884bf4188c33c640d827f2739bfbc6686dfc
timtay-microsoft <timtay@microsoft.com>
2019-01-03 12:17:27 -0800

    Provisioning Transport HTTP symmetric key support

--- provisioning/transport/http/src/ProvisioningTransportHandlerHttp.cs
+++ provisioning/transport/http/src/ProvisioningTransportHandlerHttp.cs
@@ -55,6 +55,10 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Transport
                 {
                     authStrategy = new HttpAuthStrategyX509((SecurityProviderX509)message.Security);
                 }
+                else if (message.Security is SecurityProviderSymmetricKey)
+                {
+                    authStrategy = new HttpAuthStrategySymmetricKey((SecurityProviderSymmetricKey)message.Security);
+                }
                 else
                 {
                     if (Logging.IsEnabled) Logging.Error(this, $"Invalid {nameof(SecurityProvider)} type.");
@@ -157,11 +161,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Transport
                 try
                 {
                     var errorDetails = JsonConvert.DeserializeObject<ProvisioningErrorDetails>(oe.Response.Content);
-                    throw new ProvisioningTransportException(
-                        errorDetails.CreateMessage("HTTP transport exception: service error."),
-                        oe,
-                        isTransient,
-                        errorDetails.TrackingId);
+                    throw new ProvisioningTransportException(oe.Response.Content, oe, isTransient, errorDetails);
                 }
                 catch (JsonException ex)
                 {
---
+++ provisioning/transport/http/src/SymmetricKeyCredentials.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.Transport
+{
+    internal class SymmetricKeyCredentials : ServiceClientCredentials
+    {
+        private const string SASHeaderName = "SharedAccessSignature";
+        private const string Registration = "registration";
+        private readonly string SymmetricKey;
+        private volatile string _sasToken;
+
+        public SymmetricKeyCredentials(string symmetricKey) : base()
+        {
+            SymmetricKey = symmetricKey;
+        }
+
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            string audience = request.RequestUri.AbsolutePath.Trim('/');
+            var segments = audience.Split('/');
+
+            _sasToken = ProvisioningSasBuilder.BuildSasSignature(Registration, this.SymmetricKey, string.Concat(segments[0], '/', segments[1], '/', segments[2]), TimeSpan.FromDays(1));
+            SetAuthorizationHeader(request, _sasToken);
+
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+
+        private static void SetAuthorizationHeader(HttpRequestMessage request, string sasToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue(SASHeaderName, sasToken.Substring(SASHeaderName.Length + 1));
+        }
+    }
+}
