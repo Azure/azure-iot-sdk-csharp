9cca1a6525d6c8c3d96b91fcd28f314c89cd85f2
Abhipsa Misra <abmisr@microsoft.com>
2019-02-08 17:34:02 -0800

    Added device cleanup sample.

---
+++ iot-hub/Samples/service/CleanUpDevicesSample/CleanUpDevicesSample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Samples
+{
+    public class CleanUpDevicesSample
+    {
+        private RegistryManager _rm;
+        private List<string> devicesToBeRetained =
+            new List<string>{
+                "xdevice1"
+            };
+
+        public CleanUpDevicesSample(RegistryManager rm)
+        {
+            _rm = rm ?? throw new ArgumentNullException(nameof(rm));
+        }
+
+        public async Task RunSampleAsync()
+        {
+            try
+            {
+                Console.WriteLine("Get devices");
+                string sqlQueryString = "select * from devices";
+                IQuery query = _rm.CreateQuery(sqlQueryString);
+                while (query.HasMoreResults)
+                {
+                    IEnumerable<Shared.Twin> result = await query.GetNextAsTwinAsync();
+                    foreach (var twinResult in result)
+                    {
+                        string deviceId = twinResult.DeviceId;
+                        if (!devicesToBeRetained.Contains(deviceId))
+                        {
+                            Console.WriteLine($"Removing Device ID: {deviceId}");
+                            await _rm.RemoveDeviceAsync(deviceId).ConfigureAwait(false);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception {ex.Message}");
+            }
+        }
+    }
+}
