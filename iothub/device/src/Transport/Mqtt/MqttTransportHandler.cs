1059cdef6846a152806ae929a2b70ba0566c89b3
zolvarga <zolvarga@microsoft.com>
2019-02-19 14:40:49 -0800

    Add IsReady flag to IDelegatingHandler to check MqttTransportHandler state

--- iothub/device/src/Transport/Mqtt/MqttTransportHandler.cs
+++ iothub/device/src/Transport/Mqtt/MqttTransportHandler.cs
@@ -154,7 +154,9 @@ namespace Microsoft.Azure.Devices.Client.Transport.Mqtt
             this.closeRetryPolicy = new RetryPolicy(new TransientErrorIgnoreStrategy(), 5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
         }
 
-#region Client operations
+        public override bool IsUsable => this.State != TransportState.Closed && this.State != TransportState.Error;
+
+        #region Client operations
 
         public override async Task OpenAsync(CancellationToken cancellationToken)
         {
