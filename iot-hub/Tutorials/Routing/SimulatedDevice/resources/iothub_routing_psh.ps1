ab05c1421b7f169d61a5b7ceb6cca34e0819cd0b
Robin Shahan <robinsh@microsoft.com>
2019-03-08 13:54:53 -0800

    Add scripts and RM template to code sample.

---
+++ iot-hub/Tutorials/Routing/SimulatedDevice/resources/iothub_routing_psh.ps1
@@ -0,0 +1,165 @@
+﻿#$psise.Options.FontSize=12
+
+# Log into Azure account. If using CloudShell, you've already logged in.
+#Login-AzAccount 
+
+# Set the values for the resource names that don't have to be globally unique.
+# The resources that have to have unique names are named in the script below
+#   with a random number concatenated to the name so you can probably just
+#   run this script, and it will work with no conflicts.
+$location = "West US"
+$resourceGroup = "ContosoResources"
+$iotHubConsumerGroup = "ContosoConsumers"
+$containerName = "contosoresults"
+
+# Concatenate this number onto the resources that have to be globally unique.
+# You can also use $(Get-Random) but it gives long numbers.
+#     randomNum=$(Get-Random)
+$randomNum = 1357
+
+# Create the resource group to be used 
+#   for all resources for this tutorial.
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# The IoT hub name must be globally unique, so add a random number to the end.
+$iotHubName = "ContosoTestHub" + $randomNum
+Write-Host "IoT hub name is " $iotHubName
+
+# Create the IoT hub.
+New-AzIotHub -ResourceGroupName $resourceGroup `
+    -Name $iotHubName `
+    -SkuName "S1" `
+    -Location $location `
+    -Units 1 
+
+# Add a consumer group to the IoT hub for the 'events' endpoint.
+Add-AzIotHubEventHubConsumerGroup -ResourceGroupName $resourceGroup `
+  -Name $iotHubName `
+  -EventHubConsumerGroupName $iotHubConsumerGroup `
+  -EventHubEndpointName "events"
+
+# The storage account name must be globally unique, so add a random number to the end.
+$storageAccountName = "contosostorage" + $randomNum
+Write-Host "storage account name is " $storageAccountName
+
+# Create the storage account to be used as a routing destination.
+# Save the context for the storage account 
+#   to be used when creating a container.
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName Standard_LRS `
+    -Kind Storage
+# Retrieve the connection string from the context. 
+$storageConnectionString = $storageAccount.Context.ConnectionString
+Write-Host "storage connection string = " $storageConnectionString 
+
+# Create the container in the storage account.
+New-AzStorageContainer -Name $containerName `
+    -Context $storageAccount.Context
+
+# The Service Bus namespace must be globally unique,
+#   so add a random number to the end.
+$serviceBusNamespace = "ContosoSBNamespace" + $randomNum
+Write-Host "Service Bus namespace is " $serviceBusNamespace
+
+# Create the Service Bus namespace.
+New-AzServiceBusNamespace -ResourceGroupName $resourceGroup `
+    -Location $location `
+    -Name $serviceBusNamespace 
+
+# The Service Bus queue name must be globally unique,
+#  so add a random number to the end.
+$serviceBusQueueName  = "ContosoSBQueue" + $randomNum
+Write-Host "Service Bus queue name is " $serviceBusQueueName 
+
+# Create the Service Bus queue to be used as a routing destination.
+New-AzServiceBusQueue -ResourceGroupName $resourceGroup `
+    -Namespace $serviceBusNamespace `
+    -Name $serviceBusQueueName  `
+    -EnablePartitioning $False 
+
+##### ROUTING FOR STORAGE #####
+
+# Put your subscription ID here.
+$subscriptionID = "YOURSUBSCRIPTIONID"
+$endpointName = "ContosoStorageEndpoint"
+$endpointType = "azurestoragecontainer"
+$routeName = "ContosoStorageRoute"
+$condition = 'level="storage"'
+
+# Create the routing endpoint for storage.
+Add-AzIotHubRoutingEndpoint `
+  -ResourceGroupName $resourceGroup `
+  -Name $iotHubName `
+  -EndpointName $endpointName `
+  -EndpointType $endpointType `
+  -EndpointResourceGroup $resourceGroup `
+  -EndpointSubscriptionId $subscriptionId `
+  -ConnectionString $storageConnectionString  `
+  -ContainerName $containerName
+
+# Create the route for the storage endpoint.
+Add-AzIotHubRoute `
+   -ResourceGroupName $resourceGroup `
+   -Name $iotHubName `
+   -RouteName $routeName `
+   -Source DeviceMessages `
+   -EndpointName $endpointName `
+   -Condition $condition `
+   -Enabled 
+
+##### ROUTING FOR SERVICE BUS QUEUE ##### 
+
+$endpointName = "ContosoSBQueueEndpoint"
+$endpointType = "ServiceBusQueue"
+$routeName = "ContosoSBQueueRoute"
+$condition = 'level="critical"'
+
+# Create the authorization rule for the Service Bus queue.
+New-AzServiceBusAuthorizationRule `
+  -ResourceGroupName $resourceGroup `
+  -NamespaceName $serviceBusNamespace `
+  -Queue $serviceBusQueueName `
+  -Name "sbauthrule" `
+  -Rights @("Manage","Listen","Send")
+
+# Show the rule. This is optional.
+#Get-AzServiceBusAuthorizationRule `
+#      -ResourceGroupName $resourceGroup `
+#      -Namespace $serviceBusNamespace `
+#      -Queue $serviceBusQueueName
+
+# Get the Service Bus Queue key. 
+$sbqkey = Get-AzServiceBusKey `
+    -ResourceGroupName $resourceGroup `
+    -NamespaceName $serviceBusNamespace `
+    -Queue $servicebusQueueName `
+    -Name "sbauthrule"
+
+# Set up the routing endpoint for the Service Bus queue.
+# Get the connection string from the key.
+Add-AzIotHubRoutingEndpoint `
+  -ResourceGroupName $resourceGroup `
+  -Name $iotHubName `
+  -EndpointName $endpointName `
+  -EndpointType $endpointType `
+  -EndpointResourceGroup $resourceGroup `
+  -EndpointSubscriptionId $subscriptionId `
+  -ConnectionString $sbqkey.PrimaryConnectionString
+
+# Set up the route for the Service Bus queue endpoint.
+Add-AzIotHubRoute `
+   -ResourceGroupName $resourceGroup `
+   -Name $iotHubName `
+   -RouteName $routeName `
+   -Source DeviceMessages `
+   -EndpointName $endpointName `
+   -Condition $condition `
+   -Enabled 
+
+# Show the route. This is optional.
+#Get-AzIotHubRoute `
+#   -ResourceGroupName $resourceGroup `
+#   -Name $iotHubName `
+#   -RouteName $routeName 
