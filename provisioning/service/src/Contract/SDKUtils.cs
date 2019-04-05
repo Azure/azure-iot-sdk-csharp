e97140d32bdc221cdc70df7d4161d69512cb810e
timtay-microsoft <timtay@microsoft.com>
2019-04-05 12:29:35 -0700

    Increase provisioning service api version for enrollmentGroupDeviceCapabilities

--- provisioning/service/src/Contract/SDKUtils.cs
+++ provisioning/service/src/Contract/SDKUtils.cs
@@ -5,7 +5,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Service
 {
     internal class SDKUtils
     {
-        private const string ApiVersionProvisioning = "2019-01-15";
+        private const string ApiVersionProvisioning = "2019-03-31";
         public const string ApiVersionQueryString = CustomHeaderConstants.ApiVersion + "=" + ApiVersionProvisioning;
     }
 }
