3b64f55676840000ddc1a5a237a42e63531560f5
Abhipsa Misra <abmisr@microsoft.com>
2019-04-12 17:48:51 -0700

    wip: mux template.

--- e2e/test/net451/EventHubTestListener.NetFramework.cs
+++ e2e/test/net451/EventHubTestListener.NetFramework.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.E2ETests
@@ -20,14 +21,39 @@ namespace Microsoft.Azure.Devices.E2ETests
     public partial class EventHubTestListener
     {
         private EventHubReceiver _receiver;
+        private static Dictionary<string, EventHubReceiver> s_eventHubListenerCache = new Dictionary<string, EventHubReceiver>();
+        private static SemaphoreSlim s_semaphore = new SemaphoreSlim(1, 1);
 
         private EventHubTestListener(EventHubReceiver receiver)
         {
             _receiver = receiver;
         }
 
-        public static Task<EventHubTestListener> CreateListenerPal(string deviceName)
+        public static async Task<EventHubTestListener> GetListenerAsync(string deviceName)
         {
+            try
+            {
+                await s_semaphore.WaitAsync().ConfigureAwait(false);
+                if (!s_eventHubListenerCache.TryGetValue(deviceName, out EventHubReceiver receiver))
+                {
+                    await CreateListenerPal(deviceName).ConfigureAwait(false);
+                }
+
+                EventHubReceiver ret = s_eventHubListenerCache[deviceName];
+
+                s_log.WriteLine($"{nameof(GetListenerAsync)}: Using listener with partition ID: {ret.PartitionId} for device {deviceName}.");
+                return new EventHubTestListener(ret);
+            }
+            finally
+            {
+                s_semaphore.Release();
+            }
+        }
+
+        public static Task CreateListenerPal(string deviceName)
+        {
+            s_log.WriteLine($"{nameof(GetListenerAsync)}: Listener for device {deviceName} not found.");
+
             EventHubReceiver receiver = null;
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -51,7 +77,8 @@ namespace Microsoft.Azure.Devices.E2ETests
 
             sw.Stop();
 
-            return Task.FromResult(new EventHubTestListener(receiver));
+            s_eventHubListenerCache[deviceName] = receiver;
+            return Task.FromResult(0);
         }
     }
 }
