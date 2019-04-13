92bd3660aefe25766515ea70779799bf93d4857d
Abhipsa Misra <abmisr@microsoft.com>
2019-04-12 17:48:51 -0700

    wip: mux template.

---
+++ e2e/test/MultiplexingOverAmqp.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.E2ETests
+{
+    public static class MultiplexingOverAmqp
+    {
+        public const int MuxWithoutPoolingDevicesCount = 2;
+        public const int MuxWithoutPoolingPoolSize = 1; // For enabling multiplexing without pooling, the pool size needs to be set to 1
+        public const int MuxWithPoolingDevicesCount = 4;
+        public const int MuxWithPoolingPoolSize = 2;
+
+        private static TestLogging s_log = TestLogging.GetInstance();
+        private static int connectionStatusChangesHandlerCount = 0;
+        private static ConnectionStatus lastConnectionStatus;
+        private static ConnectionStatusChangeReason lastConnectionStatusChangeReason;
+
+        private static void ConnectionStatusChangesHandler(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            connectionStatusChangesHandlerCount++;
+            lastConnectionStatus = status;
+            lastConnectionStatusChangeReason = reason;
+            s_log.WriteLine($"{nameof(MultiplexingOverAmqp)}.{nameof(ConnectionStatusChangesHandler)}: status={status} statusChangeReason={reason} count={connectionStatusChangesHandlerCount}");
+        }
+
+        public static async Task TestMultiplexingOperationAsync(
+            string devicePrefix,
+            Client.TransportType transport,
+            int poolSize,
+            int devicesCount,
+            Func<DeviceClient, Task> testOperation,
+            Func<DeviceClient, Task> cleanupOperation,
+            ConnectionStringAuthScope authScope = ConnectionStringAuthScope.Device)
+        {
+            var transportSettings = new ITransportSettings[]
+            {
+                new AmqpTransportSettings(transport)
+                {
+                    AmqpConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                    {
+                        MaxPoolSize = unchecked((uint)poolSize),
+                        Pooling = true
+                    }
+                }
+            };
+
+            ICollection<DeviceClient> deviceClients = new List<DeviceClient>();
+
+            // Arrange
+            // Initialize the test device client instances
+            // Set the device client connection status change handler
+            s_log.WriteLine($">>> {nameof(MultiplexingOverAmqp)} Initializing Device Clients for multiplexing test.");
+            for (int i = 0; i < devicesCount; i++)
+            {
+                TestDevice testDevice = await TestDevice.GetTestDeviceAsync($"{devicePrefix}_{i}_").ConfigureAwait(false);
+                DeviceClient deviceClient = testDevice.CreateDeviceClient(transportSettings, authScope);
+                // WIP: set connection status change handler
+                deviceClient.SetConnectionStatusChangesHandler(ConnectionStatusChangesHandler);
+
+                deviceClients.Add(deviceClient);
+            }
+
+            try
+            {
+                // Act-Assert
+                // Perform the test operation and verify the operation is successful
+                foreach (DeviceClient deviceClient in deviceClients)
+                {
+                    await testOperation(deviceClient).ConfigureAwait(false);
+                }
+
+                // Close the device client instances and verify the connection status change checks
+                foreach (DeviceClient deviceClient in deviceClients)
+                {
+                    await deviceClient.CloseAsync().ConfigureAwait(false);
+
+                    // WIP: set connection status change handler
+                    // The connection status change count should be 2: connect (open) and disabled (close)
+                    // The connection status should be "Disabled", with connection status change reason "Client_close"
+                }
+
+            }
+            finally
+            {
+                // Close the service-side components and dispose the device client instances.
+                foreach (DeviceClient deviceClient in deviceClients)
+                {
+                    await cleanupOperation(deviceClient).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
--- e2e/test/net451/EventHubTestListener.NetFramework.cs
+++ e2e/test/net451/EventHubTestListener.NetFramework.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.E2ETests
@@ -20,14 +21,39 @@ namespace Microsoft.Azure.Devices.E2ETests
     public partial class EventHubTestListener
     {
         private EventHubReceiver _receiver;
+        private static Dictionary<string, EventHubReceiver> s_eventHubListenerCache = new Dictionary<string, EventHubReceiver>();
+        private static SemaphoreSlim s_semaphore = new SemaphoreSlim(1, 1);
 
         private EventHubTestListener(EventHubReceiver receiver)
         {
             _receiver = receiver;
         }
 
-        public static Task<EventHubTestListener> CreateListenerPal(string deviceName)
+        public static async Task<EventHubTestListener> GetListenerAsync(string deviceName)
         {
+            try
+            {
+                await s_semaphore.WaitAsync().ConfigureAwait(false);
+                if (!s_eventHubListenerCache.TryGetValue(deviceName, out EventHubReceiver receiver))
+                {
+                    await CreateListenerPal(deviceName).ConfigureAwait(false);
+                }
+
+                EventHubReceiver ret = s_eventHubListenerCache[deviceName];
+
+                s_log.WriteLine($"{nameof(GetListenerAsync)}: Using listener with partition ID: {ret.PartitionId} for device {deviceName}.");
+                return new EventHubTestListener(ret);
+            }
+            finally
+            {
+                s_semaphore.Release();
+            }
+        }
+
+        public static Task CreateListenerPal(string deviceName)
+        {
+            s_log.WriteLine($"{nameof(GetListenerAsync)}: Listener for device {deviceName} not found.");
+
             EventHubReceiver receiver = null;
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -37,6 +63,7 @@ namespace Microsoft.Azure.Devices.E2ETests
             string partition = EventHubPartitionKeyResolver.ResolveToPartition(deviceName, eventHubPartitionsCount);
             string consumerGroupName = Configuration.IoTHub.EventHubConsumerGroup;
 
+            s_log.WriteLine($"{nameof(GetListenerAsync)}: Creating listner for device {deviceName}.");
             while (receiver == null && sw.Elapsed.Minutes < MaximumWaitTimeInMinutes)
             {
                 try
@@ -51,7 +78,8 @@ namespace Microsoft.Azure.Devices.E2ETests
 
             sw.Stop();
 
-            return Task.FromResult(new EventHubTestListener(receiver));
+            s_eventHubListenerCache[deviceName] = receiver;
+            return Task.FromResult(0);
         }
     }
 }
