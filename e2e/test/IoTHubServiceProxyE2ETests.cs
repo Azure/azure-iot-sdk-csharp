b1958390f9feab073d0c2920869dd9fb6cc9523b
Abhipsa Misra <abmisr@microsoft.com>
2019-04-25 12:00:46 -0700

    Reverting spacing changes for IoTHubServiceProxyE2ETests.cs file.

--- e2e/test/IoTHubServiceProxyE2ETests.cs
+++ e2e/test/IoTHubServiceProxyE2ETests.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Client;
-using Microsoft.Azure.Devices.Shared;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Azure.Devices.E2ETests
 {
@@ -105,7 +105,7 @@ namespace Microsoft.Azure.Devices.E2ETests
             }
         }
 
-        private Message ComposeD2CTestMessage(out string payload, out string p1Value)
+       private Message ComposeD2CTestMessage(out string payload, out string p1Value)
         {
             payload = Guid.NewGuid().ToString();
             p1Value = Guid.NewGuid().ToString();
