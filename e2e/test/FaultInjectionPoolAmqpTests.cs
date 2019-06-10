fd2bf83c97a139a1de6b0c8f65a688250e18c9b6
Abhipsa Misra <22563986+abhipsaMisra@users.noreply.github.com>
2019-06-10 11:02:06 -0700

    Fault injection tests for pooling scenarios (#941)
    * E2E tests with pooling - fault injection scenarios
    
    * Reverting the change for using 2 hubs for e2e tests.

---
+++ e2e/test/FaultInjectionPoolAmqpTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace Microsoft.Azure.Devices.E2ETests
+{
+    [TestClass]
+    [TestCategory("IoTHub-E2E")]
+    [TestCategory("IoTHub-FaultInjection-PoolAmqp")]
+    public partial class FaultInjectionPoolAmqpTests : IDisposable
+    {
+        private static TestLogging _log = TestLogging.GetInstance();
+
+        private readonly ConsoleEventListener _listener;
+
+        public FaultInjectionPoolAmqpTests()
+        {
+            _listener = TestConfig.StartEventListener();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
