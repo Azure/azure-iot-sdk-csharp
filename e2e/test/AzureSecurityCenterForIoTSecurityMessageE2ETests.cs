8cb14d0e040f684b03cae03d24c260a59a4be93e
AmitEnoch <45037036+AmitEnoch@users.noreply.github.com>
2019-06-26 21:14:38 +0300

    AddE2ETestsWithAscForIotIntegration (#910)
    * Add Oms validation for events sent from a security message
    
    * Move ASCforIoT Security message e2e test to a different class
    
    * method rename
    
    * Type in pre-req script
    
    * according to comments,
    Create json instead of template
    
    * Added readme section for ASC for IoT tests
    Added env vars for ASC for ioT tests in vsts yaml file
    
    * More detailed docs
    Name change
    
    * Update readme.md
    
    minor text edits
    
    * Authentication with X509Certificate instead of username and password
    
    * Update scripts to use certificate
    
    * refactor ASC to Azure Security center
    
    * Better utilisation log analytics http client

---
+++ e2e/test/AzureSecurityCenterForIoTSecurityMessageE2ETests.cs
@@ -0,0 +1,206 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics.Tracing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.E2ETests
+{
+    [TestClass]
+    [TestCategory("IoTHub-E2E")]
+    public class AzureSecurityCenterForIoTSecurityMessageE2ETests : IDisposable
+    {
+        private readonly string _devicePrefix = $"E2E_{nameof(AzureSecurityCenterForIoTSecurityMessageE2ETests)}_";
+        private readonly string _modulePrefix = $"E2E_{nameof(AzureSecurityCenterForIoTSecurityMessageE2ETests)}_";
+        private static TestLogging _log = TestLogging.GetInstance();
+
+        private readonly ConsoleEventListener _listener;
+        private readonly AzureSecurityCenterForIoTLogAnalyticsClient _logAnalyticsClient;
+
+        public AzureSecurityCenterForIoTSecurityMessageE2ETests()
+        {
+            _listener = TestConfig.StartEventListener();
+            _logAnalyticsClient = AzureSecurityCenterForIoTLogAnalyticsClient.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_DeviceSendSingleMessage_Amqp()
+        {
+            await TestSecurityMessage(Client.TransportType.Amqp_Tcp_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_ModuleSendSingleMessage_Amqp()
+        {
+            await TestSecurityMessageModule(Client.TransportType.Amqp_Tcp_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_DeviceSendSingleMessage_AmqpWs()
+        {
+            await TestSecurityMessage(Client.TransportType.Amqp_WebSocket_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_ModuleSendSingleMessage_AmqpWs()
+        {
+            await TestSecurityMessageModule(Client.TransportType.Amqp_WebSocket_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_DeviceSendSingleMessage_Mqtt()
+        {
+            await TestSecurityMessage(Client.TransportType.Mqtt_Tcp_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_ModuleSendSingleMessage_Mqtt()
+        {
+            await TestSecurityMessageModule(Client.TransportType.Mqtt_Tcp_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_DeviceSendSingleMessage_MqttWs()
+        {
+            await TestSecurityMessage(Client.TransportType.Mqtt_WebSocket_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_ModuleSendSingleMessage_MqttWs()
+        {
+            await TestSecurityMessageModule(Client.TransportType.Mqtt_WebSocket_Only).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task SecurityMessage_DeviceSendSingleMessage_Http()
+        {
+            await TestSecurityMessage(Client.TransportType.Http1).ConfigureAwait(false);
+        }
+
+        private Client.Message ComposeD2CSecurityTestMessage(out string eventId, out string payload, out string p1Value)
+        {
+            eventId = p1Value = Guid.NewGuid().ToString();
+            payload = ComposeAzureSecurityCenterForIoTSecurityMessagePayload(eventId).ToString(Newtonsoft.Json.Formatting.None);
+
+            Client.Message message = new Client.Message(Encoding.UTF8.GetBytes(payload))
+            {
+                Properties = { ["property1"] = p1Value }
+            };
+            message.SetAsSecurityMessage();
+
+            return message;
+        }
+
+        private JObject ComposeAzureSecurityCenterForIoTSecurityMessagePayload(string eventId)
+        {
+            var now = DateTime.UtcNow;
+            return new JObject
+            {
+                { "AgentVersion", "0.0.1" },
+                { "AgentId" , Guid.NewGuid().ToString() },
+                { "MessageSchemaVersion", "1.0" },
+                { "Events", new JArray
+                    { new JObject
+                        {
+                            { "EventType", "Security" },
+                            { "Category", "Periodic" },
+                            { "Name", "ListeningPorts" },
+                            { "IsEmpty", true },
+                            { "PayloadSchemaVersion", "1.0" },
+                            { "Id", eventId },
+                            { "TimestampLocal", now },
+                            { "TimestampUTC", now },
+                            { "Payload", new JArray() }
+                        }
+                    }
+                }
+            };
+        }
+
+        private async Task TestSecurityMessage(Client.TransportType transport)
+        {
+            TestDevice testDevice = await TestDevice.GetTestDeviceAsync(_devicePrefix).ConfigureAwait(false);
+            EventHubTestListener testListener = await EventHubTestListener.CreateListener(testDevice.Id).ConfigureAwait(false);
+
+            using (DeviceClient deviceClient = testDevice.CreateDeviceClient(transport))
+            {
+                try
+                {
+                    await SendSingleSecurityMessage(deviceClient, testDevice.Id, testListener, _logAnalyticsClient).ConfigureAwait(false);
+                }
+                finally
+                {
+                    await deviceClient.CloseAsync().ConfigureAwait(false);
+                    await testListener.CloseAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        private async Task TestSecurityMessageModule(Client.TransportType transport)
+        {
+            TestModule testModule = await TestModule.GetTestModuleAsync(_devicePrefix, _modulePrefix).ConfigureAwait(false);
+            EventHubTestListener testListener = await EventHubTestListener.CreateListener(testModule.Id).ConfigureAwait(false);
+
+            using (ModuleClient moduleClient = ModuleClient.CreateFromConnectionString(testModule.ConnectionString, transport))
+            {
+                try
+                {
+                    await SendSingleSecurityMessageModule(moduleClient, testModule.DeviceId, testListener, _logAnalyticsClient).ConfigureAwait(false);
+                }
+                finally
+                {
+                    await moduleClient.CloseAsync().ConfigureAwait(false);
+                    await testListener.CloseAsync().ConfigureAwait(false);
+                }
+            }
+        }
+
+        private async Task SendSingleSecurityMessage(DeviceClient deviceClient, string deviceId, EventHubTestListener testListener, AzureSecurityCenterForIoTLogAnalyticsClient logAnalticsTestClient)
+        {
+            await deviceClient.OpenAsync().ConfigureAwait(false);
+
+            Client.Message testMessage = ComposeD2CSecurityTestMessage(out string eventId, out string payload, out string p1Value);
+            await deviceClient.SendEventAsync(testMessage).ConfigureAwait(false);
+
+            await ValidateEvent(deviceId, eventId, payload, p1Value, testListener, logAnalticsTestClient).ConfigureAwait(false);
+        }
+
+        private async Task SendSingleSecurityMessageModule(ModuleClient moduleClient, string deviceId, EventHubTestListener testListener, AzureSecurityCenterForIoTLogAnalyticsClient logAnalticsTestClient)
+        {
+            await moduleClient.OpenAsync().ConfigureAwait(false);
+
+            Client.Message testMessage = ComposeD2CSecurityTestMessage(out string eventId, out string payload, out string p1Value);
+            await moduleClient.SendEventAsync(testMessage).ConfigureAwait(false);
+
+            await ValidateEvent(deviceId, eventId, payload, p1Value, testListener, logAnalticsTestClient).ConfigureAwait(false);
+        }
+
+        private async Task ValidateEvent(string deviceId, string eventId, string payload, string p1Value,
+            EventHubTestListener testListener, AzureSecurityCenterForIoTLogAnalyticsClient logAnalticsTestClient)
+        {
+            bool isReceivedEventHub = await testListener.WaitForMessage(deviceId, payload, p1Value).ConfigureAwait(false);
+            Assert.IsFalse(isReceivedEventHub, "Security message received in customer event hub.");
+            bool isReceivedOms = await logAnalticsTestClient.IsRawEventExist(deviceId, eventId).ConfigureAwait(false);
+            Assert.IsTrue(isReceivedOms, "Security message was not recived in customer log analytics");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _logAnalyticsClient.Dispose();
+            }
+        }
+    }
+}
--- e2e/test/prerequisites/readme.md
+++ e2e/test/prerequisites/readme.md
@@ -31,15 +31,14 @@ docker run -d --restart unless-stopped --name azure-iot-tinyproxy -p 127.0.0.1:8
 Alternatives:
  - TinyProxy for Linux/Mac: https://tinyproxy.github.io/
  - Squid for Windows: https://chocolatey.org/packages/squid
-
  
 ### Far away hub
+
 In order to test reprovisioning, your dps instance under test must be linked to two different iot hubs. The first hub should already be set under the
 IOTHUB_CONN_STRING_CSHARP environment variable, and the other linked hub should be set as the value to the FAR_AWAY_IOTHUB_HOSTNAME environment variable
 
 While the name of this variable indicates that the hub is far away, the tests currently do not require the hub to be far away. The tests
 only require it to be a separate hub from the IOTHUB_CONN_STRING_CSHARP hub. It can be deployed to any region.
-
 
 ### Custom allocation policy webhook
 
@@ -54,3 +53,60 @@ In this folder, there is a run.csx file that is the code the azure function shou
 The actual webhook url can be found through the azure portal, under your function, where there is a "Get Function URL" button that gives you a url 
 such as: "https://someazurefunction.azurewebsites.net/api/SomeTriggerName?code=XXXXXXXX" and this is the value that should be configured to the 
 CUSTOM_ALLOCATION_POLICY_WEBHOOK environment name
+
+### Azure Security Center for IoT Security Message E2E Tests
+
+The Azure Security Center for IoT Security Message E2E tests validates that messages marked with SetAsSecurityMessage and have Azure Security Center for IoT security message payload are sent correctly and ingested to the customer Log Analytics workspace. 
+For more information about Azure Security Center for IoT please visit: [Azure Security Center for IoT architecture](https://docs.microsoft.com/en-us/azure/asc-for-iot/architecture)
+
+Important Note:
+
+Azure Security Center for IoT is currently available in limited regions.
+Please make sure the tested IoT Hub is in the supported regions
+
+Test Flow:
+
+- Generate fake message payload that complies with Azure Security Center for IoT security message schemas. reference: [Send security messages SDK](https://docs.microsoft.com/en-us/azure/asc-for-iot/how-to-send-security-messages)
+- Mark the message as a security message with the method SetAsSecurityMessage
+- Send the message in one of the supported Device SDK protocols
+- Validate the message has arrived and ingested to the customer Log Analytics workspace
+
+Prerequisites:
+
+- Log Analytics workspace - Where Azure Security Center for IoT stores its data.
+- Enable Azure Security Center for IoT - Azure Security Center for IoT should be enabled on the hub found in `Configuration.IoTHub` with the feature “Store raw device security events in LogAnalytics.” set to on, for onboarding instructions, please see: [ASC for IoT Quickstart](https://docs.microsoft.com/en-us/azure/asc-for-iot/quickstart-onboard-iot-hub) 
+- Azure Active Directory application with a reader role on the Log Analytics workspace - The tests uses Azure Active Directory Application to authenticate against the Log Analytics workspace. The service principal created by the Active Directory application must be assigned with a reader role on the Log Analytics workspace. Follow this link for instructions [Creating Azure Active Directory application and a service principal](https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal#assign-the-application-to-a-role)
+- Self signed certificate - for authenticating with the Active Directory app
+
+Note on Central US EUAP region:
+
+Azure Security Center for IoT UI is currently not available in Central US EUAP.
+To enable Azure Security Center for IoT in this region, please use the follwoing REST command:
+
+```
+URL: https://management.azure.com/subscriptions/<SubscriptionID>/resourceGroups/<ResourceGroup>/providers/Microsoft.Security/IoTSecuritySolutions/<SecuritySolutionName>?api-version=2017-08-01-preview
+Method: PUT
+Headers:
+Content-Type - application/json
+Authorization - bearer token
+Body:
+{   "location": "North Europe",
+    "properties": {        
+        "displayName": "<DisplayName>",
+        "status": "Enabled",
+        "export": ["RawEvents"],
+        "disabledDataSources": [],
+        "workspace": "<Log Analytics Resource ID>",
+        "iotHubs": [
+            "<IoT hub Resource ID>"
+        ]
+    }
+}
+```
+
+Test configuration:
+
+- `LA_AAD_TENANT` – The Azure Active Directory tenant, can be found [here](https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/Properties)  under Directory ID
+- `LA_AAD_APP_ID` – The Azure Active Directory application ID. How to [Get application ID and authentication key](https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal#get-application-id-and-authentication-key)
+- `LA_AAD_APP_CERT_BASE64` – The certificate for authenticating with the given app
+- `LA_WORKSPACE_ID` – The Log Analytics workspace Id of the Log Analytics workspace that connected to the ASC for IoT security solution
