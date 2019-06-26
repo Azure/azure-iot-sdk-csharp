b342ecf6b62d89129f13a9e38351ddaac8a7cbb8
AmitEnoch <45037036+AmitEnoch@users.noreply.github.com>
2019-06-26 21:14:38 +0300

    AddE2ETestsWithAscForIotIntegration (#910)
    * Add Oms validation for events sent from a security message
    
    * Move ASCforIoT Security message e2e test to a different class
    
    * method rename
    
    * Type in pre-req script
    
    * according to comments,
    Create json instead of template
    
    * Added readme section for ASC for IoT tests
    Added env vars for ASC for ioT tests in vsts yaml file
    
    * More detailed docs
    Name change
    
    * Update readme.md
    
    minor text edits
    
    * Authentication with X509Certificate instead of username and password
    
    * Update scripts to use certificate
    
    * refactor ASC to Azure Security center
    
    * Better utilisation log analytics http client

--- e2e/test/AzureSecurityCenterForIoTSecurityMessageE2ETests.cs
+++ e2e/test/AzureSecurityCenterForIoTSecurityMessageE2ETests.cs
@@ -169,15 +169,15 @@ namespace Microsoft.Azure.Devices.E2ETests
 
             await ValidateEvent(deviceId, eventId, payload, p1Value, testListener, logAnalticsTestClient).ConfigureAwait(false);
         }
-
-        private Task SendSingleSecurityMessageModule(ModuleClient moduleClient, string deviceId, EventHubTestListener testListener, AzureSecurityCenterForIoTLogAnalyticsClient logAnalticsTestClient)
+        
+        private async Task SendSingleSecurityMessageModule(ModuleClient moduleClient, string deviceId, EventHubTestListener testListener, AzureSecurityCenterForIoTLogAnalyticsClient logAnalticsTestClient)
         {
-            moduleClient.OpenAsync();
+            await moduleClient.OpenAsync().ConfigureAwait(false);
 
             Client.Message testMessage = ComposeD2CSecurityTestMessage(out string eventId, out string payload, out string p1Value);
-            moduleClient.SendEventAsync(testMessage);
+            await moduleClient.SendEventAsync(testMessage).ConfigureAwait(false);
 
-            return ValidateEvent(deviceId, eventId, payload, p1Value, testListener, logAnalticsTestClient);
+            await ValidateEvent(deviceId, eventId, payload, p1Value, testListener, logAnalticsTestClient).ConfigureAwait(false);
         }
 
         private async Task ValidateEvent(string deviceId, string eventId, string payload, string p1Value,
