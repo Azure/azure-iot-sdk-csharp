4ed985ca1addca5707d05824f73b80cb2fbe6d8c
timtay-microsoft <timtay@microsoft.com>
2019-07-01 11:48:07 -0700

    Digital Twin Service Client for public preview
    netstandard2.0 project because AutoRest dependency

---
+++ iothub/digitaltwin/service/AuthorizationDelegatingHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.IoT.DigitalTwin.Service
+{
+    class AuthorizationDelegatingHandler : DelegatingHandler
+    {
+        IoTServiceClientCredentials ioTServiceClientCredentials;
+        public AuthorizationDelegatingHandler(IoTServiceClientCredentials ioTServiceClientCredentials)
+        {
+            this.ioTServiceClientCredentials = ioTServiceClientCredentials;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            await ioTServiceClientCredentials.ProcessHttpRequestAsync(request, cancellationToken).ConfigureAwait(false);
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ iothub/digitaltwin/service/models/DigitalTwin.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.IoT.DigitalTwin.Service.Models
+{
+    public partial class DigitalTwin
+    {
+        /// <summary>
+        /// Initializes a new instance of the DigitalTwinInterfaces class.
+        /// </summary>
+        public DigitalTwin()
+        {
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DigitalTwinInterfaces class.
+        /// </summary>
+        /// <param name="interfaces">Interface(s) data on the digital
+        /// twin.</param>
+        /// <param name="version">Version of digital twin.</param>
+        public DigitalTwin(IDictionary<string, InterfaceModel> interfaces = default(IDictionary<string, InterfaceModel>), long? version = default(long?))
+        {
+            Components = interfaces;
+            Version = version;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults
+        /// </summary>
+        partial void CustomInit();
+
+        /// <summary>
+        /// Gets or sets interface(s) data on the digital twin.
+        /// </summary>
+        [JsonProperty(PropertyName = "interfaces")]
+        public IDictionary<string, InterfaceModel> Components { get; set; }
+
+        /// <summary>
+        /// Gets or sets version of digital twin.
+        /// </summary>
+        [JsonProperty(PropertyName = "version")]
+        public long? Version { get; set; }
+    }
+}
