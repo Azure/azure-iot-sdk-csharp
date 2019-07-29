814ee474315f949fe53eca05a308ee118b6a0c48
David Lu <davilu@microsoft.com>
2019-07-29 11:11:12 -0700

    fixed retry policy issue while reconnecting

--- iothub/device/src/Transport/RetryDelegatingHandler.cs
+++ iothub/device/src/Transport/RetryDelegatingHandler.cs
@@ -529,6 +529,17 @@ namespace Microsoft.Azure.Devices.Client.Transport
                 return;
             }
 
+            if (!_internalRetryPolicy.RetryStrategy.GetShouldRetry().Invoke(0, new IotHubCommunicationException(), out TimeSpan delay))
+            {
+                if (Logging.IsEnabled) Logging.Info(this, "Transport disconnected: closed by application.", nameof(HandleDisconnect));
+                _onConnectionStatusChanged(ConnectionStatus.Disconnected, ConnectionStatusChangeReason.Retry_Expired);
+                return;
+            }
+
+            if (delay > TimeSpan.Zero)
+            {
+                await Task.Delay(delay).ConfigureAwait(false);
+            }
 
             if (Logging.IsEnabled) Logging.Info(this, "Transport disconnected: unexpected.", nameof(HandleDisconnect));
             await _handlerLock.WaitAsync().ConfigureAwait(false);
