0f50514f3870eb92f2bb1ed7252f50f12330f97f
Jasmine Lo <jasminel@microsoft.com>
2019-08-19 13:27:01 -0700

    feat(digitaltwin-device): add commands dispatching

---
+++ digitaltwin/device/src/Model/DigitalTwinPropertyResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Devices.DigitalTwin.Client.Model
+{
+    public class DigitalTwinPropertyResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DigitalTwinPropertyResponse"/> class.
+        /// </summary>
+        /// <param name="respondVersion">The response version.</param>
+        /// <param name="statusCode">The status code which maps to appropriate HTTP status code of the property updates.</param>
+        /// <param name="statusDescription">Friendly description string of current status of update.</param>
+        public DigitalTwinPropertyResponse(int respondVersion, int statusCode, string statusDescription)
+        {
+            RespondVersion = respondVersion;
+            StatusCode = statusCode;
+            StatusDescription = statusDescription;
+        }
+
+        /// <summary>
+        /// The respond version is used for server to disambiguate calls for given property.
+        /// </summary>
+        public int RespondVersion
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The status code which maps to appropriate HTTP status code of the property updates.
+        /// </summary>
+        public int StatusCode
+        {
+            get; private set;
+        }
+
+        public string StatusDescription
+        {
+            get; private set;
+        }
+    }
+}
