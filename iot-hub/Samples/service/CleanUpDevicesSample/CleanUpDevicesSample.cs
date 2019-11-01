3778288b30aa73179c23e3625221bf6758a2a7eb
Cristian Pop <Cristian.Pop@microsoft.com>
2019-11-01 10:34:14 -0700

    IoT Hub device cleaning sample.

--- iot-hub/Samples/service/CleanUpDevicesSample/CleanUpDevicesSample.cs
+++ iot-hub/Samples/service/CleanUpDevicesSample/CleanUpDevicesSample.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Samples
 {
     public class CleanUpDevicesSample
     {
+        private const int QueryBatchSize = 1000;
+        private const int DeleteBatchSize = 100;
         private RegistryManager _rm;
-        private List<string> devicesToBeRetained =
+        private List<string> _deleteDeviceWithPrefix =
             new List<string>{
-                "DoNotDelete1"
+                // C# E2E tests
+                "E2E_",
+
+                // C E2E tests
+                "e2e_",
+                "e2e-",
+                "symmetrickey-registration-id-",
+                "tpm-registration-id-",
+                "csdk_",
+                "someregistrationid-",
             };
+
+        private List<Device> _devicesToDelete = new List<Device>();
 
         public CleanUpDevicesSample(RegistryManager rm)
         {
@@ -22,41 +38,76 @@ namespace Microsoft.Azure.Devices.Samples
         {
             try
             {
-                string countSqlQuery = "SELECT COUNT() AS numberOfDevices FROM devices";
-                IQuery countQuery = _rm.CreateQuery(countSqlQuery);
-                while (countQuery.HasMoreResults)
-                {
-                    IEnumerable<string> result = await countQuery.GetNextAsJsonAsync().ConfigureAwait(false);
-                    foreach (var item in result)
-                    {
-                        Console.WriteLine($"Total no of devices on the hub: \n{item}");
-                    }
-                }
+                await PrintDeviceCount();
 
                 int devicesDeleted = 0;
                 Console.WriteLine("Clean up devices:");
                 string sqlQueryString = "select * from devices";
-                IQuery query = _rm.CreateQuery(sqlQueryString);
+                IQuery query = _rm.CreateQuery(sqlQueryString, QueryBatchSize);
+
                 while (query.HasMoreResults)
                 {
                     IEnumerable<Shared.Twin> result = await query.GetNextAsTwinAsync();
                     foreach (var twinResult in result)
                     {
                         string deviceId = twinResult.DeviceId;
-                        if (!devicesToBeRetained.Contains(deviceId))
+                        foreach (string prefix in _deleteDeviceWithPrefix)
                         {
-                            Console.WriteLine($"Removing Device ID: {deviceId}");
-                            await _rm.RemoveDeviceAsync(deviceId).ConfigureAwait(false);
-                            devicesDeleted++;
+                            if (deviceId.StartsWith(prefix))
+                            {
+                                _devicesToDelete.Add(new Device(deviceId));
+                            }
                         }
                     }
                 }
-                Console.Write($"Total no of devices deleted: {devicesDeleted}");
+
+                var _bulkDeleteList = new List<Device>(DeleteBatchSize);
+                while (_devicesToDelete.Count > 0)
+                {
+                    int i;
+                    for (i = 0; (i < DeleteBatchSize) && (i < _devicesToDelete.Count); i++)
+                    {
+                        _bulkDeleteList.Add(_devicesToDelete[i]);
+                        Console.WriteLine($"\tRemove: {_devicesToDelete[i].Id}");
+                    }
+
+                    _devicesToDelete.RemoveRange(0, i);
+
+                    BulkRegistryOperationResult ret = await _rm.RemoveDevices2Async(_bulkDeleteList, true, CancellationToken.None).ConfigureAwait(false);
+                    devicesDeleted += _bulkDeleteList.Count - ret.Errors.Length;
+                    Console.WriteLine($"BATCH DELETE: {devicesDeleted}/{_bulkDeleteList.Count}");
+                    if (!ret.IsSuccessful)
+                    {
+                        foreach (DeviceRegistryOperationError error in ret.Errors)
+                        {
+                            Console.WriteLine($"\tERROR: {error.DeviceId} - {error.ErrorCode}: {error.ErrorStatus}");
+                        }
+                    }
+
+                    _bulkDeleteList.Clear();
+                }
+
+                Console.WriteLine($"-- Total no of devices deleted: {devicesDeleted}");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception {ex.Message}");
+                Console.WriteLine(ex);
+            }
+        }
+
+        private async Task PrintDeviceCount()
+        {
+            string countSqlQuery = "SELECT COUNT() AS numberOfDevices FROM devices";
+            IQuery countQuery = _rm.CreateQuery(countSqlQuery);
+            while (countQuery.HasMoreResults)
+            {
+                IEnumerable<string> result = await countQuery.GetNextAsJsonAsync().ConfigureAwait(false);
+                foreach (var item in result)
+                {
+                    Console.WriteLine($"Total no of devices on the hub: \n{item}");
+                }
             }
         }
     }
 }
+
