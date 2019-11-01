170532fc567f4e9d2c07db126d5df205aa96c0ad
timtay-microsoft <timtay@microsoft.com>
2019-11-01 12:12:02 -0700

    Fix issue where integer underflow caused exponential backoff time to retry to underflow
    keeping all potentially large values as doubles makes them go to infinity rather than underflow, thus preventing negative values from happening

--- iothub/device/src/TransientFaultHandling/ExponentialBackoff.cs
+++ iothub/device/src/TransientFaultHandling/ExponentialBackoff.cs
@@ -98,10 +98,23 @@ namespace Microsoft.Azure.Devices.Client.TransientFaultHandling
                 if (currentRetryCount < this.retryCount)
                 {
                     Random random = new Random();
-                    int num = (int)((Math.Pow(2.0, (double)currentRetryCount) - 1.0) * (double)random.Next((int)(this.deltaBackoff.TotalMilliseconds * 0.8), (int)(this.deltaBackoff.TotalMilliseconds * 1.2)));
-                    int num2 = (int)Math.Min(this.minBackoff.TotalMilliseconds + (double)num, this.maxBackoff.TotalMilliseconds);
-                    retryInterval = TimeSpan.FromMilliseconds((double)num2);
-                    return true;
+                    try
+                    {
+                        checked
+                        {
+                            int num = (int) (Math.Pow(2.0, currentRetryCount) - 1.0) * random.Next((int)(this.deltaBackoff.TotalMilliseconds * 0.8), (int)(this.deltaBackoff.TotalMilliseconds * 1.2));
+                            int num2 = (int) Math.Min(this.minBackoff.TotalMilliseconds + num, this.maxBackoff.TotalMilliseconds);
+                            retryInterval = TimeSpan.FromMilliseconds(num2);
+                            return true;
+                        }
+                    }
+                    catch (OverflowException e)
+                    {
+                        //Integer overflow only occurs due to the exponential backoff calculations at high retry count
+                        // Therefore, if integer overflow occurs, it is reasonable to default to the default maxBackoff
+                        retryInterval = TimeSpan.FromMilliseconds(this.maxBackoff.TotalMilliseconds);
+                        return true;
+                    }
                 }
                 retryInterval = TimeSpan.Zero;
                 return false;
