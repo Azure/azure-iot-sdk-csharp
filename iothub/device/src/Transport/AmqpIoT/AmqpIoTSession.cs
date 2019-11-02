1583b99ad404be1b0cb4551b353b461f37cc15ed
David Lu <davilu@microsoft.com>
2019-11-01 21:44:57 -0700

    fixed incorrect exception mapping

--- iothub/device/src/Transport/AmqpIoT/AmqpIoTSession.cs
+++ iothub/device/src/Transport/AmqpIoT/AmqpIoTSession.cs
@@ -235,8 +235,9 @@ namespace Microsoft.Azure.Devices.Client.Transport.AmqpIoT
                     if (ex is AmqpIoTResourceException)
                     {
                         amqpSession.SafeClose();
+                        throw new IotHubCommunicationException(ex.Message, ex);
                     }
-                    throw new IotHubCommunicationException(ex.Message, ex);
+                    throw ex;
                 }
             }
             finally
