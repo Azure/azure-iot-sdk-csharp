bba415da2fc9ca41e99a09a96a8f66449732d974
timtay-microsoft <timtay@microsoft.com>
2019-11-14 13:46:01 -0800

    Replace DigitalTwinId with Device Id in samples, fix env vars
    Existing samples use device id, not digital twin id.
    
    IotHubConnection string env var was not consistent with usage string

--- digitaltwin/service/sample/GetDigitalTwin/Program.cs
+++ digitaltwin/service/sample/GetDigitalTwin/Program.cs
@@ -7,18 +7,18 @@ namespace Sample
 {
     class Program
     {
-        private static string CONNECTION_STRING = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-        private static string DIGITAL_TWIN_ID = Environment.GetEnvironmentVariable("DIGITAL_TWIN_ID");
+        private static string IOTHUB_CONNECTION_STRING = Environment.GetEnvironmentVariable("IOTHUB_CONNECTION_STRING");
+        private static string DEVICE_ID = Environment.GetEnvironmentVariable("DEVICE_ID");
 
         public static int Main(string[] args)
         {
             verifyInputs();
 
-            var digitalTwinServiceClient = new DigitalTwinServiceClient(CONNECTION_STRING);
+            var digitalTwinServiceClient = new DigitalTwinServiceClient(IOTHUB_CONNECTION_STRING);
 
-            Console.WriteLine("Getting the status of digital twin " + DIGITAL_TWIN_ID);
+            Console.WriteLine("Getting the status of digital twin " + DEVICE_ID);
 
-            string digitalTwin = digitalTwinServiceClient.GetDigitalTwin(DIGITAL_TWIN_ID);
+            string digitalTwin = digitalTwinServiceClient.GetDigitalTwin(DEVICE_ID);
             string jsonFormatted = JValue.Parse(digitalTwin).ToString(Formatting.Indented);
 
             Console.WriteLine("Got the status of the digital twin successfully, the returned string was:");
@@ -32,7 +32,7 @@ namespace Sample
 
         private static void verifyInputs()
         {
-            if (isNullOrEmpty(CONNECTION_STRING) || isNullOrEmpty(DIGITAL_TWIN_ID))
+            if (isNullOrEmpty(IOTHUB_CONNECTION_STRING) || isNullOrEmpty(DEVICE_ID))
             {
                 Console.WriteLine(usage);
                 Console.WriteLine("Enter any key to finish");
@@ -48,6 +48,6 @@ namespace Sample
 
         private static String usage = "In order to run this sample, you must set environment variables for \n" +
                 "IOTHUB_CONNECTION_STRING - Your IoT Hub's connection string\n" +
-                "DIGITAL_TWIN_ID - your digital twin id to invoke the command onto\n";
+                "DEVICE_ID - The ID of the device to invoke the command onto\n";
     }
 }
