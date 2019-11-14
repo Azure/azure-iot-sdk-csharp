0082058698ccef95337d7d19e9859bd121a287b7
timtay-microsoft <timtay@microsoft.com>
2019-11-14 11:36:24 -0800

    Change digital twin solution to csproj, bump device client api version to preview version
    Removed references to device client 1.22.0 since we no longer need to build a device client nuget from source to get the preview api version device client. This is because we now do project reference rather than nuget reference for digital twin to device client

--- iothub/device/src/Common/Api/ClientApiVersionHelper.cs
+++ iothub/device/src/Common/Api/ClientApiVersionHelper.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Azure.Devices.Client
     internal class ClientApiVersionHelper
     {
         internal const string ApiVersionQueryPrefix = "api-version=";
-        internal const string ApiVersionLatest = "2018-06-30";
+        internal const string ApiVersionLatest = "2019-07-01-preview";
 
         public const string ApiVersionString = ApiVersionLatest;
         public const string ApiVersionQueryString = ApiVersionQueryPrefix + ApiVersionString;
