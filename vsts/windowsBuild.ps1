ca17278a58f5e640d0172ef383320fe5099968db
timtay-microsoft <timtay@microsoft.com>
2020-01-29 11:30:38 -0800

    adsf

--- vsts/windowsBuild.ps1
+++ vsts/windowsBuild.ps1
@@ -5,7 +5,7 @@ logman create trace IotTrace -o iot.etl -pf tools/CaptureLogs/iot_providers.txt
 logman start IotTrace
 
 Write-Host Start build
-.\gatedBuild.ps1
+.\vsts\gatedBuild.ps1
 
 Write-Host Stop ETL logging
 logman stop IotTrace
