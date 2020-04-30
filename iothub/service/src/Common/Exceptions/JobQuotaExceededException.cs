119f9c911be123ad294c4424599ea9b5f407a6bc
abhipds <misra.abhi14@gmail.com>
2020-04-30 11:36:41 -0700

    fix(iot-service): Update JobQuotaExceededException to include the associated http error reason

--- iothub/service/src/Common/Exceptions/JobQuotaExceededException.cs
+++ iothub/service/src/Common/Exceptions/JobQuotaExceededException.cs
@@ -1,17 +1,33 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.Azure.Devices.Common.Exceptions
 {
-    using System;
-
+    /// <summary>
+    /// Exception thrown when IoT Hub exceeds the available quota for active jobs
+    /// </summary>
     [Serializable]
     public sealed class JobQuotaExceededException : IotHubException
     {
-        public JobQuotaExceededException()
-            : base("Job quota has been exceeded")
-        {
+        private const string JobQuotaExceededMessage = "Job quota has been exceeded";
 
+        /// <summary>
+        /// Initializes an instance of JobQuotaExceededException with the default message
+        /// </summary>
+        public JobQuotaExceededException()
+            : base(JobQuotaExceededMessage)
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of JobQuotaExceededException with the message from the Http response filled in
+        /// </summary>
+        /// <param name="message">The error message returned in the Http response</param>
+        public JobQuotaExceededException(string message)
+            : base(message)
+        {
         }
     }
 }
