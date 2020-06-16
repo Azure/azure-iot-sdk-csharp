7d3122513c15996eb996aa6cbbf3856faf279c7b
Basel Rustum <barustum@microsoft.com>
2020-06-16 15:28:54 -0700

    feat(iot-dev): Add client options to module client constructors to allow for passing module Id

--- iothub/device/tests/ModuleClientTests.cs
+++ iothub/device/tests/ModuleClientTests.cs
@@ -73,6 +73,22 @@ namespace Microsoft.Azure.Devices.Client.Test
         }
 
         [TestMethod]
+        public void ModuleClient_CreateFromConnectionString_WithClientOptions()
+        {
+            // setup
+            var clientOptions = new ClientOptions
+            {
+                ModelId = "tempModuleId"
+            };
+
+            // act
+            var moduleClient = ModuleClient.CreateFromConnectionString(FakeConnectionString, clientOptions);
+
+            // assert
+            Assert.IsNotNull(moduleClient);
+        }
+
+        [TestMethod]
         // Tests_SRS_DEVICECLIENT_33_003: [** It shall EnableEventReceiveAsync when called for the first time. **]**
         public async Task ModuleClient_SetReceiveCallbackAsync_SetCallback()
         {
