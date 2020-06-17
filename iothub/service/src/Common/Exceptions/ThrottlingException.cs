1fdad0df68589d79572753758079e49037161fdb
Basel Rustum <barustum@microsoft.com>
2020-06-17 14:59:16 -0700

    fix(iot-service): Expose the exception Code to the user (#1404)
    * fix(iot-service): Expose the exception Code to the user
    
    * Address PR feedback
    
    * minor change to a comment

--- iothub/service/src/Common/Exceptions/ThrottlingException.cs
+++ iothub/service/src/Common/Exceptions/ThrottlingException.cs
@@ -1,15 +1,20 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Microsoft.Azure.Devices.Common.Exceptions
 {
-    using System;
-
     [Serializable]
     public class ThrottlingException : IotHubException
     {
         public ThrottlingException(string message)
             : base(message)
+        {
+        }
+
+        public ThrottlingException(ErrorCode code, string message)
+            : base(code, message)
         {
         }
 
