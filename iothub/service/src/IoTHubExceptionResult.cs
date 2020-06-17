7febc17ab673f64f59052251165ea1a106c25165
Basel Rustum <barustum@microsoft.com>
2020-06-17 14:59:16 -0700

    fix(iot-service): Expose the exception Code to the user (#1404)
    * fix(iot-service): Expose the exception Code to the user
    
    * Address PR feedback
    
    * minor change to a comment

---
+++ iothub/service/src/IoTHubExceptionResult.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Devices
+{
+    /// <summary>
+    /// A class used as a model to deserialize error response object received from IoT Hub
+    /// </summary>
+    internal class IoTHubExceptionResult
+    {
+        [JsonProperty("Message")]
+        internal string _message;
+
+        [JsonProperty("ExceptionMessage")]
+        internal string _exceptionMessage;
+    }
+}
