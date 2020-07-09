3e23d1f92acf80e19210a5e8af13b32f2ffb549c
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-07-08 19:02:02 -0700

    Revert "refactor(samples): Update samples to as per updated command request format, fix comments"
    This reverts commit b94be0b2ebfdb19838107304423de0300d325ef1.

--- iothub/service/samples/PnpServiceSamples/TemperatureController/Program.cs
+++ iothub/service/samples/PnpServiceSamples/TemperatureController/Program.cs
@@ -1,12 +1,10 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using PnpHelpers;
 
 namespace TemperatureController
 {
@@ -14,27 +12,19 @@ namespace TemperatureController
     {
         // Get connection string and device id inputs
         private static readonly string s_hubConnectionString = Environment.GetEnvironmentVariable("IOTHUB_CONNECTION_STRING");
-
         private static readonly string s_deviceId = Environment.GetEnvironmentVariable("DEVICE_ID");
 
-        // These are values as defined in DTMI used for PnP device client sample: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples/PnpDeviceSamples/TemperatureController
+        // These are values as defined in DTMI used for device client sample with component.
         // DTDL interface used: https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/samples/TemperatureController.json
         private const string ComponentName = "thermostat1";
 
         // Writable property to update
         private const string PropertyName = "targetTemperature";
-
         private const double PropertyValue = 60;
 
-        // Command on a root interface
-        private const string RebootCommandName = "reboot";
-
-        private const int RebootDelayInSecs = 3;
-
-        // Command on a "Thermostat" component
-        private const string ReportCommandName = "getMaxMinReport";
-
-        private static readonly DateTime s_dateTime = DateTime.Now;
+        // Method on a specified component
+        private const string MethodName = "getMaxMinReport";
+        private static readonly DateTime dateTime = DateTime.Now;
 
         private static ServiceClient s_serviceClient;
         private static RegistryManager s_registryManager;
@@ -64,61 +54,38 @@ namespace TemperatureController
             s_logger.LogDebug($"Get model Id and Update Component Property");
             await GetAndUpdateTwinAsync();
 
-            s_logger.LogDebug($"Invoke a command on a Component");
-            await InvokeCommandOnComponentAsync();
-
-            s_logger.LogDebug($"Invoke a command on root interface");
-            await InvokeCommandOnRootInterfaceAsync();
+            s_logger.LogDebug($"Invoke a method on a Component");
+            await InvokeMethodAsync();
         }
 
-        private static async Task InvokeCommandOnComponentAsync()
+        private static async Task InvokeMethodAsync()
         {
-            // Create command name to invoke for component
-            string commandToInvoke = $"{ComponentName}*{ReportCommandName}";
-            var commandInvocation = new CloudToDeviceMethod(commandToInvoke) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            // Create method name to invoke for component
+            string methodToInvoke = PnpHelper.CreatePnpCommandName(MethodName, ComponentName);
+            var methodInvocation = new CloudToDeviceMethod(methodToInvoke) { ResponseTimeout = TimeSpan.FromSeconds(30) };
 
-            // Set command payload
-            string componentCommandPayload = JsonConvert.SerializeObject(s_dateTime);
-            commandInvocation.SetPayloadJson(componentCommandPayload);
+            // Set Method Payload    
+            var componentMethodPayload = PnpHelper.CreatePnpCommandRequestPayload(JsonConvert.SerializeObject(dateTime));
+            methodInvocation.SetPayloadJson(componentMethodPayload);
 
-            CloudToDeviceMethodResult result = await s_serviceClient.InvokeDeviceMethodAsync(s_deviceId, commandInvocation);
+            CloudToDeviceMethodResult result = await s_serviceClient.InvokeDeviceMethodAsync(s_deviceId, methodInvocation);
 
             if (result == null)
             {
-                throw new Exception($"Command {ReportCommandName} invocation returned null");
+                throw new Exception($"Method {MethodName} invovation returned null");
             }
 
-            s_logger.LogDebug($"Command {ReportCommandName} invocation result status is: {result.Status}");
-        }
-
-        private static async Task InvokeCommandOnRootInterfaceAsync()
-        {
-            // Create command name to invoke for component
-            string commandToInvoke = RebootCommandName;
-            var commandInvocation = new CloudToDeviceMethod(commandToInvoke) { ResponseTimeout = TimeSpan.FromSeconds(30) };
-
-            // Set command payload
-            string componentCommandPayload = JsonConvert.SerializeObject(RebootDelayInSecs);
-            commandInvocation.SetPayloadJson(componentCommandPayload);
-
-            CloudToDeviceMethodResult result = await s_serviceClient.InvokeDeviceMethodAsync(s_deviceId, commandInvocation);
-
-            if (result == null)
-            {
-                throw new Exception($"Command {ReportCommandName} invocation returned null");
-            }
-
-            s_logger.LogDebug($"Command {ReportCommandName} invocation result status is: {result.Status}");
+            s_logger.LogDebug("Method invocation result status is: " + result.Status);
         }
 
         private static async Task GetAndUpdateTwinAsync()
         {
             // Get a Twin and retrieves model Id set by Device client
             Twin twin = await s_registryManager.GetTwinAsync(s_deviceId);
-            s_logger.LogDebug($"Model Id of this Twin is: {twin.ModelId}");
+            Console.WriteLine("Model Id of this Twin is: " + twin.ModelId);
 
             // Update the twin
-            string propertyUpdate = CreatePropertyPatch(PropertyName, JsonConvert.SerializeObject(PropertyValue), ComponentName);
+            string propertyUpdate = PnpHelper.CreatePropertyPatch(PropertyName, JsonConvert.SerializeObject(PropertyValue), ComponentName);
             string twinPatch = $"{{ \"properties\": {{\"desired\": {propertyUpdate} }} }}";
 
             await s_registryManager.UpdateTwinAsync(s_deviceId, twinPatch, twin.ETag);
@@ -128,27 +95,6 @@ namespace TemperatureController
         {
             s_registryManager = RegistryManager.CreateFromConnectionString(s_hubConnectionString);
             s_serviceClient = ServiceClient.CreateFromConnectionString(s_hubConnectionString);
-        }
-
-        /* The property update patch (for a property within a component) needs to be in the following format:
-         * {
-         *  "sampleComponentName":
-         *      {
-         *          "__t": "c",
-         *          "samplePropertyName": 20
-         *      }
-         *  }
-         */
-
-        private static string CreatePropertyPatch(string propertyName, string serializedPropertyValue, string componentName)
-        {
-            return $"{{" +
-                    $"  \"{componentName}\": " +
-                    $"      {{" +
-                    $"          \"__t\": \"c\"," +
-                    $"          \"{propertyName}\": {serializedPropertyValue}" +
-                    $"      }} " +
-                    $"}}";
         }
     }
 }
--- iothub/service/samples/PnpServiceSamples/Thermostat/Program.cs
+++ iothub/service/samples/PnpServiceSamples/Thermostat/Program.cs
@@ -1,12 +1,10 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using PnpHelpers;
 
 namespace Thermostat
 {
@@ -14,21 +12,18 @@ namespace Thermostat
     {
         // Get connection string and device id inputs
         private static readonly string s_hubConnectionString = Environment.GetEnvironmentVariable("IOTHUB_CONNECTION_STRING");
-
         private static readonly string s_deviceId = Environment.GetEnvironmentVariable("DEVICE_ID");
 
-        // These are values as defined in DTMI used for PnP device client sample: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples/PnpDeviceSamples/Thermostat
+        // These are values as defined in DTMI used for PnP no Component device client sample.
         // DTDL interface used: https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/samples/Thermostat.json
 
         // Writable property to update
         private const string PropertyName = "targetTemperature";
-
         private const double PropertyValue = 60;
 
-        // Command on a given component
-        private const string CommandName = "getMaxMinReport";
-
-        private static readonly DateTime s_dateTime = DateTime.Now;
+        // Method on a given component
+        private const string MethodName = "getMaxMinReport";
+        private static readonly DateTime dateTime = DateTime.Now;
 
         private static ServiceClient s_serviceClient;
         private static RegistryManager s_registryManager;
@@ -58,36 +53,36 @@ namespace Thermostat
             s_logger.LogDebug($"Get Twin model Id and Update Twin");
             await GetAndUpdateTwinAsync();
 
-            s_logger.LogDebug($"Invoke a command");
-            await InvokeCommandAsync();
+            s_logger.LogDebug($"Invoke a method");
+            await InvokeMethodAsync();
         }
 
-        private static async Task InvokeCommandAsync()
+        private static async Task InvokeMethodAsync()
         {
-            var commandInvocation = new CloudToDeviceMethod(CommandName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            var methodInvocation = new CloudToDeviceMethod(MethodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
 
-            // Set command payload
-            string componentCommandPayload = JsonConvert.SerializeObject(s_dateTime);
-            commandInvocation.SetPayloadJson(componentCommandPayload);
+            // Set Method Payload    
+            var componentMethodPayload = PnpHelper.CreatePnpCommandRequestPayload(JsonConvert.SerializeObject(dateTime));
+            methodInvocation.SetPayloadJson(componentMethodPayload);
 
-            CloudToDeviceMethodResult result = await s_serviceClient.InvokeDeviceMethodAsync(s_deviceId, commandInvocation);
+            CloudToDeviceMethodResult result = await s_serviceClient.InvokeDeviceMethodAsync(s_deviceId, methodInvocation);
 
-            if (result == null)
+            if(result == null)
             {
-                throw new Exception($"Command {CommandName} invocation returned null");
+                throw new Exception($"Method {MethodName} invovation returned null");
             }
 
-            s_logger.LogDebug($"Command {CommandName} invocation result status is: {result.Status}");
+            s_logger.LogDebug("Method invocation result status is: " + result.Status);
         }
 
         private static async Task GetAndUpdateTwinAsync()
         {
             // Get a Twin and retrieves model Id set by Device client
             Twin twin = await s_registryManager.GetTwinAsync(s_deviceId);
-            s_logger.LogDebug($"Model Id of this Twin is: {twin.ModelId}");
+            s_logger.LogDebug("Model Id of this Twin is: " + twin.ModelId);
 
             // Update the twin
-            var twinPatch = new Twin();
+            Twin twinPatch = new Twin();
             twinPatch.Properties.Desired[PropertyName] = PropertyValue;
             await s_registryManager.UpdateTwinAsync(s_deviceId, twinPatch, twin.ETag);
         }
