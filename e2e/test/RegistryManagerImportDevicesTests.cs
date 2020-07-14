211165ffc70498ef22c7b5a75023a7570ee70cd8
Sindhu Nagesh <vinagesh@microsoft.com>
2020-07-14 13:51:52 -0700

    fix(e2e): Cancel previous jobs to avoid failures in test runs

--- e2e/test/RegistryManagerImportDevicesTests.cs
+++ e2e/test/RegistryManagerImportDevicesTests.cs
@@ -100,6 +100,23 @@ namespace Microsoft.Azure.Devices.E2ETests
                     // Concurrent jobs can be rejected, so implement a retry mechanism to handle conflicts with other tests
                     catch (JobQuotaExceededException) when (++tryCount < MaxIterationWait)
                     {
+                        // Cancel any jobs submitted by previous runs and are still running.
+                        IEnumerable<JobProperties> jobs = await registryManager.GetJobsAsync().ConfigureAwait(false);
+                        foreach (var job in jobs)
+                        {
+                            if (job.Status == JobStatus.Running)
+                            {
+                                try
+                                {
+                                    _log.WriteLine($"JobQuotaExceededException... Canceling existing running jobs.");
+                                    await registryManager.CancelJobAsync(job.JobId).ConfigureAwait(false);
+                                }
+                                catch
+                                {
+                                    // Do nothing. Best effort cancellation only.
+                                }
+                            }
+                        }
                         _log.WriteLine($"JobQuotaExceededException... waiting.");
                         await Task.Delay(s_waitDuration).ConfigureAwait(false);
                         continue;
