b347c30a3a2c2a9263fc37d13a8ee3a546c95363
timtay-microsoft <timtay@microsoft.com>
2020-08-25 12:25:46 -0700

    refactor(e2e): Address gaps in registry manager
    Bulk device and twin APIs were missing an e2e test

--- e2e/test/iothub/service/RegistryManagerE2ETests.cs
+++ e2e/test/iothub/service/RegistryManagerE2ETests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Azure.Devices.Common.Exceptions;
 using Microsoft.Azure.Devices.E2ETests.Helpers;
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -84,6 +85,62 @@ namespace Microsoft.Azure.Devices.E2ETests.Iothub.Service
                 await registryManager.RemoveDeviceAsync(deviceId).ConfigureAwait(false);
 
                 Assert.IsTrue(actual.Capabilities.IotEdge);
+            }
+        }
+
+        [LoggedTestMethod]
+        public async Task RegistryManager_BulkLifecycle()
+        {
+            int bulkCount = 50;
+            string[] deviceIds = new string[bulkCount];
+            List<Device> devices = new List<Device>();
+            for (int i = 0; i < bulkCount; i++)
+            {
+                deviceIds[i] = _devicePrefix + Guid.NewGuid();
+                devices.Add(new Device(deviceIds[i]));
+            }
+
+            var registryManager = RegistryManager.CreateFromConnectionString(Configuration.IoTHub.ConnectionString);
+
+            // Test that you can create devices in bulk
+            var bulkAddResult = await registryManager.AddDevices2Async(devices).ConfigureAwait(false);
+            Assert.IsTrue(bulkAddResult.IsSuccessful);
+
+            foreach (Device device in devices)
+            {
+                // After a bulk add, every device should be able to be retrieved
+                Assert.IsNotNull(await registryManager.GetDeviceAsync(device.Id).ConfigureAwait(false));
+            }
+
+            List<Twin> twins = new List<Twin>();
+            string expectedProperty = "someNewProperty";
+            string expectedPropertyValue = "someNewPropertyValue";
+            foreach (string deviceId in deviceIds)
+            {
+                Twin twin = await registryManager.GetTwinAsync(deviceId).ConfigureAwait(false);
+                twin.Properties.Desired[expectedProperty] = expectedPropertyValue;
+                twins.Add(twin);
+            }
+
+            // Test that you can update twins in bulk
+            await registryManager.UpdateTwins2Async(twins).ConfigureAwait(false);
+
+            foreach (string deviceId in deviceIds)
+            {
+                Twin twin = await registryManager.GetTwinAsync(deviceId).ConfigureAwait(false);
+                Assert.IsNotNull(twin.Properties.Desired[expectedProperty]);
+                Assert.AreEqual(expectedPropertyValue, (string)twin.Properties.Desired[expectedProperty]);
+            }
+
+            // Test that you can delete device identities in bulk
+            var bulkDeleteResult = await registryManager.RemoveDevices2Async(devices, true, default).ConfigureAwait(false);
+
+            Assert.IsTrue(bulkDeleteResult.IsSuccessful);
+
+            foreach (Device device in devices)
+            {
+                // After a bulk delete, every device should not be found
+                Assert.IsNull(await registryManager.GetDeviceAsync(device.Id).ConfigureAwait(false));
             }
         }
 
