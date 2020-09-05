ba15b9ea1da9f93463e0e24910f7768bf3dbe111
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-09-04 17:57:26 -0700

    fix(iot-device): Remove unreferenced IotHubNotFoundException

--- iothub/device/tests/ErrorDelegatingHandlerTests.cs
+++ iothub/device/tests/ErrorDelegatingHandlerTests.cs
@@ -27,7 +27,6 @@ namespace Microsoft.Azure.Devices.Client.Test
             typeof(MessageTooLargeException),
             typeof(DeviceMessageLockLostException),
             typeof(UnauthorizedException),
-            typeof(IotHubNotFoundException),
             typeof(DeviceNotFoundException),
             typeof(QuotaExceededException),
             typeof(IotHubException),
@@ -38,7 +37,6 @@ namespace Microsoft.Azure.Devices.Client.Test
         private static readonly Dictionary<Type, Func<Exception>> ExceptionFactory = new Dictionary<Type, Func<Exception>>
         {
             { typeof(UnauthorizedException), () => new UnauthorizedException(ErrorMessage) },
-            { typeof(IotHubNotFoundException), () => new IotHubNotFoundException(ErrorMessage) },
             { typeof(DeviceNotFoundException), () => new DeviceNotFoundException(ErrorMessage) },
             { typeof(QuotaExceededException), () => new QuotaExceededException(ErrorMessage) },
             { typeof(IotHubCommunicationException), () => new IotHubCommunicationException(ErrorMessage) },
