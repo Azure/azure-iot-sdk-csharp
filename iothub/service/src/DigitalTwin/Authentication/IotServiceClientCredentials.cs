7fd272bebe4faad00a178646326978b9027862f9
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-09-18 10:10:14 -0700

    feat(digitaltwin): Add convenience layer and helpres for digital twin APIs

---
+++ iothub/service/src/DigitalTwin/Authentication/IotServiceClientCredentials.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Common;
+using Microsoft.Rest;
+
+namespace Microsoft.Azure.Devices.Authentication
+{
+    internal abstract class IotServiceClientCredentials : ServiceClientCredentials
+    {
+        /// <summary>
+        /// Add a sas token to the outgoing http request, then send it to the next pipeline segment
+        /// </summary>
+        /// <param name="request">The request that is being sent</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>The result of calling the next pipeline to process the request</returns>
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.ThrowIfNull(nameof(HttpRequestMessage));
+
+            request.Headers.Add(HttpRequestHeader.Authorization.ToString(), GetSasToken());
+            request.Headers.Add(HttpRequestHeader.UserAgent.ToString(), Utils.GetClientVersion());
+
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+
+        /// <summary>
+        /// Return a SAS token
+        /// </summary>
+        /// <returns>A sas token</returns>
+        protected abstract string GetSasToken();
+    }
+}
---
+++ iothub/service/src/DigitalTwin/DigitalTwinClient.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Authentication;
+using Microsoft.Azure.Devices.Generated;
+using Microsoft.Azure.Devices.Generated.Models;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Devices
+{
+    /// <summary>
+    /// /// The Digital Twins Service Client contains methods to retrieve and update digital twin information, and invoke commands on a digital twin device.
+    /// </summary>
+    public class DigitalTwinClient : IDisposable
+    {
+        private readonly IotHubGatewayServiceAPIs _client;
+        private readonly DigitalTwin _protocolLayer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DigitalTwinClient"/> class.</summary>
+        /// <param name="connectionString">Your IoT hub's connection string.</param>
+        public static DigitalTwinClient CreateFromConnectionString(string connectionString)
+        {
+            connectionString.ThrowIfNullOrWhiteSpace(nameof(connectionString));
+
+            var iotHubConnectionString = IotHubConnectionString.Parse(connectionString);
+            var sharedAccessKeyCredential = new SharedAccessKeyCredentials(connectionString);
+            return new DigitalTwinClient(iotHubConnectionString.HttpsEndpoint, sharedAccessKeyCredential);
+        }
+
+        private DigitalTwinClient(Uri uri, IotServiceClientCredentials credentials)
+        {
+            _client = new IotHubGatewayServiceAPIs(uri, credentials);
+            _protocolLayer = new DigitalTwin(_client);
+        }
+
+        /// <summary>
+        /// Gets a strongly-typed digital twin asynchronously.
+        /// </summary>
+        /// <param name="digitalTwinId">The Id of the digital twin.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The application/json digital twin and the http response.</returns>
+        public async Task<HttpOperationResponse<T, DigitalTwinGetHeaders>> GetAsync<T>(string digitalTwinId, CancellationToken cancellationToken = default)
+        {
+            using HttpOperationResponse<string, DigitalTwinGetHeaders> response = await _protocolLayer.GetDigitalTwinWithHttpMessagesAsync(digitalTwinId, null, cancellationToken)
+                .ConfigureAwait(false);
+            return new HttpOperationResponse<T, DigitalTwinGetHeaders>
+            {
+                Body = typeof(T) == typeof(string) ? (T)(object)response.Body : JsonConvert.DeserializeObject<T>(response.Body),
+                Headers = response.Headers,
+                Request = response.Request,
+                Response = response.Response
+            };
+        }
+
+        /// <summary>
+        /// Updates a digital twin asynchronously.
+        /// </summary>
+        /// <param name="digitalTwinId">The Id of the digital twin.</param>
+        /// <param name="digitalTwinUpdateOperations">The application/json-patch+json operations to be performed on the specified digital twin.</param>
+        /// <param name="requestOptions">The optional settings for this request.</param>
+        /// <param name="cancellationToken">The cancellationToken.</param>
+        /// <returns>The http response.</returns>
+        public Task<HttpOperationHeaderResponse<DigitalTwinUpdateHeaders>> UpdateAsync(string digitalTwinId, string digitalTwinUpdateOperations, DigitalTwinUpdateRequestOptions requestOptions = default, CancellationToken cancellationToken = default)
+        {
+            return _protocolLayer.UpdateDigitalTwinWithHttpMessagesAsync(digitalTwinId, digitalTwinUpdateOperations, requestOptions?.IfMatch, null, cancellationToken);
+        }
+
+        /// <summary>
+        /// Invoke a command on a digital twin asynchronously.
+        /// </summary>
+        /// <param name="digitalTwinId">The Id of the digital twin.</param>
+        /// <param name="commandName">The command to be invoked.</param>
+        /// <param name="payload">The command payload.</param>
+        /// <param name="requestOptions">The optional settings for this request.</param>
+        /// <param name="cancellationToken">The cancellationToken.</param>
+        /// <returns>The application/json command invocation response and the http response. </returns>
+        public Task<HttpOperationResponse<string, DigitalTwinInvokeCommandHeaders>> InvokeCommandAsync(string digitalTwinId, string commandName, string payload, DigitalTwinInvokeCommandRequestOptions requestOptions = default, CancellationToken cancellationToken = default)
+        {
+            return _protocolLayer.InvokeRootLevelCommandWithHttpMessagesAsync(digitalTwinId, commandName, payload, requestOptions?.ConnectTimeoutInSeconds, requestOptions?.ResponseTimeoutInSeconds, null, cancellationToken);
+        }
+
+        /// <summary>
+        /// Invoke a command on a digital twin asynchronously.
+        /// </summary>
+        /// <param name="digitalTwinId">The Id of the digital twin.</param>
+        /// <param name="componentName">The component name under which the command is defined.</param>
+        /// <param name="commandName">The command to be invoked.</param>
+        /// <param name="payload">The command payload.</param>
+        /// <param name="requestOptions">The optional settings for this request.</param>
+        /// <param name="cancellationToken">The cancellationToken.</param>
+        /// <returns>The application/json command invocation response and the http response. </returns>
+        public Task<HttpOperationResponse<string, DigitalTwinInvokeCommandHeaders>> InvokeComponentCommandAsync(string digitalTwinId, string componentName, string commandName, string payload, DigitalTwinInvokeCommandRequestOptions requestOptions = default, CancellationToken cancellationToken = default)
+        {
+            return _protocolLayer.InvokeComponentCommandWithHttpMessagesAsync(digitalTwinId, componentName, commandName, payload, requestOptions?.ConnectTimeoutInSeconds, requestOptions?.ResponseTimeoutInSeconds, null, cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            _client?.Dispose();
+        }
+    }
+}
