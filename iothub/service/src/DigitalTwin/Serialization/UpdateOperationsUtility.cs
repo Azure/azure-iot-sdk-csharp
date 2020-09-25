e32633b6730ba89400b9a8b19c6eb0bbcf961f81
Abhipsa Misra <abhipsa.misra@microsoft.com>
2020-09-25 14:27:48 -0700

    refactor(iot-service, readme): Update digital twin update operations utility, add readme demonstrating DelegatingHandler usage

--- iothub/service/src/DigitalTwin/Serialization/UpdateOperationsUtility.cs
+++ iothub/service/src/DigitalTwin/Serialization/UpdateOperationsUtility.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using Microsoft.Azure.Devices.Extensions;
 using Newtonsoft.Json;
 
 namespace Microsoft.Azure.Devices.Serialization
@@ -21,11 +22,34 @@ namespace Microsoft.Azure.Devices.Serialization
         private readonly List<Dictionary<string, object>> _ops = new List<Dictionary<string, object>>();
 
         /// <summary>
-        /// Include an add operation.
+        /// Include an add property operation.
+        /// Learn more about managing digital twins here <see href="https://docs.microsoft.com/en-us/azure/iot-pnp/howto-manage-digital-twin"/>.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The root-level property patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "add",
+        ///         "path": "/samplePropertyName",
+        ///         "value": 20
+        ///     }
+        /// ]
+        /// </para>
+        /// <para>
+        /// The component-level property patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "add",
+        ///         "path": "/sampleComponentName/samplePropertyName",
+        ///         "value": 20
+        ///     }
+        /// ]
+        /// </para>
+        /// </remarks>
         /// <param name="path">The path to the property to be added.</param>
         /// <param name="value">The value to update to.</param>
-        public void AppendAddOp(string path, object value)
+        public void AppendAddPropertyOp(string path, object value)
         {
             var op = new Dictionary<string, object>
             {
@@ -37,11 +61,34 @@ namespace Microsoft.Azure.Devices.Serialization
         }
 
         /// <summary>
-        /// Include a replace operation.
+        /// Include a replace property operation.
+        /// Learn more about managing digital twins here <see href="https://docs.microsoft.com/en-us/azure/iot-pnp/howto-manage-digital-twin"/>.
         /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The root-level property patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "replace",
+        ///         "path": "/samplePropertyName",
+        ///         "value": 20
+        ///     }
+        /// ]
+        /// </para>
+        /// <para>
+        /// The component-level property patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "replace",
+        ///         "path": "/sampleComponentName/samplePropertyName",
+        ///         "value": 20
+        ///     }
+        /// ]
+        /// </para>
+        /// </remarks>
         /// <param name="path">The path to the property to be updated.</param>
         /// <param name="value">The value to update to.</param>
-        public void AppendReplaceOp(string path, object value)
+        public void AppendReplacePropertyOp(string path, object value)
         {
             var op = new Dictionary<string, object>
             {
@@ -54,14 +101,118 @@ namespace Microsoft.Azure.Devices.Serialization
 
         /// <summary>
         /// Include a remove operation.
+        /// Learn more about managing digital twins here <see href="https://docs.microsoft.com/en-us/azure/iot-pnp/howto-manage-digital-twin"/>.
         /// </summary>
-        /// <param name="path">The path to the property to be added.</param>
+        /// <remarks>
+        /// <para>
+        /// The patch for removing a root-level property should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "remove",
+        ///         "path": "/samplePropertyName"
+        ///     }
+        /// ]
+        /// </para>
+        /// <para>
+        /// The patch for removing a component-level property should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "remove",
+        ///         "path": "/sampleComponentName/samplePropertyName"
+        ///     }
+        /// ]
+        /// <para>
+        /// The patch for removing a component should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "remove",
+        ///         "path": "/sampleComponentName"
+        ///     }
+        /// ]
+        /// </para>
+        /// </para>
+        /// </remarks>
+        /// <param name="path">The path to the property to be removed.</param>
         public void AppendRemoveOp(string path)
         {
             var op = new Dictionary<string, object>
             {
                 { Op, Remove },
                 { Path, path },
+            };
+            _ops.Add(op);
+        }
+
+        /// <summary>
+        /// Include an add component operation.
+        /// Learn more about managing digital twins here <see href="https://docs.microsoft.com/en-us/azure/iot-pnp/howto-manage-digital-twin"/>.
+        /// </summary>
+        /// <remarks>
+        /// This utility appends the "$metadata" identifier to the property values,
+        /// which helps the service identify this as a component update, and not a root-level property update.
+        /// <para>
+        /// The component patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "add",
+        ///         "path": "/sampleComponentName",
+        ///         "value": {
+        ///             "samplePropertyName": 20,
+        ///             "$metadata": {}
+        ///         }
+        ///     }
+        /// ]
+        /// </para>
+        /// </remarks>
+        /// <param name="path">The path to the component to be added.</param>
+        /// <param name="propertyValues">The dictionary of property key values pairs to update to.</param>
+        public void AppendAddComponentOp(string path, Dictionary<string, object> propertyValues)
+        {
+            propertyValues.ThrowIfNull(nameof(propertyValues));
+            propertyValues.AddComponentUpdateIdentifier();
+
+            var op = new Dictionary<string, object>
+            {
+                { Op, Add },
+                { Path, path },
+                { Value, propertyValues },
+            };
+            _ops.Add(op);
+        }
+
+        /// <summary>
+        /// Include a replace component operation.
+        /// Learn more about managing digital twins here <see href="https://docs.microsoft.com/en-us/azure/iot-pnp/howto-manage-digital-twin"/>.
+        /// </summary>
+        /// <remarks>
+        /// This utility appends the "$metadata" identifier to the property values,
+        /// which helps the service identify this as a component update, and not a root-level property update.
+        /// <para>
+        /// The component patch should be in the following format:
+        /// [
+        ///     {
+        ///         "op": "replace",
+        ///         "path": "/sampleComponentName",
+        ///         "value": {
+        ///             "samplePropertyName": 20,
+        ///             "$metadata": {}
+        ///         }
+        ///     }
+        /// ]
+        /// </para>
+        /// </remarks>
+        /// <param name="path">The path to the component to be updated.</param>
+        /// <param name="propertyValues">The dictionary of property key values pairs to update to.</param>
+        public void AppendReplaceComponentOp(string path, Dictionary<string, object> propertyValues)
+        {
+            propertyValues.ThrowIfNull(nameof(propertyValues));
+            propertyValues.AddComponentUpdateIdentifier();
+
+            var op = new Dictionary<string, object>
+            {
+                { Op, Replace },
+                { Path, path },
+                { Value, propertyValues },
             };
             _ops.Add(op);
         }
