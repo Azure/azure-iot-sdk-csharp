b3857a9c1b19710ac57f672da43a89d821244bfe
David R. Williamson <drwill@microsoft.com>
2020-12-01 11:34:11 -0800

    Update DPS device symmetric key sample, and fix warnings in other sln samples

---
+++ provisioning/Samples/device/SymmetricKeySample/EnrollmentType.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
+{
+    /// <summary>
+    /// The type of enrollment for a device in the provisioning service.
+    /// </summary>
+    public enum EnrollmentType
+    {
+        /// <summary>
+        ///  Enrollment for a single device.
+        /// </summary>
+        Individual,
+
+        /// <summary>
+        /// Enrollment for a group of devices.
+        /// </summary>
+        Group,
+    }
+}
