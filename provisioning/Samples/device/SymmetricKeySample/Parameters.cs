7e854fef261cfd5c30cec24eb2ed55d803823b80
David R. Williamson <drwill@microsoft.com>
2020-12-01 17:31:51 -0800

    Update device DPS x509 sample

--- provisioning/Samples/device/SymmetricKeySample/Parameters.cs
+++ provisioning/Samples/device/SymmetricKeySample/Parameters.cs
@@ -1,4 +1,7 @@
-﻿using CommandLine;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
 using Microsoft.Azure.Devices.Client;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
