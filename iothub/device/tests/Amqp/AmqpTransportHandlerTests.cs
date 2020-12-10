a7babeefb2f9537746db2204f765ddb83758684a
Sindhu Nagesh <vinagesh@microsoft.com>
2020-12-10 09:54:22 -0800

    Re-enabling test that was disabled and passes now

--- iothub/device/tests/Amqp/AmqpTransportHandlerTests.cs
+++ iothub/device/tests/Amqp/AmqpTransportHandlerTests.cs
@@ -1,4 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information
 
 using System;
@@ -15,7 +17,7 @@ namespace Microsoft.Azure.Devices.Client.Test.Transport
     [TestCategory("Unit")]
     public class AmqpTransportHandlerTests
     {
-        private const string DumpyConnectionString = "HostName=Do.Not.Exist;SharedAccessKeyName=AllAccessKey;DeviceId=FakeDevice;SharedAccessKey=dGVzdFN0cmluZzE=";
+        private const string TestConnectionString = "HostName=Do.Not.Exist;SharedAccessKeyName=AllAccessKey;DeviceId=FakeDevice;SharedAccessKey=dGVzdFN0cmluZzE=";
 
         [TestMethod]
         public async Task AmqpTransportHandlerOpenAsyncTokenCancellationRequested()
@@ -59,31 +61,30 @@ namespace Microsoft.Azure.Devices.Client.Test.Transport
             await TestOperationCanceledByToken(token => CreateFromConnectionString().RejectAsync(Guid.NewGuid().ToString(), token)).ConfigureAwait(false);
         }
 
-        [Ignore] // TODO #584 Uncomment later once we support throwing exceptions on TransportSettingsChange
         [TestMethod]
         public void AmqpTransportHandler_RejectAmqpSettingsChange()
         {
-            //var amqpTransportHandler1 = new AmqpTransportHandler(new PipelineContext(), IotHubConnectionString.Parse(DumpyConnectionString), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only, 60, new AmqpConnectionPoolSettings()
-            //{
-            //    Pooling = true,
-            //    MaxPoolSize = 10,
-            //    ConnectionIdleTimeout = TimeSpan.FromMinutes(1)
-            //}));
-            //
-            //try
-            //{
-            //    // Try to create a set AmqpTransportHandler with different connection pool settings.
-            //    var amqpTransportHandler2 = new AmqpTransportHandler(new PipelineContext(), IotHubConnectionString.Parse(DumpyConnectionString), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only, 60, new AmqpConnectionPoolSettings()
-            //    {
-            //        Pooling = true,
-            //        MaxPoolSize = 7, // different pool size
-            //        ConnectionIdleTimeout = TimeSpan.FromMinutes(1)
-            //    }));
-            //}
-            //catch (ArgumentException ae)
-            //{
-            //    Assert.IsTrue(ae.Message.Contains("AmqpTransportSettings cannot be modified from the initial settings."), "Did not return the correct error message");
-            //}
+            var amqpTransportHandler1 = new AmqpTransportHandler(new PipelineContext(), new IotHubConnectionString(IotHubConnectionStringBuilder.Create(TestConnectionString)), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only, 60, new AmqpConnectionPoolSettings()
+            {
+                Pooling = true,
+                MaxPoolSize = 10,
+                ConnectionIdleTimeout = TimeSpan.FromMinutes(1)
+            }));
+
+            try
+            {
+                // Try to create a set AmqpTransportHandler with different connection pool settings.
+                var amqpTransportHandler2 = new AmqpTransportHandler(new PipelineContext(), new IotHubConnectionString(IotHubConnectionStringBuilder.Create(TestConnectionString)), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only, 60, new AmqpConnectionPoolSettings()
+                {
+                    Pooling = true,
+                    MaxPoolSize = 7, // different pool size
+                    ConnectionIdleTimeout = TimeSpan.FromMinutes(1)
+                }));
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.IsTrue(ae.Message.Contains("AmqpTransportSettings cannot be modified from the initial settings."), "Did not return the correct error message");
+            }
         }
 
         private async Task TestOperationCanceledByToken(Func<CancellationToken, Task> asyncMethod)
@@ -103,7 +104,7 @@ namespace Microsoft.Azure.Devices.Client.Test.Transport
         {
             return new AmqpTransportHandler(
                 new PipelineContext(),
-                IotHubConnectionStringExtensions.Parse(DumpyConnectionString),
+                IotHubConnectionStringExtensions.Parse(TestConnectionString),
                 new AmqpTransportSettings(TransportType.Amqp_Tcp_Only));
         }
     }
