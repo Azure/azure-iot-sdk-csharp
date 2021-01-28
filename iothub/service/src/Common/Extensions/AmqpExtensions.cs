35121e1f109241a01c7c52439b552a09130af468
Abhipsa Misra <abhipsa.misra@microsoft.com>
2021-01-28 12:59:15 -0800

    fix(iot-service): Add xml comments to public types and methods

--- iothub/service/src/Common/Extensions/AmqpExtensions.cs
+++ iothub/service/src/Common/Extensions/AmqpExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Threading.Tasks;
+using Microsoft.Azure.Amqp;
+
 namespace Microsoft.Azure.Devices.Common.Extensions
 {
-    using System.Threading.Tasks;
-    using Microsoft.Azure.Amqp;
-
     /// <summary>
     /// Contains extension methods for amqp
     /// </summary>
@@ -18,8 +18,7 @@ namespace Microsoft.Azure.Devices.Common.Extensions
         /// <returns></returns>
         internal static async Task<ReceivingAmqpLink> GetReceivingLinkAsync(this FaultTolerantAmqpObject<ReceivingAmqpLink> faultTolerantReceivingLink)
         {
-            ReceivingAmqpLink receivingLink;
-            if (!faultTolerantReceivingLink.TryGetOpenedObject(out receivingLink))
+            if (!faultTolerantReceivingLink.TryGetOpenedObject(out ReceivingAmqpLink receivingLink))
             {
                 receivingLink = await faultTolerantReceivingLink.GetOrCreateAsync(IotHubConnection.DefaultOpenTimeout).ConfigureAwait(false);
             }
