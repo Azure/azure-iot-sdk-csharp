947f77a6c731a2b3b770b017f64d142ac8188bd3
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-02-12 10:34:44 +0100

    Make DeviceClient extensible

--- iothub/device/src/DeviceClient.cs
+++ iothub/device/src/DeviceClient.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Azure.Devices.Client
     /// Contains methods that a device can use to send messages to and receive from the service.
     /// </summary>
     /// <threadsafety static="true" instance="true" />
-    public sealed class DeviceClient : IDisposable
+    public class DeviceClient : IDisposable
     {
         /// <summary>
         /// Default operation timeout.
