8256ab1d2fe6c2dd54ff84c0b1ab5f8bd092a9e0
Abhipsa Misra <abhipsa.misra@microsoft.com>
2021-03-05 18:12:24 -0800

    some renaming

--- common/src/HttpContentExtensions.cs
+++ common/src/HttpContentExtensions.cs
@@ -3,10 +3,8 @@
 
 using System.IO;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Microsoft.Azure.Devices.Shared
 {
--- common/src/StreamExtensions.cs
+++ common/src/StreamExtensions.cs
@@ -2,15 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
-using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Microsoft.Azure.Devices.Shared
 {
-    internal static class HttpMessageHelper
+    internal static class StreamExtensions
     {
         internal static async Task WriteToStreamAsync(this Stream stream, byte[] requestBytes, CancellationToken cancellationToken)
         {
