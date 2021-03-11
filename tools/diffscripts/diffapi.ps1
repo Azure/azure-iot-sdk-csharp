f4a04eb349b31ae9d840b39d5e59dfed18bf2d7e
James Davis ⛺️🏔 <jamdavi@microsoft.com>
2021-03-11 09:04:32 -0500

    Adding release tools for diff

---
+++ tools/diffscripts/diffapi.ps1
@@ -0,0 +1,68 @@
+$reporoot = (gi $pwd).Parent.Parent.FullName
+$baseroot = (gi $reporoot).Parent.FullName
+$asmtool = (Join-Path -Path $baseroot -Child "\arcade\artifacts\bin\Microsoft.DotNet.AsmDiff\Debug\netcoreapp3.1\") + "Microsoft.DotNet.AsmDiff.exe"
+$internalroot = Join-Path -Path $baseroot -Child "\iot-sdks-internals"
+$masterdirectory = Join-Path -Path $internalroot -Child "\sdk_design_docs\CSharp\master"
+$previewdirectory = Join-Path -Path $internalroot -Child "\sdk_design_docs\CSharp\preview"
+
+if ((Test-Path $asmtool) -ne $TRUE) {
+    Write-Host "Please get the AsmDiff tool from the dotnet arcade https://github.com/dotnet/arcade/tree/master/src/Microsoft.DotNet.AsmDiff"
+}
+
+$filenames = @(
+    "Microsoft.Azure.Devices.Shared",
+    "Microsoft.Azure.Devices.Client",
+    "Microsoft.Azure.Devices",
+    "Microsoft.Azure.Devices.Provisioning.Client",
+    "Microsoft.Azure.Devices.Provisioning.Service",
+    "Microsoft.Azure.Devices.Provisioning.Transport.Amqp",
+    "Microsoft.Azure.Devices.Provisioning.Transport.Mqtt",
+    "Microsoft.Azure.Devices.Provisioning.Transport.Http",
+    "Microsoft.Azure.Devices.Provisioning.Security.Tpm"
+)
+
+$oldsetNames = @(
+    ((Join-Path -Path $reporoot -ChildPath "\shared\src\bin\Release\netstandard2.1\")+($filenames[0]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\iothub\device\src\bin\Release\netstandard2.1\")+($filenames[1]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\iothub\service\src\bin\Release\netstandard2.1\")+($filenames[2]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\provisioning\device\src\bin\Release\netstandard2.1\")+($filenames[3]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\provisioning\service\src\bin\Release\netstandard2.1\")+($filenames[4]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\provisioning\transport\amqp\src\bin\Release\netstandard2.1\")+($filenames[5]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\provisioning\transport\mqtt\src\bin\Release\netstandard2.1\")+($filenames[6]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\provisioning\transport\http\src\bin\Release\netstandard2.1\")+($filenames[7]+".dll")),
+    ((Join-Path -Path $reporoot -ChildPath "\security\tpm\src\bin\Release\netstandard2.1\")+($filenames[8]+".dll"))
+)
+
+$markdownoutput = @(
+    ($masterdirectory+ "\" + ($filenames[0]+".md")),
+    ($masterdirectory+ "\" + ($filenames[1]+".md")),
+    ($masterdirectory+ "\" + ($filenames[2]+".md")),
+    ($masterdirectory+ "\" + ($filenames[3]+".md")),
+    ($masterdirectory+ "\" + ($filenames[4]+".md")),
+    ($masterdirectory+ "\" + ($filenames[5]+".md")),
+    ($masterdirectory+ "\" + ($filenames[6]+".md")),
+    ($masterdirectory+ "\" + ($filenames[7]+".md")),
+    ($masterdirectory+ "\" + ($filenames[8]+".md"))
+)
+
+for($i = 0; $i -lt $filenames.length; $i++) { 
+    if (Test-Path $oldsetNames[$i]) {
+        Write-Host "Getting original header for" $markdownoutput[$i]
+        $originalheader = Get-Content $markdownoutput[$i] | select -First 5
+        Write-Host "Creating markdown for" $oldsetNames[$i]
+        $switches = "-os", $oldsetNames[$i], "-w", "Markdown", "-o", $markdownoutput[$i]
+        & $asmtool $switches
+        Write-Host "Replacing header for new" $markdownoutput[$i]
+        $newbody = Get-Content $markdownoutput[$i] | Select-Object -Skip 5
+        .{
+            $originalheader
+            $newbody
+        } | Set-Content $markdownoutput[$i]
+
+        Write-Host 'Removing ``` headers for' $markdownoutput[$i]
+        $textToParse = Get-Content $markdownoutput[$i]
+        $textToParse = $textToParse -Replace '(?gm)```\s\s##.*\s\s``` diff\s', ''
+    } else {
+        Write-Host $oldsetNames[$i] "Does not exist!"
+    }
+}
