7abf130acb6f5325cf78fe0c85ac6cc411f4730d
James Davis ⛺️🏔 <jamdavi@microsoft.com>
2021-03-11 15:11:30 -0500

    Fixups to the diff tool

--- tools/diffscripts/diffapi.ps1
+++ tools/diffscripts/diffapi.ps1
@@ -10,12 +10,23 @@ if ((Test-Path $asmtool) -ne $TRUE) {
     Write-Host "For example, if this SDK is cloned to c:\repos you should clone https://github.com/dotnet/arcade.git to the c:\repos folder."
     Write-Host ""
     Write-Host "Once you clone the folder run dotnet build in the src/Microsoft.DotNet.AsmDiff folder."
+    exit 1
 }
 
-git describe --tags --abbrev=0 | tee -Variable lasttag
+# Get the last tag from the git repo and do the comparison
+$lasttag = git describe --tags --abbrev=0  | tee -Variable lasttag
+& git log --stat "$lasttag..HEAD" --output releaselog_detailed.txt
+& git log --oneline "$lasttag..HEAD" --output releaselog_short.txt
 
-& git log --stat "$lasttag..HEAD" | Out-File releaselog_detailed.txt
-& git log --oneline "$lasttag..HEAD" | Out-File releaselog_short.txt
+Write-Host ""
+Write-Host "Generated release log from tag $lasttag to the current HEAD."
+Write-Host "The detailed log can be found by editing: releaselog_detailed.txt"
+Write-Host "The short log can be found by editing: releaselog_short.txt"
+Write-Host ""
+Write-Host "NOTE: If there is a tag that you want to compare to that is earlier than the most recent tag you can run:"
+Write-Host "git log --stat <tagversion>..HEAD --output releaselog_detailed.txt"
+Write-Host "git log --oneline $lasttag..HEAD --output releaselog_short.txt"
+Write-Host ""
 
 # Hardcoded list of assembly names
 $filenames = @(
@@ -30,6 +41,7 @@ $filenames = @(
     "Microsoft.Azure.Devices.Provisioning.Security.Tpm"
 )
 
+# All of the files from the build
 $oldsetNames = @(
     ((Join-Path -Path $reporoot -ChildPath "\shared\src\bin\Release\netstandard2.1\")+($filenames[0]+".dll")),
     ((Join-Path -Path $reporoot -ChildPath "\iothub\device\src\bin\Release\netstandard2.1\")+($filenames[1]+".dll")),
@@ -42,6 +54,7 @@ $oldsetNames = @(
     ((Join-Path -Path $reporoot -ChildPath "\security\tpm\src\bin\Release\netstandard2.1\")+($filenames[8]+".dll"))
 )
 
+# Create a list of the markdown files so we can compare them to the API doc directory
 $markdownoutput = @(
     ($masterdirectory+ "\" + ($filenames[0]+".md")),
     ($masterdirectory+ "\" + ($filenames[1]+".md")),
@@ -54,6 +67,7 @@ $markdownoutput = @(
     ($masterdirectory+ "\" + ($filenames[8]+".md"))
 )
 
+# Create a list of the markdown files so we can compare them to the API doc directory
 for($i = 0; $i -lt $filenames.length; $i++) { 
     if (Test-Path $oldsetNames[$i]) {
         Write-Host "Getting original header for" $markdownoutput[$i]
@@ -75,12 +89,38 @@ for($i = 0; $i -lt $filenames.length; $i++) {
     }
 }
 
+Write-Host ""
+
+# Save old directory so we can return here.
 $olddir = $pwd
+
+# Nav to the docs directory to run the comparison
 Set-Location -Path $masterdirectory
-& git diff --numstat | tee -Variable diffoutput
+$diffoutput = git diff --numstat
 
 if ($diffoutput -eq $null) {
-    Write-Host "There were no changes in the API Surface related to the comparison of the ASM Tool. Check to make sure there are no changes to any dependencies and decide if you want to make a dot release."
+    Write-Host "There were no changes in the API Surface related to the comparison of the ASM Tool. Check the solutions to make sure there were not other changes that would affect the release and require a major version update."
+} else {
+    Write-Host "Changes have been detected. Verify each file listed below to be sure." 
+    $added = 0
+    $deleted = 0
+    foreach ($line in $diffoutput) {
+        $_ = $line -match "(?<added>\d)\s(?<deleted>\d)\s(?<file>.*)" 
+        $fileadd = $Matches.added
+        $filedel = $Matches.deleted
+        $file = $Matches.file
+        $added = $added + $fileadd
+        $deleted = $deleted + $filedel
+        Write-Host "There have been $filedel deletions and $fileadd additions to the $file file. We will need to evaluate the changes." 
+    }
+
+    if ($deleted -gt 0) {
+        Write-Host "There have been $deleted deletions from the SDK suface. We will need to make a minor version change." 
+    } else {
+        if($added -gt 0) {
+        Write-Host "There have been $added additions to the SDK suface. We will need to make a `"dot version`" change." 
+        }
+    }
 }
 
 Set-Location -Path $olddir
