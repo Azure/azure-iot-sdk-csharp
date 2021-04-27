cbea693ce9ce61b0b140b7626c064ce84ab2113d
David R. Williamson <drwill@microsoft.com>
2021-04-27 15:25:19 -0700

    Updating device SDK versions

--- iot-hub/Samples/device/MessageReceiveSample/MessageReceiveSample.cs
+++ iot-hub/Samples/device/MessageReceiveSample/MessageReceiveSample.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Client.Samples
@@ -19,21 +20,33 @@ namespace Microsoft.Azure.Devices.Client.Samples
     /// </summary>
     public class MessageReceiveSample
     {
-        private static readonly TimeSpan s_sleepDuration = TimeSpan.FromSeconds(5);
-        private static readonly TimeSpan s_receiveTimeout = TimeSpan.FromSeconds(10);
+        private static readonly TimeSpan s_sleepDuration = TimeSpan.FromSeconds(3);
+        private readonly TimeSpan? _maxRunTime;
         private readonly DeviceClient _deviceClient;
 
-        public MessageReceiveSample(DeviceClient deviceClient)
+        public MessageReceiveSample(DeviceClient deviceClient, TimeSpan? maxRunTime)
         {
             _deviceClient = deviceClient ?? throw new ArgumentNullException(nameof(deviceClient));
+            _maxRunTime = maxRunTime;
         }
 
         public async Task RunSampleAsync()
         {
+            using var cts = _maxRunTime.HasValue
+                ? new CancellationTokenSource(_maxRunTime.Value)
+                : new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Sample execution cancellation requested; will exit.");
+            };
+            Console.WriteLine($"{DateTime.Now}> Press Control+C at any time to quit the sample.");
+
             // First receive C2D messages using the polling ReceiveAsync().
-            Console.WriteLine($"\n{DateTime.Now}> Device waiting for C2D messages from the hub for {s_receiveTimeout}...");
-            Console.WriteLine($"{DateTime.Now}> Use the Azure Portal IoT Hub blade or Azure IoT Explorer to send a message to this device.");
-            await ReceiveC2dMessagesPollingAndComplete(s_receiveTimeout);
+            Console.WriteLine($"\n{DateTime.Now}> Device waiting for C2D messages from the hub...");
+            Console.WriteLine($"{DateTime.Now}> Use the Azure Portal IoT hub blade or Azure IoT Explorer to send a message to this device.");
+            await ReceiveC2dMessagesPollingAndComplete(cts.Token);
 
             // Now subscribe to receive C2D messages through a callback.
             await _deviceClient.SetReceiveMessageHandlerAsync(OnC2dMessageReceived, _deviceClient);
@@ -41,45 +54,53 @@ namespace Microsoft.Azure.Devices.Client.Samples
 
             // Now wait to receive C2D messages through the callback.
             // Since you are subscribed to receive messages through the callback, any call to the polling ReceiveAsync() API will now return "null".
-            Console.WriteLine($"\n{DateTime.Now}> Device waiting for C2D messages from the hub for {s_receiveTimeout}...");
+            Console.WriteLine($"\n{DateTime.Now}> Device waiting for C2D messages from the hub...");
             Console.WriteLine($"{DateTime.Now}> Use the Azure Portal IoT Hub blade or Azure IoT Explorer to send a message to this device.");
-            await Task.Delay(s_receiveTimeout);
+
+            try
+            {
+                await Task.Delay(-1, cts.Token);
+            }
+            catch (TaskCanceledException)
+            {
+                // Done running.
+            }
 
             // Now unsubscibe from receiving the callback.
-            await _deviceClient.SetReceiveMessageHandlerAsync(null, _deviceClient);
+            await _deviceClient.SetReceiveMessageHandlerAsync(null, null);
         }
 
-        private async Task ReceiveC2dMessagesPollingAndComplete(TimeSpan timeout)
+        private async Task ReceiveC2dMessagesPollingAndComplete(CancellationToken ct)
         {
-            var sw = new Stopwatch();
-            sw.Start();
+            Console.WriteLine($"{DateTime.Now}> Trying to receive C2D messages by polling using the ReceiveAsync() method. Press 'n' to move to the next phase.");
 
-            Console.WriteLine($"{DateTime.Now}> Receiving C2D messages on the polling ReceiveAsync().");
-            while (sw.Elapsed < timeout)
+            while (!ct.IsCancellationRequested)
             {
-                using Message receivedMessage = await _deviceClient.ReceiveAsync(timeout);
+                if (Console.KeyAvailable
+                    && ConsoleKey.N == Console.ReadKey().Key)
+                {
+                    Console.WriteLine($"\n{DateTime.Now}> Ending message polling.");
+                    break;
+                }
 
+                using Message receivedMessage = await _deviceClient.ReceiveAsync(s_sleepDuration);
                 if (receivedMessage == null)
                 {
-                    Console.WriteLine($"{DateTime.Now}> Polling ReceiveAsync() - no message received.");
-                    await Task.Delay(s_sleepDuration);
                     continue;
                 }
 
-                Console.WriteLine($"{DateTime.Now}> Polling ReceiveAsync() - received message with Id={receivedMessage.MessageId}");
-                ProcessReceivedMessage(receivedMessage);
+                Console.WriteLine($"{DateTime.Now}> Polling using ReceiveAsync() - received message with Id={receivedMessage.MessageId}");
+                PrintMessage(receivedMessage);
 
                 await _deviceClient.CompleteAsync(receivedMessage);
                 Console.WriteLine($"{DateTime.Now}> Completed C2D message with Id={receivedMessage.MessageId}.");
             }
-
-            sw.Stop();
         }
 
         private async Task OnC2dMessageReceived(Message receivedMessage, object _)
         {
             Console.WriteLine($"{DateTime.Now}> C2D message callback - message received with Id={receivedMessage.MessageId}.");
-            ProcessReceivedMessage(receivedMessage);
+            PrintMessage(receivedMessage);
 
             await _deviceClient.CompleteAsync(receivedMessage);
             Console.WriteLine($"{DateTime.Now}> Completed C2D message with Id={receivedMessage.MessageId}.");
@@ -87,7 +108,7 @@ namespace Microsoft.Azure.Devices.Client.Samples
             receivedMessage.Dispose();
         }
 
-        private void ProcessReceivedMessage(Message receivedMessage)
+        private void PrintMessage(Message receivedMessage)
         {
             string messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
             var formattedMessage = new StringBuilder($"Received message: [{messageData}]\n");
@@ -97,8 +118,8 @@ namespace Microsoft.Azure.Devices.Client.Samples
             {
                 formattedMessage.AppendLine($"\tProperty: key={prop.Key}, value={prop.Value}");
             }
-            // System properties can be accessed using their respective accessors.
-            formattedMessage.AppendLine($"\tMessageId: {receivedMessage.MessageId}");
+            // System properties can be accessed using their respective accessors, e.g. DeliveryCount.
+            formattedMessage.AppendLine($"\tDelivery count: {receivedMessage.DeliveryCount}");
 
             Console.WriteLine($"{DateTime.Now}> {formattedMessage}");
         }
