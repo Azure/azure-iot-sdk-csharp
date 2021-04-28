b33de5f9cfdac86dbcb2df2770b3df615c685ce6
Abhipsa Misra <abhipsa.misra@microsoft.com>
2021-04-28 15:51:53 -0700

    update temperatureUpdate class

--- iothub/device/samples/PnpDeviceSamples/TemperatureController/UpdateTemperatureResponse.cs
+++ iothub/device/samples/PnpDeviceSamples/TemperatureController/UpdateTemperatureResponse.cs
@@ -1,16 +1,23 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace Microsoft.Azure.Devices.Client.Samples
 {
     internal class UpdateTemperatureResponse
     {
+        // Multiple json property annotations are added to demonstrate the difference in
+        // API usgage for the different serializers.
         [JsonProperty("targetTemp")]
+        [JsonPropertyName("targetTemp")]
         public double TargetTemperature { get; set; }
 
+        // Multiple json property annotations are added to demonstrate the difference in
+        // API usgage for the different serializers.
         [JsonProperty("status")]
+        [JsonPropertyName("status")]
         public int Status { get; set; }
     }
 }
