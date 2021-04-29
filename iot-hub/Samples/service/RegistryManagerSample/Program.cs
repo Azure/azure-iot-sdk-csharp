32c03a702fef3c6ce17bf7f90a13dcb847d2b61f
David R. Williamson <drwill@microsoft.com>
2021-04-28 17:29:49 -0700

    Update service SDK versions

--- iot-hub/Samples/service/RegistryManagerSample/Program.cs
+++ iot-hub/Samples/service/RegistryManagerSample/Program.cs
@@ -1,37 +1,40 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using CommandLine;
 using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Samples
 {
+    /// <summary>
+    /// A sample to illustrate using registry manager.
+    /// </summary>
+    /// <param name="args">
+    /// Run with `--help` to see a list of required and optional parameters.
+    /// </param>
     public class Program
     {
-        // The IoT Hub connection string. This is available under the "Shared access policies" in the Azure portal.
-
-        // For this sample either:
-        // - pass this value as a command-prompt argument
-        // - set the IOTHUB_CONNECTION_STRING environment variable 
-        // - create a launchSettings.json (see launchSettings.json.template) containing the variable
-
-        private static string s_connectionString = Environment.GetEnvironmentVariable("IOTHUB_CONNECTION_STRING");
-
-        public static async Task<int> Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            if (string.IsNullOrEmpty(s_connectionString) && args.Length > 0)
-            {
-                s_connectionString = args[0];
-            }
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(
+                    parsedParams =>
+                    {
+                        parameters = parsedParams;
+                    })
+                .WithNotParsed(
+                    errors =>
+                    {
+                        Environment.Exit(1);
+                    });
 
-            using RegistryManager registryManager = RegistryManager.CreateFromConnectionString(s_connectionString);
+            var sample = new RegistryManagerSample(parameters);
+            await sample.RunSampleAsync();
 
-            var sample = new RegistryManagerSample(registryManager);
-
-            await sample.RunSampleAsync().ConfigureAwait(false);
-
-            Console.WriteLine("Done.");
-            return 0;
+            Console.WriteLine("The sample has completed.");
         }
     }
 }
