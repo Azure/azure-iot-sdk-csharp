4a5cf5aa138c92e331d0e3c476fe7a9bc0566ada
Sindhu Nagesh <vinagesh@microsoft.com>
2021-05-17 11:08:31 -0700

    Add samples for using user assigned managed identity in IoT hub import and export jobs (#207)
    * feat(samples): Sample to use msi with import and export jobs. (#198)
    
    * refactor(sample): Simplifying the import export job sample with msi. (#200)
    
    * refactor(msi-sample): Updating sample with best practices. (#202)

---
+++ iot-hub/Samples/service/ImportExportDevicesWithManagedIdentitySample/Program.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
+using System;
+using System.Threading.Tasks;
+
+namespace ImportExportDevicesWithManagedIdentitySample
+{
+    /// <summary>
+    /// A sample to illustrate how to perform import and export jobs using managed identity 
+    /// to access the storage account. This sample will copy all the devices in the source hub
+    /// to the destination hub.
+    /// For this sample to succeed, the managed identity should be configured to access the 
+    /// storage account used for import and export.
+    /// For more information on configuration, see <see href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-managed-identity"/>.
+    /// For more information on managed identities, see <see href="https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/overview"/>
+    /// </summary>
+    public class Program
+    {        
+        public static async Task Main(string[] args)
+        {
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(parsedParams =>
+                {
+                    parameters = parsedParams;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+
+            var sample = new ImportExportDevicesWithManagedidentitySample();
+
+            await sample.RunSampleAsync(parameters.SourceHubConnectionString,
+                parameters.DestinationHubConnectionString,
+                parameters.BlobContainerUri,
+                parameters.UserDefinedManagedIdentityResourceId);
+        }
+
+    }
+}
