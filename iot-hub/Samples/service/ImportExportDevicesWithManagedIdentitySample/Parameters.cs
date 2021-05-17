4d628eaf5360ee83dce379345752caf15890a99f
Sindhu Nagesh <vinagesh@microsoft.com>
2021-05-17 11:08:31 -0700

    Add samples for using user assigned managed identity in IoT hub import and export jobs (#207)
    * feat(samples): Sample to use msi with import and export jobs. (#198)
    
    * refactor(sample): Simplifying the import export job sample with msi. (#200)
    
    * refactor(msi-sample): Updating sample with best practices. (#202)

---
+++ iot-hub/Samples/service/ImportExportDevicesWithManagedIdentitySample/Parameters.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
+
+namespace ImportExportDevicesWithManagedIdentitySample
+{
+    /// <summary>
+    /// Parameters for the application.
+    /// </summary>
+    internal class Parameters
+    {
+        [Option(
+            "sourceHubConnectionString",
+            Required = true,
+            HelpText = "The connection string of the source IoT Hub.")]
+        public string SourceHubConnectionString { get; set; }
+
+        [Option(
+           "destinationHubConnectionString",
+           Required = true,
+           HelpText = "The connection string of the destination IoT Hub.")]
+        public string DestinationHubConnectionString { get; set; }
+
+        [Option(
+            "blobContainerUri",
+            Required = true,
+            HelpText = "The Uri of storage container for import and export jobs.")]
+        public string BlobContainerUri { get; set; }
+
+        [Option(
+            "userDefinedManagedIdentityResourceId",
+            Required = false,
+            HelpText = "The resource Id of the user defined managed identity. This is not required if you want to use system defined managed identity.")]
+        public string UserDefinedManagedIdentityResourceId { get; set; }
+    }
+}
