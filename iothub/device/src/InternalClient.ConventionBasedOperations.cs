5285c79da6fd632331dff83ddb1d0e9466665306
Abhipsa Misra <abhipsa.misra@microsoft.com>
2021-05-20 16:16:35 -0700

    feat(iot-device): Add support for convention-based command operations

--- iothub/device/src/InternalClient.ConventionBasedOperations.cs
+++ iothub/device/src/InternalClient.ConventionBasedOperations.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Shared;
@@ -31,6 +32,35 @@ namespace Microsoft.Azure.Devices.Client
             }
 
             return SendEventAsync(telemetryMessage, cancellationToken);
+        }
+
+        internal Task SubscribeToCommandsAsync(Func<CommandRequest, object, Task<CommandResponse>> callback, object userContext, CancellationToken cancellationToken)
+        {
+            // Subscribe to methods default handler internally and use the callback received internally to invoke the user supplied command callback.
+            var methodDefaultCallback = new MethodCallback(async (methodRequest, userContext) =>
+            {
+                CommandRequest commandRequest;
+                if (methodRequest.Name != null
+                    && methodRequest.Name.Contains(ConventionBasedConstants.ComponentLevelCommandSeparator))
+                {
+                    string[] split = methodRequest.Name.Split(ConventionBasedConstants.ComponentLevelCommandSeparator);
+                    string componentName = split[0];
+                    string commandName = split[1];
+                    commandRequest = new CommandRequest(PayloadConvention, commandName, componentName, methodRequest.Data);
+                }
+                else
+                {
+                    commandRequest = new CommandRequest(payloadConvention: PayloadConvention, commandName: methodRequest.Name, data: methodRequest.Data);
+                }
+
+                CommandResponse commandResponse = await callback.Invoke(commandRequest, userContext).ConfigureAwait(false);
+                commandResponse.PayloadConvention = PayloadConvention;
+                return commandResponse.ResultAsBytes != null
+                    ? new MethodResponse(commandResponse.ResultAsBytes, commandResponse.Status)
+                    : new MethodResponse(commandResponse.Status);
+            });
+
+            return SetMethodDefaultHandlerAsync(methodDefaultCallback, userContext, cancellationToken);
         }
     }
 }
