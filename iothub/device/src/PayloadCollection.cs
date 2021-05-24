68d4ca3415d8ad981284aebffd323399e659719c
James Davis ⛺️🏔 <jamdavi@microsoft.com>
2021-05-24 15:12:00 -0400

    Extra line break.

--- iothub/device/src/PayloadCollection.cs
+++ iothub/device/src/PayloadCollection.cs
@@ -102,7 +102,8 @@ namespace Microsoft.Azure.Devices.Client
         {
             if (Logging.IsEnabled && Convention == null)
             {
-                Logging.Info($"The convention for this collection is not set; this typically means this collection was not created by the client. TryGetValue will attempt to get the property value but may not behave as expected.");
+                Logging.Info(this, $"The convention for this collection is not set; this typically means this collection was not created by the client. " +
+                    $"TryGetValue will attempt to get the property value but may not behave as expected.", nameof(TryGetValue));
             }
 
             if (Collection.ContainsKey(key))
