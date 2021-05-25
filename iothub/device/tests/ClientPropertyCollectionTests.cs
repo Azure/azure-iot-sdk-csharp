fc19655f450aed633561fce20a3d1f0de11b8386
James Davis ⛺️🏔 <jamdavi@microsoft.com>
2021-05-25 14:29:23 -0400

    Updates to the unit tests

---
+++ iothub/device/tests/ClientPropertyCollectionTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Devices.Shared;
+
+namespace Microsoft.Azure.Devices.Client.Tests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class ClientPropertyCollectionTests
+    {
+        private const string propertyName = "propertyOne";
+        private const string simplePropertyValue = "propertyValue";
+        private const string componentName = "testableComponent";
+        private const string writablePropertyDescription = "testableWritablePropertyDescription";
+        private const string updatedPropertyValue = "updatedPropertyValue";
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanAddSimpleObjectAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { propertyName, simplePropertyValue }
+            };
+            clientProperties.TryGetValue<string>(propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanAddSimpleObjectWithComponentAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { componentName, propertyName, simplePropertyValue }
+            };
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanUpdateSimpleObjectAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { propertyName, simplePropertyValue }
+            };
+            clientProperties.TryGetValue<string>(propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+
+            clientProperties.AddOrUpdate(propertyName, updatedPropertyValue);
+            clientProperties.TryGetValue<string>(propertyName, out var outValueChanged);
+            Assert.AreEqual(updatedPropertyValue, outValueChanged, $"Property values do not match, expected {updatedPropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanUpdateSimpleObjectWithComponentAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { componentName, propertyName, simplePropertyValue }
+            };
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+
+            clientProperties.AddOrUpdate(componentName, propertyName, updatedPropertyValue);
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValueChanged);
+            Assert.AreEqual(updatedPropertyValue, outValueChanged, $"Property values do not match, expected {updatedPropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_AddSimpleObjectAgainThrowsException()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { propertyName, simplePropertyValue }
+            };
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                clientProperties.Add(propertyName, simplePropertyValue);
+            });
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_AddSimpleObjectWithComponentAgainThrowsException()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { componentName, propertyName, simplePropertyValue }
+            };
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                clientProperties.Add(componentName, propertyName, simplePropertyValue);
+            });
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanAddSimpleWritablePropertyAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { propertyName, simplePropertyValue, 200, 2, writablePropertyDescription }
+            };
+            clientProperties.TryGetValue<dynamic>(propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue.value, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(200, outValue.ac, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(2, outValue.av, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(writablePropertyDescription, outValue.ad, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_CanAddWritablePropertyWithComponentAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { componentName, propertyName, simplePropertyValue, 200, 2, writablePropertyDescription }
+            };
+            clientProperties.TryGetValue<dynamic>(componentName, propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue.value, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(200, outValue.ac, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(2, outValue.av, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual(writablePropertyDescription, outValue.ad, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollection_AddingComponentAddsComponentIdentifier()
+        {
+            var clientProperties = new ClientPropertyCollection
+            {
+                { componentName, propertyName, simplePropertyValue }
+            };
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValue);
+            clientProperties.TryGetValue<string>(componentName, "__t", out var componentOut);
+
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual("c", componentOut, $"Property values do not match, expected c but got {componentOut}");
+        }
+    }
+}
---
+++ iothub/device/tests/ClientPropertyCollectionTestsNewtonsoft.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Devices.Shared;
+
+namespace Microsoft.Azure.Devices.Client.Tests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class ClientPropertyCollectionTestsNewtonsoft
+    {
+        private const string propertyName = "propertyOne";
+        private const string simplePropertyValue = "propertyValue";
+        private const string componentName = "testableComponent";
+        private const string writablePropertyDescription = "testableWritablePropertyDescription";
+        private const string updatedPropertyValue = "updatedPropertyValue";
+        
+        private const string twinJson = "{ \"propertyOne\" : \"propertyValue\" }";
+        private const string twinJsonWritableProperty = "{ \"propertyOne\" : { \"value\" : \"propertyValue\",  \"ac\" : 200, \"av\" : 2, \"ad\" : \"testableWritablePropertyDescription\" } }";
+        private const string twinJsonWithComponent = "{ \"testableComponent\" : { \"__t\" : \"c\",  \"propertyOne\" : \"propertyValue\" } }";
+        private const string twinJsonWritablePropertyWithComponent = "{ \"testableComponent\" : { \"__t\" : \"c\", \"propertyOne\" : { \"value\" : \"propertyValue\",  \"ac\" : 200, \"av\" : 2, \"ad\" : \"testableWritablePropertyDescription\" } } }";
+
+        private static TwinCollection collectionToRoundTrip = new TwinCollection(twinJson);
+        private static TwinCollection collectionWritablePropertyToRoundTrip = new TwinCollection(twinJsonWritableProperty);
+        private static TwinCollection collectionWithComponentToRoundTrip = new TwinCollection(twinJsonWithComponent);
+        private static TwinCollection collectionWritablePropertyWithComponentToRoundTrip = new TwinCollection(twinJsonWritablePropertyWithComponent);
+
+        [TestMethod]
+        public void ClientPropertyCollectionNewtonsoft_CanGetSimpleValue()
+        {
+            
+            var clientProperties = ClientPropertyCollection.FromTwinCollection(collectionToRoundTrip, DefaultPayloadConvention.Instance);
+            clientProperties.Convention = DefaultPayloadConvention.Instance;
+            clientProperties.TryGetValue<string>(propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollectionNewtonsoft_CanGetSimpleValueWithComponent()
+        {
+            var clientProperties = ClientPropertyCollection.FromTwinCollection(collectionWithComponentToRoundTrip, DefaultPayloadConvention.Instance);
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollectionNewtonsoft_CanAddSimpleWritablePropertyAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = ClientPropertyCollection.FromTwinCollection(collectionWritablePropertyToRoundTrip, DefaultPayloadConvention.Instance);
+
+            clientProperties.TryGetValue<NewtonsoftJsonWritablePropertyResponse>(propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue.Value, $"Property values do not match, expected {simplePropertyValue} but got {outValue.Value}");
+            Assert.AreEqual(200, outValue.AckCode, $"Property values do not match, expected 200 but got {outValue.AckCode}");
+            Assert.AreEqual(2, outValue.AckVersion, $"Property values do not match, expected 2 but got {outValue.AckVersion}");
+            Assert.AreEqual(writablePropertyDescription, outValue.AckDescription, $"Property values do not match, expected {writablePropertyDescription} but got {outValue.AckDescription}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollectionNewtonsoft_CanAddWritablePropertyWithComponentAndGetBackWithoutDeviceClient()
+        {
+            var clientProperties = ClientPropertyCollection.FromTwinCollection(collectionWritablePropertyWithComponentToRoundTrip, DefaultPayloadConvention.Instance);
+
+            clientProperties.TryGetValue<NewtonsoftJsonWritablePropertyResponse>(componentName, propertyName, out var outValue);
+            Assert.AreEqual(simplePropertyValue, outValue.Value, $"Property values do not match, expected {simplePropertyValue} but got {outValue.Value}");
+            Assert.AreEqual(200, outValue.AckCode, $"Property values do not match, expected 200 but got {outValue.AckCode}");
+            Assert.AreEqual(2, outValue.AckVersion, $"Property values do not match, expected 2 but got {outValue.AckVersion}");
+            Assert.AreEqual(writablePropertyDescription, outValue.AckDescription, $"Property values do not match, expected {writablePropertyDescription} but got {outValue.AckDescription}");
+        }
+
+        [TestMethod]
+        public void ClientPropertyCollectionNewtonsoft_CanGetComponentIdentifier()
+        {
+            var clientProperties = ClientPropertyCollection.FromTwinCollection(collectionWithComponentToRoundTrip, DefaultPayloadConvention.Instance);
+
+            clientProperties.TryGetValue<string>(componentName, propertyName, out var outValue);
+            clientProperties.TryGetValue<string>(componentName, "__t", out var componentOut);
+
+            Assert.AreEqual(simplePropertyValue, outValue, $"Property values do not match, expected {simplePropertyValue} but got {outValue}");
+            Assert.AreEqual("c", componentOut, $"Property values do not match, expected c but got {componentOut}");
+        }
+    }
+}
