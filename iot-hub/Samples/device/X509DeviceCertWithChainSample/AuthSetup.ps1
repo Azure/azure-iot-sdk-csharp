307ea1d5f8c57ecb4b9382974970b4a947ca5b2b
Sindhu Nagesh <vinagesh@microsoft.com>
2021-06-11 09:57:44 -0700

    doc(device-sample): Added readme and enhanced setup script for x509 chain auth sample (#209)

--- iot-hub/Samples/device/X509DeviceCertWithChainSample/AuthSetup.ps1
+++ iot-hub/Samples/device/X509DeviceCertWithChainSample/AuthSetup.ps1
@@ -1,20 +1,43 @@
 ﻿# Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+param(
+    # Local path where the generated certificates will be stored.
+    [Parameter(Mandatory=$true)]
+    [string] $certFolderPath,
+
+    # The password to use for the root certificate.
+    [Parameter(Mandatory=$true)]
+    [securestring] $rootCertPassword,
+
+    # Resource group name of the IoT hub.
+    [Parameter(Mandatory=$true)]
+    [string] $iotHubResourceGroup,
+
+    # IoT hub name used for running the sample.
+    [Parameter(Mandatory=$true)]
+    [string] $iotHubName,
+
+    # Device Id created to use in sample.
+    [Parameter(Mandatory=$true)]
+    [string] $deviceId
+)
+
 # Setup parameters
 $subjectPrefix = "IoT Test"
-$rootCommonName = "$subjectPrefix Test Root CA"
+$rootCommonName = "$subjectPrefix Root CA"
 $intermediateCert1CommonName = "$subjectPrefix Intermediate 1 CA"
 $intermediateCert2CommonName = "$subjectPrefix Intermediate 2 CA"
-$iotHubCertChainDeviceCommonName = "iothubx509chaindevice1"
-$certFolder = "<Path on your machine to create the certs>"
-$iotHubCredentials = New-Object System.Management.Automation.PSCredential("Password", (New-Object System.Security.SecureString))
-$resourceGroup = "<Resource group name of your hub>"
-$hubName = "<Your IotHub name>"
+$iotHubCertChainDeviceCommonName = $deviceId
+$certFolder = $certFolderPath
+$iotHubCredentials = New-Object System.Management.Automation.PSCredential("Password", $rootCertPassword)
+$resourceGroup = $iotHubResourceGroup
+$hubName = $iotHubName
 $certNameToUpload = "rootCA"
 
 # Create root cert
 $rootCertPath = "$certFolder/root.cer"
+Write-Host "Creating $rootCertPath rootCACert"
 $rootCACert = New-SelfSignedCertificate `
     -DnsName "$rootCommonName" `
     -KeyUsage CertSign `
@@ -25,6 +48,7 @@ Export-Certificate -cert $rootCACert -FilePath $rootCertPath -Type CERT | Out-Nu
 
 # Create intermediate 1 cert
 $intermediate1CertPath = "$certFolder/intermediate1.cer"
+Write-Host "Creating $intermediate1CertPath signed by rootCACert"
 $intermediateCert1 = New-SelfSignedCertificate `
     -DnsName "$intermediateCert1CommonName" `
     -KeyUsage CertSign `
@@ -36,6 +60,7 @@ Export-Certificate -cert $intermediateCert1 -FilePath $intermediate1CertPath -Ty
 
 # Create intermediate 2 cert
 $intermediate2CertPath = "$certFolder/intermediate2.cer"
+Write-Host "Creating $intermediate2CertPath signed by intermediateCert1"
 $intermediateCert2 = New-SelfSignedCertificate `
     -DnsName "$intermediateCert2CommonName" `
     -KeyUsage CertSign `
@@ -47,6 +72,7 @@ Export-Certificate -cert $intermediateCert2 -FilePath $intermediate2CertPath -Ty
 
 # Create device cert signed by intermediate2 Certificate
 $devicePfxPath = "$certFolder/$iotHubCertChainDeviceCommonName.pfx"
+Write-Host "Creating $devicePfxPath signed by intermediateCert2"
 $deviceCert = New-SelfSignedCertificate `
     -DnsName "$iotHubCertChainDeviceCommonName" `
     -KeySpec Signature `
@@ -54,18 +80,21 @@ $deviceCert = New-SelfSignedCertificate `
     -CertStoreLocation "Cert:\LocalMachine\My" `
     -NotAfter (Get-Date).AddYears(2) `
     -Signer $intermediateCert2
+Write-Host "Exporting $iotHubCertChainDeviceCommonName.pfx to $devicePfxPath"
 Export-PFXCertificate -cert $deviceCert -filePath $devicePfxPath -password $iotHubCredentials.Password | Out-Null
 
 # Upload rootCA certificate to IotHub
+Write-Host "Uploading $rootCertPath to $hubName"
 $certExits = az iot hub certificate list -g $resourceGroup --hub-name $hubName --query "value[?name=='$certNameToUpload']" --output tsv
 if ($certExits)
 {
     $etag = az iot hub certificate show -g $resourceGroup --hub-name $hubName --name $certNameToUpload --query 'etag'
     az iot hub certificate delete -g $resourceGroup --hub-name $hubName --name $certNameToUpload --etag $etag
 }
-az iot hub certificate create -g $resourceGroup --hub-name $hubName --name $certNameToUpload --path $rootCertPath
+az iot hub certificate create -g $resourceGroup --hub-name $hubName --name $certNameToUpload --path $rootCertPath | Out-Null
 
 # Verify rootCA cert in IotHub
+Write-Host "Verifying possesion of rootCACert in $hubName"
 $etag = az iot hub certificate show -g $resourceGroup --hub-name $hubName --name $certNameToUpload --query 'etag'
 $requestedCommonName = az iot hub certificate generate-verification-code -g $resourceGroup --hub-name $hubName --name $certNameToUpload -e $etag --query 'properties.verificationCode'
 $verificationCertArgs = @{
@@ -80,3 +109,4 @@ $verificationCert = New-SelfSignedCertificate @verificationCertArgs
 Export-Certificate -cert $verificationCert -filePath $verificationCertPath -Type Cert | Out-Null
 $etag = az iot hub certificate show -g $resourceGroup --hub-name $hubName --name $certNameToUpload --query 'etag'
 az iot hub certificate verify -g $resourceGroup --hub-name $hubName --name $certNameToUpload -e $etag --path $verificationCertPath --output none
+Write-Host "Successfuly verified possesion of rootCACert in $hubName"
