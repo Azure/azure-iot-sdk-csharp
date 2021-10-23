32c2c7a34262d672ef604dafba817f8a43dd82b1
James Davis <jamdavi@microsoft.com>
2021-10-22 18:31:25 -0600

    Adding assemblyinfo

---
+++ iothub/device/tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
