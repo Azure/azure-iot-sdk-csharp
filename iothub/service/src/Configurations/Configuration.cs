c44b32085774028881e10b379cf55cf7c041f916
David R. Williamson <drwill@microsoft.com>
2021-11-04 17:49:16 -0700

    Add a test for RegistryManager operations for configurations CRUD (#2231)

--- iothub/service/src/Configurations/Configuration.cs
+++ iothub/service/src/Configurations/Configuration.cs
@@ -9,8 +9,12 @@ using Newtonsoft.Json;
 namespace Microsoft.Azure.Devices
 {
     /// <summary>
-    /// Azure IoT Edge Configurations.
+    /// Device configurations provide the ability to perform IoT device configuration at scale.
+    /// You can define configurations and summarize compliance as the configuration is applied.
     /// </summary>
+    /// <remarks>
+    /// See <see href="https://docs.microsoft.com/azure/iot-hub/iot-hub-automatic-device-management"/> for more details.
+    /// </remarks>
     [System.Diagnostics.CodeAnalysis.SuppressMessage(
     "Naming",
     "CA1724:Type names should not match namespaces",
@@ -20,7 +24,7 @@ namespace Microsoft.Azure.Devices
         /// <summary>
         /// Initializes a new instance of the <see cref="Configuration"/> class.
         /// </summary>
-        /// <param name="configurationId">configuration Id</param>
+        /// <param name="configurationId">The configuration Id. Lowercase and the following special characters are allowed: [-+%_*!'].</param>
         public Configuration(string configurationId)
             : this()
         {
@@ -28,17 +32,16 @@ namespace Microsoft.Azure.Devices
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Configuration"/> class.
+        /// Initializes a new instance of the Configuration class.
         /// </summary>
         internal Configuration()
         {
             SchemaVersion = "1.0";
             ContentType = "assignment";
-            Metrics = new ConfigurationMetrics();
         }
 
         /// <summary>
-        /// Gets Identifier for the configuration
+        /// Gets the identifier for the configuration.
         /// </summary>
         [JsonProperty(PropertyName = "id", Required = Required.Always)]
         public string Id { get; internal set; }
@@ -47,31 +50,31 @@ namespace Microsoft.Azure.Devices
         /// Gets Schema version for the configuration
         /// </summary>
         [JsonProperty(PropertyName = "schemaVersion", NullValueHandling = NullValueHandling.Ignore)]
-        public string SchemaVersion { get; internal set; }
+        public string SchemaVersion { get; }
 
         /// <summary>
         /// Gets or sets labels for the configuration
         /// </summary>
         [JsonProperty(PropertyName = "labels", NullValueHandling = NullValueHandling.Ignore)]
 #pragma warning disable CA2227 // Collection properties should be read only
-        public IDictionary<string, string> Labels { get; set; }
+        public IDictionary<string, string> Labels { get; set; } = new Dictionary<string, string>();
 
 #pragma warning restore CA2227 // Collection properties should be read only
 
         /// <summary>
-        /// Gets or sets Content for the configuration
+        /// Gets or sets content for the configuration
         /// </summary>
         [JsonProperty(PropertyName = "content", NullValueHandling = NullValueHandling.Ignore)]
-        public ConfigurationContent Content { get; set; }
+        public ConfigurationContent Content { get; set; } = new ConfigurationContent();
 
         /// <summary>
         /// Gets the content type for configuration
         /// </summary>
         [JsonProperty(PropertyName = "contentType")]
-        public string ContentType { get; internal set; }
+        public string ContentType { get; }
 
         /// <summary>
-        /// Gets or sets Target Condition for the configuration
+        /// Gets or sets target condition for the configuration
         /// </summary>
         [JsonProperty(PropertyName = "targetCondition")]
         public string TargetCondition { get; set; }
@@ -89,22 +92,22 @@ namespace Microsoft.Azure.Devices
         public DateTime LastUpdatedTimeUtc { get; internal set; }
 
         /// <summary>
-        /// Gets or sets Priority for the configuration
+        /// Gets or sets priority for the configuration
         /// </summary>
         [JsonProperty(PropertyName = "priority")]
         public int Priority { get; set; }
 
         /// <summary>
-        /// System Configuration Metrics
+        /// System configuration metrics
         /// </summary>
         [JsonProperty(PropertyName = "systemMetrics", NullValueHandling = NullValueHandling.Ignore)]
-        public ConfigurationMetrics SystemMetrics { get; internal set; }
+        public ConfigurationMetrics SystemMetrics { get; internal set; } = new ConfigurationMetrics();
 
         /// <summary>
-        /// Custom Configuration Metrics
+        /// Custom configuration metrics
         /// </summary>
         [JsonProperty(PropertyName = "metrics", NullValueHandling = NullValueHandling.Ignore)]
-        public ConfigurationMetrics Metrics { get; set; }
+        public ConfigurationMetrics Metrics { get; set; } = new ConfigurationMetrics();
 
         /// <summary>
         /// Gets or sets configuration's ETag
