f82dec66323365adf0b90dae3e2ec0f1ae401e5f
jamdavi <73593426+jamdavi@users.noreply.github.com>
2021-12-07 14:08:22 -0700

    doc(client): Update HTTP DeviceTwin messaging (#2239)
    * Update twin messaging over Http
    
    * Update twin messaging over Http
    
    * Fixing merge overwrite
    
    * Update iothub/device/src/Transport/HttpTransportHandler.cs
    
    Co-authored-by: David R. Williamson <drwill@microsoft.com>
    
    Co-authored-by: David R. Williamson <drwill@microsoft.com>

--- iothub/device/src/Transport/HttpTransportHandler.cs
+++ iothub/device/src/Transport/HttpTransportHandler.cs
@@ -257,7 +257,7 @@ namespace Microsoft.Azure.Devices.Client.Transport
 
         public override Task<Twin> SendTwinGetAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException("Device twins are only supported with Mqtt protocol.");
+            throw new NotImplementedException("Device twins are not supported over HTTP; they are only supported over the MQTT and AMQP protocols.");
         }
 
         public override async Task<Message> ReceiveAsync(CancellationToken cancellationToken)
@@ -323,7 +323,7 @@ namespace Microsoft.Azure.Devices.Client.Transport
             TimeSpan timeout = timeoutHelper.GetRemainingTime();
             if (timeout > TimeSpan.Zero)
             {
-                throw new ArgumentOutOfRangeException(nameof(timeoutHelper), "Http Protocol does not support a non-zero receive timeout");
+                throw new ArgumentOutOfRangeException(nameof(timeoutHelper), "HTTP Protocol does not support a non-zero receive timeout.");
             }
             else
             {
