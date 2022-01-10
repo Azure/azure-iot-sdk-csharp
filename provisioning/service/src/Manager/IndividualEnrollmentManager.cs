7a26eda629b6b1b564535d2476423ef7fad56dc6
dylanbulfinMS <95251881+dylanbulfinMS@users.noreply.github.com>
2022-01-10 17:21:11 -0500

    Adding RBAC support for provisioning SDK (#2262)
    * Added types for different credentials
    
    * Altered code to work with new types
    
    * Fixed Managers
    
    * removed unneeded #if !NET451
    
    * Filled in method summaries
    
    * refactored GetHeaderProvider
    
    * Added documentation
    
    * Formatting fixes to address comments

--- provisioning/service/src/Manager/IndividualEnrollmentManager.cs
+++ provisioning/service/src/Manager/IndividualEnrollmentManager.cs
@@ -150,7 +150,8 @@ namespace Microsoft.Azure.Devices.Provisioning.Service
         }
 
         internal static Query CreateQuery(
-            ServiceConnectionString provisioningConnectionString,
+            string hostName,
+            IAuthorizationHeaderProvider headerProvider,
             QuerySpecification querySpecification,
             HttpTransportSettings httpTransportSettings,
             CancellationToken cancellationToken,
@@ -168,7 +169,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Service
             }
 
             /* SRS_INDIVIDUAL_ENROLLMENT_MANAGER_21_015: [The CreateQuery shall return a new Query for IndividualEnrollments.] */
-            return new Query(provisioningConnectionString, ServiceName, querySpecification, httpTransportSettings, pageSize, cancellationToken);
+            return new Query(hostName, headerProvider, ServiceName, querySpecification, httpTransportSettings, pageSize, cancellationToken);
         }
 
         private static Uri GetEnrollmentUri(string registrationId)
