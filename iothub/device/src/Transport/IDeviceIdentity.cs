f4bf405ead62cafecb3a704d1cd2a3587db79003
Azad Abbasi <azabbasi@microsoft.com>
2022-01-11 16:33:14 -0800

    Update IDeviceIdentity interface to add doc comments.

--- iothub/device/src/Transport/IDeviceIdentity.cs
+++ iothub/device/src/Transport/IDeviceIdentity.cs
@@ -3,15 +3,44 @@
 
 namespace Microsoft.Azure.Devices.Client.Transport
 {
+    /// <summary>
+    /// Interface for device configurations and common attributes.
+    /// </summary>
     internal interface IDeviceIdentity
     {
+        /// <summary>
+        /// Authentication model for the device.
+        /// </summary>
         AuthenticationModel AuthenticationModel { get; }
+
+        /// <summary>
+        /// AMQP transport layer settings of the device.
+        /// </summary>
         AmqpTransportSettings AmqpTransportSettings { get; }
+
+        /// <summary>
+        /// Device connection string details.
+        /// </summary>
         IotHubConnectionString IotHubConnectionString { get; }
+
+        /// <summary>
+        /// Device details and information.
+        /// </summary>
         ProductInfo ProductInfo { get; }
+
+        /// <summary>
+        /// Device configuration options at the time of initialization.
+        /// </summary>
         ClientOptions Options { get; }
+
+        /// <summary>
+        /// Device authentication audience.
+        /// </summary>
         string Audience { get; }
 
+        /// <summary>
+        /// Whether or not Device is part of a connection pooling.
+        /// </summary>
         bool IsPooling();
     }
 }
