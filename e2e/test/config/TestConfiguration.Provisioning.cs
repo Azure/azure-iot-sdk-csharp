93d3d09a6aace69b085d9303e9a7a6427a994b97
dylanbulfinMS <95251881+dylanbulfinMS@users.noreply.github.com>
2022-01-25 19:37:12 -0500

    Fix namespace issue

--- e2e/test/config/TestConfiguration.Provisioning.cs
+++ e2e/test/config/TestConfiguration.Provisioning.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.Azure.Devices.E2ETests.provisioning;
 
 #if !NET451
 
