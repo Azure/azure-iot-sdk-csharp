2f49f92adb29c715fd07aa9d4bd5f53f69c2fd6a
Kelly Gremban <kgremban@microsoft.com>
2022-03-10 15:00:07 -0800

    Update InvokeDeviceMethod to support passing in deviceName argument (#259)
    * Update InvokeDeviceMethod to support passing in deviceName argument
    
    * Update command line parameter flow; update framework to net6.0
    
    * Change parameter names; update how default values are set
    
    * Change flags for parameters to standardize

---
+++ iot-hub/Quickstarts/InvokeDeviceMethod/Parameters.cs
@@ -0,0 +1,26 @@
+using CommandLine;
+using System;
+
+namespace InvokeDeviceMethod
+{
+    /// <summary>
+    /// Command line parameters for the InvokeDeviceMethod sample
+    /// </summary>
+    internal class Parameters
+    {
+        [Option(
+            'c',
+            "HubConnectionString",
+            HelpText = "The IoT Hub connection string. This is available under the \"Shared access policies\" in the Azure portal." +
+            "\nDefaults to value of environment variable IOTHUB_CONNECTION_STRING.")]
+        public string HubConnectionString { get; set; } = Environment.GetEnvironmentVariable("IOTHUB_CONNECTION_STRING");
+
+        [Option(
+            'd',
+            "DeviceId",
+            Required = false,
+            HelpText = "The Id of the device to receive the direct method." +
+            "\nDefaults to \"MyDotnetDevice\".")]
+        public string DeviceId { get; set; } = "MyDotnetDevice";
+    }
+}
