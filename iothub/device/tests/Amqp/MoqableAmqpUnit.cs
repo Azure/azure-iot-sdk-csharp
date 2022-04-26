82e8df77511e52ca1e3b5cd5081b0d8135ca9c94
David R. Williamson <drwill@microsoft.com>
2022-04-26 15:07:13 -0700

    Allow users to configure websocket keep-alive (#2352)

--- iothub/device/tests/Amqp/MoqableAmqpUnit.cs
+++ iothub/device/tests/Amqp/MoqableAmqpUnit.cs
@@ -16,8 +16,19 @@ namespace Microsoft.Azure.Devices.Client.Test.Transport
 {
     internal class MoqableAmqpUnit : AmqpUnit
     {
-        public MoqableAmqpUnit() : this(new DeviceIdentity(IotHubConnectionStringExtensions.Parse(AmqpTransportHandlerTests.TestConnectionString), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only), new ProductInfo(), new ClientOptions()),
-            new AmqpConnectionHolder(new DeviceIdentity(IotHubConnectionStringExtensions.Parse(AmqpTransportHandlerTests.TestConnectionString), new AmqpTransportSettings(TransportType.Amqp_Tcp_Only), new ProductInfo(), new ClientOptions())))
+        public MoqableAmqpUnit()
+            : this(
+                new DeviceIdentity(
+                    IotHubConnectionStringExtensions.Parse(AmqpTransportHandlerTests.TestConnectionString),
+                    new AmqpTransportSettings(TransportType.Amqp_Tcp_Only),
+                    new ProductInfo(),
+                    new ClientOptions()),
+                new AmqpConnectionHolder(
+                    new DeviceIdentity(
+                        IotHubConnectionStringExtensions.Parse(AmqpTransportHandlerTests.TestConnectionString),
+                        new AmqpTransportSettings(TransportType.Amqp_Tcp_Only),
+                        new ProductInfo(),
+                        new ClientOptions())))
         {
         }
 
