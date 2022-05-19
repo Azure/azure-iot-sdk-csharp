dad70addf26c4735d7d40909be6f3520eb35ca33
Abhipsa Misra <abhipsa.misra@microsoft.com>
2022-05-19 16:42:54 -0500

    feat(prov-device): Add new device registration substatus

--- provisioning/device/src/ProvisioningRegistrationSubstatus.cs
+++ provisioning/device/src/ProvisioningRegistrationSubstatus.cs
@@ -23,6 +23,15 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         /// Device has been assigned to a different IoT hub and its device data was populated from the initial state stored in the enrollment.
         /// Device data was removed from the previously assigned IoT hub.
         /// </summary>
-        DeviceDataReset = 3
+        DeviceDataReset = 3,
+
+        /// <summary>
+        /// Device has been re-provisioned to a previously assigned IoT hub.
+        /// </summary>
+        /// <remarks>
+        /// For API versions prior to "2019-04-15" a substatus of <see cref="InitialAssignment"/> was returned if the device previously existed in hub.
+        /// Starting API version "2019-04-15" a substatus of ReprovisionedToInitialAssignment is returned if the device previously existed in hub.
+        /// </remarks>
+        ReprovisionedToInitialAssignment = 4,
     }
 }
