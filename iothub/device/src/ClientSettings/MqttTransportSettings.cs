d66530b942f254bbebc33bb2df24a6083943c0e4
timtay-microsoft <timtay@microsoft.com>
2022-06-09 12:21:33 -0700

    fixup

--- iothub/device/src/ClientSettings/MqttTransportSettings.cs
+++ iothub/device/src/ClientSettings/MqttTransportSettings.cs
@@ -5,7 +5,6 @@ using System;
 using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
-using DotNetty.Codecs.Mqtt.Packets;
 using Microsoft.Azure.Devices.Shared;
 
 namespace Microsoft.Azure.Devices.Client.Transport.Mqtt
