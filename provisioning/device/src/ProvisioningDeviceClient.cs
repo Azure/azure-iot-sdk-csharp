8084108308b3eb7ad8ad73286151564ec586c781
brycewang-microsoft <brycewang@microsoft.com>
2022-06-14 16:28:01 -0700

    Remove "ProvisioningDeviceClient.RegisterAsync()" methods with timeout as parameter

--- provisioning/device/src/ProvisioningDeviceClient.cs
+++ provisioning/device/src/ProvisioningDeviceClient.cs
@@ -64,54 +64,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         /// <summary>
         /// Registers the current device using the Device Provisioning Service and assigns it to an IoT hub.
         /// </summary>
-        /// <param name="timeout">The maximum amount of time to allow this operation to run for before timing out.</param>
-        /// <remarks>
-        /// Due to the AMQP library used by this library uses not accepting cancellation tokens, this overload and <see cref="RegisterAsync(ProvisioningRegistrationAdditionalData, TimeSpan)"/>
-        /// are the only overloads for this method that allow for a specified timeout to be respected in the middle of an AMQP operation such as opening
-        /// the AMQP connection. MQTT and HTTPS connections do not share that same limitation, though.
-        /// </remarks>
-        /// <returns>The registration result.</returns>
-        public Task<DeviceRegistrationResult> RegisterAsync(TimeSpan timeout)
-        {
-            return RegisterAsync(null, timeout);
-        }
-
-        /// <summary>
-        /// Registers the current device using the Device Provisioning Service and assigns it to an IoT hub.
-        /// </summary>
-        /// <param name="data">
-        /// The optional additional data that is passed through to the custom allocation policy webhook if 
-        /// a custom allocation policy webhook is setup for this enrollment.
-        /// </param>
-        /// <param name="timeout">The maximum amount of time to allow this operation to run for before timing out.</param>
-        /// <remarks>
-        /// Due to the AMQP library used by this library uses not accepting cancellation tokens, this overload and <see cref="RegisterAsync(TimeSpan)"/>
-        /// are the only overloads for this method that allow for a specified timeout to be respected in the middle of an AMQP operation such as opening
-        /// the AMQP connection. MQTT and HTTPS connections do not share that same limitation, though.
-        /// </remarks>
-        /// <returns>The registration result.</returns>
-        public Task<DeviceRegistrationResult> RegisterAsync(ProvisioningRegistrationAdditionalData data, TimeSpan timeout)
-        {
-            Logging.RegisterAsync(this, _globalDeviceEndpoint, _idScope, _transport, _security);
-
-            var request = new ProvisioningTransportRegisterMessage(_globalDeviceEndpoint, _idScope, _security, data?.JsonData)
-            {
-                ProductInfo = ProductInfo,
-            };
-
-            return _transport.RegisterAsync(request, timeout);
-        }
-
-        /// <summary>
-        /// Registers the current device using the Device Provisioning Service and assigns it to an IoT hub.
-        /// </summary>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <remarks>
-        /// Due to the AMQP library used by this library uses not accepting cancellation tokens, the provided cancellation token will only be checked 
-        /// for cancellation in between AMQP operations, and not during. In order to have a timeout for this operation that is checked during AMQP operations
-        /// (such as opening the connection), you must use <see cref="RegisterAsync(TimeSpan)"/> instead. MQTT and HTTPS connections do not have the same 
-        /// behavior as AMQP connections in this regard. MQTT and HTTPS connections will check this cancellation token for cancellation during their protocol level operations.
-        /// </remarks>
         /// <returns>The registration result.</returns>
         public Task<DeviceRegistrationResult> RegisterAsync(CancellationToken cancellationToken = default)
         {
@@ -128,13 +81,6 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         /// a custom allocation policy webhook is setup for this enrollment.
         /// </param>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <remarks>
-        /// Due to the AMQP library used by this library uses not accepting cancellation tokens, the provided cancellation token will only be checked 
-        /// for cancellation in between AMQP operations, and not during. In order to have a timeout for this operation that is checked during AMQP operations
-        /// (such as opening the connection), you must use <see cref="RegisterAsync(ProvisioningRegistrationAdditionalData, TimeSpan)">this overload</see> instead. 
-        /// MQTT and HTTPS connections do not have the same behavior as AMQP connections in this regard. MQTT and HTTPS connections will check this cancellation 
-        /// token for cancellation during their protocol level operations.
-        /// </remarks>
         /// <returns>The registration result.</returns>
         public Task<DeviceRegistrationResult> RegisterAsync(ProvisioningRegistrationAdditionalData data, CancellationToken cancellationToken = default)
         {
