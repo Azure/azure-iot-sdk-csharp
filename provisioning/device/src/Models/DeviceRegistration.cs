3443630d65f9590c495e015f0eded95d6c2da3fd
David R. Williamson <drwill@microsoft.com>
2022-06-27 16:25:11 -0700

    Remove source transport projects

--- provisioning/device/src/Models/DeviceRegistration.cs
+++ provisioning/device/src/Models/DeviceRegistration.cs
@@ -9,36 +9,34 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
     /// <summary>
     /// Device registration.
     /// </summary>
-    internal partial class DeviceRegistration
+    internal class DeviceRegistration
     {
         /// <summary>
         /// Initializes a new instance of the DeviceRegistration class.
         /// </summary>
-#pragma warning disable CA1812 //False positive on this issue. Complains about no one calling the constructor, but it is called in several places
-        public DeviceRegistration()
-        {
-            CustomInit();
-        }
-#pragma warning restore CA1812
-
-        /// <summary>
-        /// Initializes a new instance of the DeviceRegistration class.
-        /// </summary>
-        public DeviceRegistration(JRaw payload = default)
+        public DeviceRegistration(JRaw payload = default, string registrationId = default, TpmAttestation tpm = default)
         {
             Payload = payload;
-            CustomInit();
+            RegistrationId = registrationId;
+            Tpm = tpm;
         }
-
-        /// <summary>
-        /// An initialization method that performs custom operations like setting defaults
-        /// </summary>
-        partial void CustomInit();
 
         /// <summary>
         /// Gets or set the custom content payload.
         /// </summary>
         [JsonProperty(PropertyName = "payload")]
         public JRaw Payload { get; set; }
+
+        /// <summary>
+        /// The device registration Id.
+        /// </summary>
+        [JsonProperty(PropertyName = "registrationId")]
+        public string RegistrationId { get; set; }
+
+        /// <summary>
+        /// Attestation via TPM, if any.
+        /// </summary>
+        [JsonProperty(PropertyName = "tpm")]
+        public TpmAttestation Tpm { get; set; }
     }
 }
---
+++ provisioning/device/src/ProvisioningTransportRegisterMessage.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Authentication;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client
+{
+    /// <summary>
+    /// Represents a provisioning registration message.
+    /// </summary>
+    public class ProvisioningTransportRegisterMessage
+    {
+        private readonly ProductInfo _productInfo = new();
+
+        /// <summary>
+        /// Creates a new instance of this class.
+        /// </summary>
+        /// <param name="globalDeviceEndpoint">The global device endpoint for this message.</param>
+        /// <param name="idScope">The IDScope for this message.</param>
+        /// <param name="authentication">The authentication provider used to authenticate the client.</param>
+        public ProvisioningTransportRegisterMessage(
+            string globalDeviceEndpoint,
+            string idScope,
+            AuthenticationProvider authentication)
+        {
+            GlobalDeviceEndpoint = globalDeviceEndpoint;
+            IdScope = idScope;
+            Authentication = authentication;
+        }
+
+        /// <summary>
+        /// Creates a new instance of this class.
+        /// </summary>
+        /// <param name="globalDeviceEndpoint">The global device endpoint for this message.</param>
+        /// <param name="idScope">The IDScope for this message.</param>
+        /// <param name="authentication">The authentication provider used to authenticate the client.</param>
+        /// <param name="payload">The custom JSON content.</param>
+        public ProvisioningTransportRegisterMessage(
+            string globalDeviceEndpoint,
+            string idScope,
+            AuthenticationProvider authentication,
+            string payload)
+        {
+            GlobalDeviceEndpoint = globalDeviceEndpoint;
+            IdScope = idScope;
+            Authentication = authentication;
+            if (!string.IsNullOrEmpty(payload))
+            {
+                Payload = payload;
+            }
+        }
+
+        /// <summary>
+        /// The global device endpoint for this message.
+        /// </summary>
+        public string GlobalDeviceEndpoint { get; private set; }
+
+        /// <summary>
+        /// The IDScope for this message.
+        /// </summary>
+        public string IdScope { get; private set; }
+
+        /// <summary>
+        /// The authentication provider used to authenticate the client.
+        /// </summary>
+        public AuthenticationProvider Authentication { get; private set; }
+
+        /// <summary>
+        /// The custom content.
+        /// </summary>
+        public string Payload { get; private set; }
+
+        /// <summary>
+        /// The product information sent to the Provisioning service. The application can specify extra information.
+        /// </summary>
+        public string ProductInfo
+        {
+            get => _productInfo.ToString();
+            set => _productInfo.Extra = value;
+        }
+    }
+}
