e23fde69afedb191ebf8c8916092fc5269efa16f
tmahmood-microsoft <tmahmood@microsoft.com>
2022-08-02 10:59:38 -0700

    Update DirectMethodsClient.cs
    updated doc comments and removed unnecessary methods.

--- iothub/service/src/DirectMethod/DirectMethodsClient.cs
+++ iothub/service/src/DirectMethod/DirectMethodsClient.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Common.Exceptions;
 using Microsoft.Azure.Devices.Http2;
 
 namespace Microsoft.Azure.Devices
 {
     /// <summary>
-    /// Subclient of <see cref="IotHubServiceClient"/> to directly invoke direct methods on devices and modules in IoT hub.
+    /// Subclient of <see cref="IotHubServiceClient"/> to invoke direct methods on devices and modules in IoT hub.
     /// </summary>
-    /// <seealso href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-direct-methods"/>
+    /// <seealso href="https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-direct-methods"/>
     public class DirectMethodsClient
     {
+        private readonly string _hostName;
+        private readonly IotHubConnectionProperties _credentialProvider;
+        private readonly HttpClient _httpClient;
+        private readonly HttpRequestMessageFactory _httpRequestMessageFactory;
+
         private const string DeviceMethodUriFormat = "/twins/{0}/methods";
         private const string ModuleMethodUriFormat = "/twins/{0}/modules/{1}/methods";
-
-
-        private string _hostName;
-        private IotHubConnectionProperties _credentialProvider;
-        private HttpClient _httpClient;
-        private HttpRequestMessageFactory _httpRequestMessageFactory;
 
         /// <summary>
         /// Creates an instance of this class. Provided for unit testing purposes only.
@@ -34,14 +30,14 @@ namespace Microsoft.Azure.Devices
 
         internal DirectMethodsClient(string hostName, IotHubConnectionProperties credentialProvider, HttpClient httpClient, HttpRequestMessageFactory httpRequestMessageFactory)
         {
-            _credentialProvider = credentialProvider;
             _hostName = hostName;
+            _credentialProvider = credentialProvider;
             _httpClient = httpClient;
             _httpRequestMessageFactory = httpRequestMessageFactory;
         }
 
         /// <summary>
-        /// Interactively invokes a method on a device.
+        /// Invokes a method on a device.
         /// </summary>
         /// <param name="deviceId">The device identifier for the target device.</param>
         /// <param name="cloudToDeviceMethod">Parameters to execute a direct method on the device.</param>
@@ -50,44 +46,18 @@ namespace Microsoft.Azure.Devices
         /// <exception cref="ArgumentNullException">When <paramref name="deviceId"/> is null.</exception>
         /// <exception cref="ArgumentNullException">When <paramref name="cloudToDeviceMethod"/> is null.</exception>
         /// <exception cref="ArgumentException">When <paramref name="deviceId"/> is empty or whitespace.</exception>
-        /// <seealso href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-direct-methods"/>
-        public virtual Task<CloudToDeviceMethodResult> InvokeAsync(string deviceId, CloudToDeviceMethod cloudToDeviceMethod, CancellationToken cancellationToken = default)
-        {
-            Argument.RequireNotNullOrEmpty(deviceId, nameof(deviceId));
-            Argument.RequireNotNull(cloudToDeviceMethod, nameof(cloudToDeviceMethod));
-
-            return InvokeAsync(GetDeviceMethodUri(deviceId), cloudToDeviceMethod, cancellationToken);
-        }
-
-        /// <summary>
-        /// Interactively invokes a method on a module.
-        /// </summary>
-        /// <param name="deviceId">The device identifier for the target device.</param>
-        /// <param name="moduleId">The module identifier for the target module.</param>
-        /// <param name="cloudToDeviceMethod">Parameters to execute a direct method on the module.</param>
-        /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
-        /// <returns>The <see cref="CloudToDeviceMethodResult"/>.</returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="deviceId"/> or <paramref name="moduleId"/> are null.</exception>
-        /// <exception cref="ArgumentNullException">When <paramref name="cloudToDeviceMethod"/> is null.</exception>
-        /// <exception cref="ArgumentException">When <paramref name="deviceId"/> or <paramref name="moduleId"/> are empty or whitespace.</exception>
-        /// <seealso href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-direct-methods"/>
-        public virtual Task<CloudToDeviceMethodResult> InvokeAsync(string deviceId, string moduleId, CloudToDeviceMethod cloudToDeviceMethod, CancellationToken cancellationToken = default)
-        {
-            Argument.RequireNotNullOrEmpty(deviceId, nameof(deviceId));
-            Argument.RequireNotNullOrEmpty(moduleId, nameof(moduleId));
-            Argument.RequireNotNull(cloudToDeviceMethod, nameof(cloudToDeviceMethod));
-
-            return InvokeAsync(GetModuleMethodUri(deviceId, moduleId), cloudToDeviceMethod, cancellationToken);
-        }
-
-        private async Task<CloudToDeviceMethodResult> InvokeAsync(Uri uri,CloudToDeviceMethod cloudToDeviceMethod,CancellationToken cancellationToken)
+        /// <seealso href="https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-direct-methods"/>
+        public virtual async Task<CloudToDeviceMethodResult> InvokeAsync(string deviceId, CloudToDeviceMethod cloudToDeviceMethod, CancellationToken cancellationToken = default)
         {
             if (Logging.IsEnabled)
-                Logging.Enter(this, $"Invoking device method for: {uri}", nameof(InvokeAsync));
+                Logging.Enter(this, $"Invoking device method for device id: {deviceId}", nameof(InvokeAsync));
 
             try
             {
-                using HttpRequestMessage request = _httpRequestMessageFactory.CreateRequest(HttpMethod.Post, uri, _credentialProvider, cloudToDeviceMethod);
+                Argument.RequireNotNullOrEmpty(deviceId, nameof(deviceId));
+                Argument.RequireNotNull(cloudToDeviceMethod, nameof(cloudToDeviceMethod));
+
+                using HttpRequestMessage request = _httpRequestMessageFactory.CreateRequest(HttpMethod.Post, GetDeviceMethodUri(deviceId), _credentialProvider, cloudToDeviceMethod);
                 HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
                 await HttpMessageHelper2.ValidateHttpResponseStatus(HttpStatusCode.OK, response);
                 return await HttpMessageHelper2.DeserializeResponse<CloudToDeviceMethodResult>(response, cancellationToken);
@@ -101,33 +71,48 @@ namespace Microsoft.Azure.Devices
             finally
             {
                 if (Logging.IsEnabled)
-                    Logging.Exit(this, $"Invoking device method for: {uri}", nameof(InvokeAsync));
+                    Logging.Exit(this, $"Invoking device method for device id: {deviceId}", nameof(InvokeAsync));
             }
         }
 
-        private static TimeSpan GetInvokeDeviceMethodOperationTimeout(CloudToDeviceMethod cloudToDeviceMethod)
+        /// <summary>
+        /// Invokes a method on a module.
+        /// </summary>
+        /// <param name="deviceId">The device identifier for the target device.</param>
+        /// <param name="moduleId">The module identifier for the target module.</param>
+        /// <param name="cloudToDeviceMethod">Parameters to execute a direct method on the module.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+        /// <returns>The <see cref="CloudToDeviceMethodResult"/>.</returns>
+        /// <exception cref="ArgumentNullException">When <paramref name="deviceId"/> or <paramref name="moduleId"/> are null.</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="cloudToDeviceMethod"/> is null.</exception>
+        /// <exception cref="ArgumentException">When <paramref name="deviceId"/> or <paramref name="moduleId"/> are empty or whitespace.</exception>
+        /// <seealso href="https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-direct-methods"/>
+        public virtual async Task<CloudToDeviceMethodResult> InvokeAsync(string deviceId, string moduleId, CloudToDeviceMethod cloudToDeviceMethod, CancellationToken cancellationToken = default)
         {
-            // For InvokeDeviceMethod, we need to take into account the timeouts specified
-            // for the Device to connect and send a response. We also need to take into account
-            // the transmission time for the request send/receive
-            var timeout = TimeSpan.FromSeconds(15); // For wire time
-            var connectionTimeOut = TimeSpan.FromSeconds(cloudToDeviceMethod.ConnectionTimeoutInSeconds ?? 0);
-            var responseTimeOut = TimeSpan.FromSeconds(cloudToDeviceMethod.ResponseTimeoutInSeconds ?? 0);
+            if (Logging.IsEnabled)
+                Logging.Enter(this, $"Invoking device method for device id: {deviceId} and module id: {moduleId}", nameof(InvokeAsync));
 
-            ValidateTimeOut(connectionTimeOut);
-            ValidateTimeOut(responseTimeOut);
-
-            timeout += connectionTimeOut;
-            timeout += responseTimeOut;
-
-            return timeout;
-        }
-
-        private static void ValidateTimeOut(TimeSpan connectionTimeOut)
-        {
-            if (connectionTimeOut.Seconds < 0)
+            try
             {
-                throw new ArgumentException("Negative timeout");
+                Argument.RequireNotNullOrEmpty(deviceId, nameof(deviceId));
+                Argument.RequireNotNullOrEmpty(moduleId, nameof(moduleId));
+                Argument.RequireNotNull(cloudToDeviceMethod, nameof(cloudToDeviceMethod));
+
+                using HttpRequestMessage request = _httpRequestMessageFactory.CreateRequest(HttpMethod.Post, GetModuleMethodUri(deviceId, moduleId), _credentialProvider, cloudToDeviceMethod);
+                HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken);
+                await HttpMessageHelper2.ValidateHttpResponseStatus(HttpStatusCode.OK, response);
+                return await HttpMessageHelper2.DeserializeResponse<CloudToDeviceMethodResult>(response, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                if (Logging.IsEnabled)
+                    Logging.Error(this, $"{nameof(InvokeAsync)} threw an exception: {ex}", nameof(InvokeAsync));
+                throw;
+            }
+            finally
+            {
+                if (Logging.IsEnabled)
+                    Logging.Exit(this, $"Invoking device method for device id: {deviceId} and module id: {moduleId}", nameof(InvokeAsync));
             }
         }
 
