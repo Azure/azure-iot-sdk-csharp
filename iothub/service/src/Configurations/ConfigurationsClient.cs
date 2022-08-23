8600fbf99cae915e51bc1e3467232694e8266e54
tmahmood-microsoft <tmahmood@microsoft.com>
2022-08-23 13:24:17 -0700

    removed unused namespace references

--- iothub/service/src/Configurations/ConfigurationsClient.cs
+++ iothub/service/src/Configurations/ConfigurationsClient.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Common.Exceptions;
--- iothub/service/src/Registry/DevicesClient.cs
+++ iothub/service/src/Registry/DevicesClient.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
