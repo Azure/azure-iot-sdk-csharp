34a62790c6b490ce4b18220b73d4e962f19c91ed
Nicholas Patilsen <npatilsen@microsoft.com>
2022-08-31 15:47:57 -0700

    provisioning service samples

---
+++ provisioning/service/samples/How To/BulkOperationSample/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection.Metadata.Ecma335;
+using BulkOperationSample;
+using CommandLine;
+
+namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+               .WithParsed(parsedParams =>
+               {
+                   parameters = parsedParams;
+               })
+               .WithNotParsed(errors =>
+               {
+                   Environment.Exit(1);
+               });
+
+            if (string.IsNullOrWhiteSpace(parameters.ProvisioningConnectionString))
+            {
+                Console.WriteLine(CommandLine.Text.HelpText.AutoBuild(result, null, null));
+                Environment.Exit(1);
+            }
+
+            using var provisioningServiceClient = ProvisioningServiceClient.CreateFromConnectionString(parameters.ProvisioningConnectionString);
+            var sample = new BulkOperationSample(provisioningServiceClient);
+            sample.RunSampleAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Done.\n");
+            return 0;
+        }
+    }
+}
