8e77f98d1a5ce002664e656d16545fb3b5a8e4ab
Nicholas Patilsen <npatilsen@microsoft.com>
2022-08-31 15:41:56 -0700

    provisioning device samples

---
+++ provisioning/device/samples/Getting Started/ComputeDerivedSymmetricKeySample/Program.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using Microsoft.Azure.Devices.Provisioning.Client.Samples;
+using System;
+
+namespace ComputeDerivedSymmetricKeySample
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(parsedParams =>
+                {
+                    parameters = parsedParams;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+
+            var sample = new ComputeDerivedKeySample(parameters);
+            sample.RunSample();
+
+            return 0;
+        }
+    }
+}
