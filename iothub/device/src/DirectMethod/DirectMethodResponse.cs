1a1ad7e5720c24e6c32ba04251e4ef2d2f740436
timtay-microsoft <timtay@microsoft.com>
2022-09-06 11:00:13 -0700

    fixup

--- iothub/device/src/DirectMethod/DirectMethodResponse.cs
+++ iothub/device/src/DirectMethod/DirectMethodResponse.cs
@@ -32,7 +32,11 @@ namespace Microsoft.Azure.Devices.Client
         /// <summary>
         /// The request Id for the transport layer.
         /// </summary>
-        [JsonIgnore] // not part of the Json payload
+        /// <remarks>
+        /// This value is not part of the Json payload. It is received as topic string parameter over MQTT and as a
+        /// property over AMQP.
+        /// </remarks>
+        [JsonIgnore]
         internal string RequestId { get; set; }
     }
 }
