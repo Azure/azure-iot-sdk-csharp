c435d95ed17774cdc1d78d70cfe48ca60cc4d3ca
Nick Patilsen <110431552+patilsnr@users.noreply.github.com>
2022-09-07 15:17:34 -0700

    Provisioning + Quickstart samples moved to sdk repo (#2652)
    * moved quickstarts to sdk repo
    
    * provisioning device samples
    
    * provisioning service samples
    
    * round 1 of fixes
    
    * removed streaming samples from v1
    
    * groupcert sample added

---
+++ provisioning/service/samples/Getting Started/EnrollmentSample/Parameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine.Text;
+using CommandLine;
+using Newtonsoft.Json;
+
+namespace EnrollmentSample
+{
+    internal class Parameters
+    {
+        [Option(
+            'c',
+            "ProvisioningConnectionString",
+            Required = false,
+            HelpText = "The connection string of device provisioning service. Not required when the PROVISIONING_CONNECTION_STRING environment variable is set.")]
+        public string ProvisioningConnectionString { get; set; } = Environment.GetEnvironmentVariable("PROVISIONING_CONNECTION_STRING");
+    }
+}
