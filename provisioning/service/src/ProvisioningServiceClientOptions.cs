a2532041cf9c58740bfe85447c1feccae2305311
timtay-microsoft <timtay@microsoft.com>
2022-09-09 10:09:39 -0700

    fixup

--- provisioning/service/src/ProvisioningServiceClientOptions.cs
+++ provisioning/service/src/ProvisioningServiceClientOptions.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Security.Authentication;
-using System.Text;
 
 namespace Microsoft.Azure.Devices.Provisioning.Service
 {
