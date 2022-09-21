eb40e7724f0cf3aa2a15d9e7b4cbb8153d675ece
David R. Williamson <drwill@microsoft.com>
2022-09-21 15:07:32 -0700

    Fix DPS device "how to" samples (#2718)

--- provisioning/device/samples/How To/SymmetricKeySample/ProvisioningDeviceClientSample.cs
+++ provisioning/device/samples/How To/SymmetricKeySample/ProvisioningDeviceClientSample.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Azure.Devices.Client;
-using Microsoft.Azure.Devices.Provisioning.Client.Transport;
-using Microsoft.Azure.Devices.Shared;
 using System;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Authentication;
+using Microsoft.Azure.Devices.Client;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
 {
@@ -31,20 +30,21 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
             // the device Id is already chosen. However, for group enrollments the device Id can be requested by
             // the device, as long as the key has been computed using that value.
             // Also, the secondary could be included, but was left out for the simplicity of this sample.
-            using var security = new SecurityProviderSymmetricKey(
+            var security = new AuthenticationProviderSymmetricKey(
                 _parameters.Id,
                 _parameters.PrimaryKey,
                 null);
 
-            using ProvisioningTransportHandler transportHandler = GetTransportHandler();
+            using ProvisioningTransportHandler transportHandler = _parameters.GetTransportHandler();
+            var options = new ProvisioningClientOptions(transportHandler);
 
-            var provClient = ProvisioningDeviceClient.Create(
+            var provClient = new ProvisioningDeviceClient(
                 _parameters.GlobalDeviceEndpoint,
                 _parameters.IdScope,
                 security,
-                transportHandler);
+                options);
 
-            Console.WriteLine($"Initialized for registration Id {security.GetRegistrationID()}.");
+            Console.WriteLine($"Initialized for registration Id {security.GetRegistrationId()}.");
 
             Console.WriteLine("Registering with the device provisioning service...");
             DeviceRegistrationResult result = await provClient.RegisterAsync();
@@ -59,34 +59,23 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
             Console.WriteLine($"Device {result.DeviceId} registered to {result.AssignedHub}.");
 
             Console.WriteLine("Creating symmetric key authentication for IoT Hub...");
-            IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
+            Devices.Client.IAuthenticationMethod auth = new DeviceAuthenticationWithRegistrySymmetricKey(
                 result.DeviceId,
                 security.GetPrimaryKey());
 
             Console.WriteLine($"Testing the provisioned device with IoT Hub...");
-            using var iotClient = DeviceClient.Create(result.AssignedHub, auth, _parameters.TransportType);
+            var hubOptions = new IotHubClientOptions(_parameters.GetHubTransportSettings())
+            {
+                
+            };
+            using var iotClient = new IotHubDeviceClient(result.AssignedHub, auth, hubOptions);
 
             Console.WriteLine("Sending a telemetry message...");
-            using var message = new Message(Encoding.UTF8.GetBytes("TestMessage"));
+            var message = new Devices.Client.Message(Encoding.UTF8.GetBytes("TestMessage"));
             await iotClient.SendEventAsync(message);
 
             await iotClient.CloseAsync();
             Console.WriteLine("Finished.");
-        }
-
-        private ProvisioningTransportHandler GetTransportHandler()
-        {
-            return _parameters.TransportType switch
-            {
-                TransportType.Mqtt => new ProvisioningTransportHandlerMqtt(),
-                TransportType.Mqtt_Tcp_Only => new ProvisioningTransportHandlerMqtt(TransportFallbackType.TcpOnly),
-                TransportType.Mqtt_WebSocket_Only => new ProvisioningTransportHandlerMqtt(TransportFallbackType.WebSocketOnly),
-                TransportType.Amqp => new ProvisioningTransportHandlerAmqp(),
-                TransportType.Amqp_Tcp_Only => new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly),
-                TransportType.Amqp_WebSocket_Only => new ProvisioningTransportHandlerAmqp(TransportFallbackType.WebSocketOnly),
-                TransportType.Http1 => new ProvisioningTransportHandlerHttp(),
-                _ => throw new NotSupportedException($"Unsupported transport type {_parameters.TransportType}"),
-            };
         }
     }
 }
--- provisioning/device/samples/How To/TpmSample/Parameters.cs
+++ provisioning/device/samples/How To/TpmSample/Parameters.cs
@@ -1,11 +1,28 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using CommandLine;
 using Microsoft.Azure.Devices.Client;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
 {
+    /// <summary>
+    /// The transport to use.
+    /// </summary>
+    public enum Transport
+    {
+        /// <summary>
+        /// MQTT 3.1.1.
+        /// </summary>
+        Mqtt,
+
+        /// <summary>
+        /// AMQP
+        /// </summary>
+        Amqp,
+    };
+
     /// <summary>
     /// Parameters for the application
     /// </summary>
@@ -30,17 +47,45 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
         public string RegistrationId { get; set; }
 
         [Option(
-            'g',
             "GlobalDeviceEndpoint",
             Default = "global.azure-devices-provisioning.net",
             HelpText = "The global endpoint for devices to connect to.")]
         public string GlobalDeviceEndpoint { get; set; }
 
         [Option(
-            't',
-            "TransportType",
-            Default = TransportType.Amqp,
-            HelpText = "The transport to use to communicate with the device provisioning instance. Possible values include Amqp, Amqp_WebSocket_Only, Amqp_Tcp_only, and Http1.")]
-        public TransportType TransportType { get; set; }
+            "Transport",
+            Default = Transport.Mqtt,
+            HelpText = "The transport to use for the connection.")]
+        public Transport Transport { get; set; }
+
+        [Option(
+            "TransportProtocol",
+            Default = ProvisioningClientTransportProtocol.Tcp,
+            HelpText = "The transport to use to communicate with the device provisioning instance.")]
+        public ProvisioningClientTransportProtocol TransportProtocol { get; set; }
+
+        internal ProvisioningTransportHandler GetTransportHandler()
+        {
+            return Transport switch
+            {
+                Transport.Mqtt => new ProvisioningTransportHandlerMqtt(TransportProtocol),
+                Transport.Amqp => new ProvisioningTransportHandlerAmqp(TransportProtocol),
+                _ => throw new NotSupportedException($"Unsupported transport type {Transport}/{TransportProtocol}"),
+            };
+        }
+
+        internal IotHubClientTransportSettings GetHubTransportSettings()
+        {
+            IotHubClientTransportProtocol protocol = TransportProtocol == ProvisioningClientTransportProtocol.Tcp
+                ? IotHubClientTransportProtocol.Tcp
+                : IotHubClientTransportProtocol.WebSocket;
+
+            return Transport switch
+            {
+                Transport.Mqtt => new IotHubClientMqttSettings(protocol),
+                Transport.Amqp => new IotHubClientAmqpSettings(protocol),
+                _ => throw new NotSupportedException($"Unsupported transport type {Transport}/{TransportProtocol}"),
+            };
+        }
     }
 }
