a3e0f4f74e8fc23c2ffd5abc01acb31a792cd354
timtay-microsoft <timtay@microsoft.com>
2022-09-23 14:33:50 -0700

    fixup

--- provisioning/device/src/Transports/Mqtt/ProvisioningTransportHandlerMqtt.cs
+++ provisioning/device/src/Transports/Mqtt/ProvisioningTransportHandlerMqtt.cs
@@ -375,7 +375,8 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
             mqttClientOptionsBuilder
                 .WithTls(tlsParameters)
                 .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V311) // 3.1.1
-                .WithKeepAlivePeriod(IdleTimeout);
+                .WithKeepAlivePeriod(IdleTimeout)
+                .WithTimeout(TimeSpan.MaxValue); // MQTTNet will only time out if the cancellation token requests cancellation.
 
             return mqttClientOptionsBuilder;
         }
