2fb74bfbb3d4483f7d7b10eb1af6f3d8353b1908
Nathanael Gastelum <ngastelum@microsoft.com>
2022-09-23 14:12:55 -0700

    Cleanup code

--- iothub/device/samples/getting started/RoutingTutorial/horizontal-arm/arm-read-write/Program.cs
+++ iothub/device/samples/getting started/RoutingTutorial/horizontal-arm/arm-read-write/Program.cs
@@ -4,7 +4,7 @@
 //This code that messages to an IoT Hub for testing the routing as defined
 //  in this article: https://docs.microsoft.com/en-us/azure/iot-hub/tutorial-routing
 //The scripts for creating the resources are included in the resources folder in this
-//  Visual Studio solution. 
+//  Visual Studio solution.
 
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
@@ -13,15 +13,15 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace arm_read_write
+namespace ArmReadWrite
 {
-    class Program
+    internal class Program
     {
         //This is the arm-read-write application that simulates a virtual device.
-        //It writes messages to the IoT Hub, which routes the messages automatically to a storage account, 
+        //It writes messages to the IoT Hub, which routes the messages automatically to a storage account,
         //where you can view them.
 
-        //  This was derived by the (more complicated) tutorial for routing 
+        //  This was derived by the (more complicated) tutorial for routing
         //  https://docs.microsoft.com/en-us/azure/iot-hub/tutorial-routing
 
         private static DeviceClient s_deviceClient;
@@ -40,16 +40,20 @@ namespace arm_read_write
 
             Console.WriteLine("write messages to a hub and use routing to write them to storage");
 
-            s_deviceClient = DeviceClient.Create(s_iotHubUri, 
-              new DeviceAuthenticationWithRegistrySymmetricKey(s_myDeviceId, s_deviceKey), TransportType.Mqtt);
+            s_deviceClient = DeviceClient.Create(s_iotHubUri,
+                new DeviceAuthenticationWithRegistrySymmetricKey(s_myDeviceId, s_deviceKey), TransportType.Mqtt);
 
-            var cts = new CancellationTokenSource();
+            using var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Sample execution cancellation requested; will exit.");
+            };
 
-            var messages = SendDeviceToCloudMessagesAsync(cts.Token);
+            Task messages = SendDeviceToCloudMessagesAsync(cts.Token);
 
-            Console.WriteLine("Press the Enter key to stop.");
-            Console.ReadLine();
-            cts.Cancel();
+            Console.WriteLine($"Press Control+C at any time to quit the sample.");
             await messages;
         }
 
@@ -67,7 +71,7 @@ namespace arm_read_write
             s_iotHubUri = Environment.GetEnvironmentVariable("IOT_HUB_URI");
             s_deviceKey = Environment.GetEnvironmentVariable("IOT_DEVICE_KEY");
 
-            if ((s_myDeviceId is null) || (s_iotHubUri is null) || (s_deviceKey is null))
+            if (s_myDeviceId is null || s_iotHubUri is null || s_deviceKey is null)
             {
                 result = false;
             }
@@ -82,11 +86,10 @@ namespace arm_read_write
         {
             double minTemperature = 20;
             double minHumidity = 60;
-            Random rand = new Random();
+            var rand = new Random();
 
             while (!token.IsCancellationRequested)
             {
-
                 double currentTemperature = minTemperature + rand.NextDouble() * 15;
                 double currentHumidity = minHumidity + rand.NextDouble() * 20;
 
@@ -120,7 +123,7 @@ namespace arm_read_write
                     pointInfo = infoString
                 };
                 // serialize the telemetry data and convert it to JSON.
-                var telemetryDataString = JsonConvert.SerializeObject(telemetryDataPoint);
+                string telemetryDataString = JsonConvert.SerializeObject(telemetryDataPoint);
 
                 // Encode the serialized object using UTF-8 so it can be parsed by IoT Hub when
                 // processing messaging rules.
@@ -134,12 +137,16 @@ namespace arm_read_write
                 message.Properties.Add("level", levelValue);
 
                 // Submit the message to the hub.
-                await s_deviceClient.SendEventAsync(message);
+                await s_deviceClient.SendEventAsync(message, token);
 
                 // Print out the message.
                 Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, telemetryDataString);
 
-                await Task.Delay(1000);
+                try
+                {
+                    await Task.Delay(1000, token);
+                }
+                catch (OperationCanceledException) { }
             }
         }
     }
