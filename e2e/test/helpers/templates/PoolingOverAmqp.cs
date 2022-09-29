164866d4fa6eeeb2266de5d5d7cbb842eea593bd
David R. Williamson <drwill@microsoft.com>
2022-09-28 18:38:13 -0700

    Reduce extra stuff from pooling test

--- e2e/test/helpers/templates/PoolingOverAmqp.cs
+++ e2e/test/helpers/templates/PoolingOverAmqp.cs
@@ -4,9 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Microsoft.Azure.Devices.Client;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Azure.Devices.E2ETests.Helpers.Templates
 {
@@ -16,8 +14,6 @@ namespace Microsoft.Azure.Devices.E2ETests.Helpers.Templates
         public const int SingleConnection_PoolSize = 1;
         public const int MultipleConnections_DevicesCount = 4;
         public const int MultipleConnections_PoolSize = 2;
-        public const int MaxTestRunCount = 5;
-        public const int TestSuccessRate = 80; // 4 out of 5 (80%) test runs should pass (even after accounting for network instability issues).
 
         public static async Task TestPoolAmqpAsync(
             string devicePrefix,
@@ -37,105 +33,64 @@ namespace Microsoft.Azure.Devices.E2ETests.Helpers.Templates
                 Pooling = true,
             };
 
-            int totalRuns = 0;
-            int successfulRuns = 0;
-            int currentSuccessRate = 0;
-            bool reRunTest = false;
-
             var testDevices = new List<TestDevice>(devicesCount);
             var deviceClients = new List<IotHubDeviceClient>(devicesCount);
             var testDeviceCallbackHandlers = new List<TestDeviceCallbackHandler>(devicesCount);
             var amqpConnectionStatuses = new List<AmqpConnectionStatusChange>(devicesCount);
             var operations = new List<Task>(devicesCount);
 
-            do
+            // Arrange
+            // Initialize the test device client instances
+            // Set the device client connection status change handler
+            logger.Trace($"{nameof(PoolingOverAmqp)} Initializing Device Clients for multiplexing test");
+            for (int i = 0; i < devicesCount; i++)
             {
-                totalRuns++;
-
-                // Arrange
                 // Initialize the test device client instances
+                TestDevice testDevice = await TestDevice.GetTestDeviceAsync(logger, $"{devicePrefix}_{i}_").ConfigureAwait(false);
+                IotHubDeviceClient deviceClient = testDevice.CreateDeviceClient(new IotHubClientOptions(transportSettings), authScope: authScope);
+
                 // Set the device client connection status change handler
-                logger.Trace($"{nameof(PoolingOverAmqp)} Initializing Device Clients for multiplexing test - Test run {totalRuns}");
+                var amqpConnectionStatusChange = new AmqpConnectionStatusChange(logger);
+                deviceClient.SetConnectionStatusChangeCallback(amqpConnectionStatusChange.ConnectionStatusChangeHandler);
+
+                var testDeviceCallbackHandler = new TestDeviceCallbackHandler(deviceClient, testDevice, logger);
+
+                testDevices.Add(testDevice);
+                deviceClients.Add(deviceClient);
+                testDeviceCallbackHandlers.Add(testDeviceCallbackHandler);
+                amqpConnectionStatuses.Add(amqpConnectionStatusChange);
+
+                if (initOperation != null)
+                {
+                    await initOperation(deviceClient, testDevice, testDeviceCallbackHandler).ConfigureAwait(false);
+                }
+            }
+
+            try
+            {
                 for (int i = 0; i < devicesCount; i++)
                 {
-                    // Initialize the test device client instances
-                    TestDevice testDevice = await TestDevice.GetTestDeviceAsync(logger, $"{devicePrefix}_{i}_").ConfigureAwait(false);
-                    IotHubDeviceClient deviceClient = testDevice.CreateDeviceClient(new IotHubClientOptions(transportSettings), authScope: authScope);
-
-                    // Set the device client connection status change handler
-                    var amqpConnectionStatusChange = new AmqpConnectionStatusChange(logger);
-                    deviceClient.SetConnectionStatusChangeCallback(amqpConnectionStatusChange.ConnectionStatusChangeHandler);
-
-                    var testDeviceCallbackHandler = new TestDeviceCallbackHandler(deviceClient, testDevice, logger);
-
-                    testDevices.Add(testDevice);
-                    deviceClients.Add(deviceClient);
-                    testDeviceCallbackHandlers.Add(testDeviceCallbackHandler);
-                    amqpConnectionStatuses.Add(amqpConnectionStatusChange);
-
-                    if (initOperation != null)
-                    {
-                        await initOperation(deviceClient, testDevice, testDeviceCallbackHandler).ConfigureAwait(false);
-                    }
+                    operations.Add(testOperation(deviceClients[i], testDevices[i], testDeviceCallbackHandlers[i]));
                 }
-
-                try
+                await Task.WhenAll(operations).ConfigureAwait(false);
+                operations.Clear();
+            }
+            finally
+            {
+                // Close the service-side components and dispose the device client instances.
+                if (cleanupOperation != null)
                 {
-                    for (int i = 0; i < devicesCount; i++)
-                    {
-                        operations.Add(testOperation(deviceClients[i], testDevices[i], testDeviceCallbackHandlers[i]));
-                    }
-                    await Task.WhenAll(operations).ConfigureAwait(false);
-                    operations.Clear();
-
-                    // Close the device client instances and verify the connection status change checks
-                    bool deviceConnectionStatusAsExpected = true;
-                    for (int i = 0; i < devicesCount; i++)
-                    {
-                        await deviceClients[i].CloseAsync().ConfigureAwait(false);
-
-                        if (!ignoreConnectionStatus)
-                        {
-                            // The connection status change count should be 2: connect (open) and disabled (close)
-                            if (amqpConnectionStatuses[i].ConnectionStatusChangeHandlerCount != 2)
-                            {
-                                deviceConnectionStatusAsExpected = false;
-                            }
-
-                            // The connection status should be "Disabled", with connection status change reason "ClientClose"
-                            deviceClients[i].ConnectionStatusInfo.Status.Should().Be(
-                                ConnectionStatus.Closed,
-                                $"Reason {deviceClients[i].ConnectionStatusInfo.ChangeReason}");
-                            deviceClients[i].ConnectionStatusInfo.ChangeReason.Should().Be(ConnectionStatusChangeReason.ClientClosed);
-                        }
-                    }
-                    if (deviceConnectionStatusAsExpected)
-                    {
-                        successfulRuns++;
-                    }
-
-                    currentSuccessRate = (int)((double)successfulRuns / totalRuns * 100);
-                    reRunTest = currentSuccessRate < TestSuccessRate;
+                    await cleanupOperation().ConfigureAwait(false);
                 }
-                finally
-                {
-                    // Close the service-side components and dispose the device client instances.
-                    if (cleanupOperation != null)
-                    {
-                        await cleanupOperation().ConfigureAwait(false);
-                    }
 
-                    deviceClients.ForEach(deviceClient => deviceClient.Dispose());
-                    testDeviceCallbackHandlers.ForEach(testDeviceCallbackHandler => testDeviceCallbackHandler.Dispose());
+                deviceClients.ForEach(deviceClient => deviceClient.Dispose());
+                testDeviceCallbackHandlers.ForEach(testDeviceCallbackHandler => testDeviceCallbackHandler.Dispose());
 
-                    // Clean up the local lists
-                    testDevices.Clear();
-                    deviceClients.Clear();
-                    amqpConnectionStatuses.Clear();
-                }
-            } while (reRunTest && totalRuns < MaxTestRunCount);
-
-            reRunTest.Should().BeFalse($"Device client instances got disconnected in {totalRuns - successfulRuns} runs out of {totalRuns}; current testSuccessRate = {currentSuccessRate}%.");
+                // Clean up the local lists
+                testDevices.Clear();
+                deviceClients.Clear();
+                amqpConnectionStatuses.Clear();
+            }
         }
 
         private class AmqpConnectionStatusChange
