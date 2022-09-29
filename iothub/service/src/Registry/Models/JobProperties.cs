63af26f60adc500d312d88d73dbfabc6a14fda81
Nathanael Gastelum <ngastelum@microsoft.com>
2022-09-29 11:11:05 -0700

    Fix set access level on JobProperties.cs

--- iothub/service/src/Registry/Models/JobProperties.cs
+++ iothub/service/src/Registry/Models/JobProperties.cs
@@ -36,19 +36,19 @@ namespace Microsoft.Azure.Devices
         /// Including a SAS token is dependent on the <see cref="StorageAuthenticationType" /> property.
         /// </remarks>
         [JsonProperty(PropertyName = "outputBlobContainerUri", NullValueHandling = NullValueHandling.Ignore)]
-        public Uri OutputBlobContainerUri { get; protected internal set; }
+        public Uri OutputBlobContainerUri { get; set; }
 
         /// <summary>
         /// Specifies authentication type being used for connecting to storage account.
         /// </summary>
         [JsonProperty(PropertyName = "storageAuthenticationType", NullValueHandling = NullValueHandling.Ignore)]
-        public StorageAuthenticationType? StorageAuthenticationType { get; protected internal set; }
+        public StorageAuthenticationType? StorageAuthenticationType { get; set; }
 
         /// <summary>
         /// The managed identity used to access the storage account for the job.
         /// </summary>
         [JsonProperty(PropertyName = "identity", NullValueHandling = NullValueHandling.Ignore)]
-        public ManagedIdentity Identity { get; protected internal set; }
+        public ManagedIdentity Identity { get; set; }
 
         /// <summary>
         /// Whether or not to include configurations in the job.
@@ -57,7 +57,7 @@ namespace Microsoft.Azure.Devices
         /// The service assumes this is false, if not specified. If true, then configurations are included in the data import/export.
         /// </remarks>
         [JsonProperty(PropertyName = "includeConfigurations", NullValueHandling = NullValueHandling.Ignore)]
-        public bool? IncludeConfigurations { get; protected internal set; }
+        public bool? IncludeConfigurations { get; set; }
 
         /// <summary>
         /// Specifies the name of the blob to use when using configurations.
@@ -66,7 +66,7 @@ namespace Microsoft.Azure.Devices
         /// The service assumes this is configurations.txt, if not specified.
         /// </remarks>
         [JsonProperty(PropertyName = "configurationsBlobName", NullValueHandling = NullValueHandling.Ignore)]
-        public string ConfigurationsBlobName { get; protected internal set; }
+        public string ConfigurationsBlobName { get; set; }
 
         /// <summary>
         /// Represents the percentage of completion.
