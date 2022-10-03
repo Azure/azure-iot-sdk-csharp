d4a9aa2c188094c59ee94b894e091c52d1d3a7ea
Sophia Choi <choisophia@microsoft.com>
2022-10-03 13:55:52 -0700

    Improve logging

--- provisioning/service/samples/Getting Started/IndividualEnrollmentX509Sample/IndividualEnrollmentX509Sample.cs
+++ provisioning/service/samples/Getting Started/IndividualEnrollmentX509Sample/IndividualEnrollmentX509Sample.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
@@ -42,16 +45,22 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
             Query query = _provisioningServiceClient.CreateIndividualEnrollmentQuery("SELECT * FROM enrollments");
             while (query.HasNext())
             {
+                Console.WriteLine("\nQuerying the next enrollments...");
                 QueryResult queryResult = await query.NextAsync();
+                IEnumerable<object> items = queryResult.Items;
+                foreach (IndividualEnrollment enrollment in items.Cast<IndividualEnrollment>())
+                {
+                    Console.WriteLine($"Individual enrollment found: {enrollment.RegistrationId}");
+                }
             }
         }
 
         public async Task CreateIndividualEnrollmentX509Async()
         {
             Console.WriteLine("\nCreating a new individualEnrollment...");
-            X509Attestation _x509 = X509Attestation.CreateFromClientCertificates(_issuerCertificate);
+            X509Attestation x509 = X509Attestation.CreateFromClientCertificates(_issuerCertificate);
             var individualEnrollment = new IndividualEnrollment(
-                _registrationId, _x509)
+                _registrationId, x509)
             {
                 //The following parameters are optional:
                 DeviceId = _deviceId,
@@ -67,9 +76,8 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
                     })
             };
 
-            Console.WriteLine("\nAdding new individual enrollment");
             IndividualEnrollment individualEnrollmentResult = await _provisioningServiceClient.CreateOrUpdateIndividualEnrollmentAsync(individualEnrollment);
-            Console.WriteLine("\nIndividual Enrollment created with success.");
+            Console.WriteLine($"\nIndividual Enrollment {individualEnrollmentResult.RegistrationId} created with success.");
         }
 
         public async Task<IndividualEnrollment> GetIndividualEnrollmentInfoAsync()
@@ -84,17 +92,24 @@ namespace Microsoft.Azure.Devices.Provisioning.Service.Samples
         public async Task UpdateIndividualEnrollmentAsync()
         {
             IndividualEnrollment individualEnrollment = await GetIndividualEnrollmentInfoAsync();
+            Console.WriteLine($"InitialTwinState is {individualEnrollment.InitialTwinState}");
+            Console.WriteLine($"iotEdge Capabilities set to {individualEnrollment.Capabilities.IotEdge}");
             individualEnrollment.InitialTwinState.DesiredProperties["Color"] = "Yellow";
             individualEnrollment.Capabilities = _optionalEdgeCapabilityDisabled;
 
+            Console.WriteLine($"\nUpdating desired properties and capabilities of individual enrollment {individualEnrollment.RegistrationId}");
             IndividualEnrollment individualEnrollmentResult =
                 await _provisioningServiceClient.CreateOrUpdateIndividualEnrollmentAsync(individualEnrollment);
+            Console.WriteLine($"Updated InitialTwinState is {individualEnrollmentResult.InitialTwinState}");
+            Console.WriteLine($"Updated iotEdge Capabilities to {individualEnrollmentResult.Capabilities.IotEdge}");
+            Console.WriteLine($"\nIndividual Enrollment updated with success.");
         }
 
         public async Task DeleteIndividualEnrollmentAsync()
         {
-            Console.WriteLine("\nDeleting the individualEnrollment...");
+            Console.WriteLine($"\nDeleting the individualEnrollment...");
             await _provisioningServiceClient.DeleteIndividualEnrollmentAsync(_registrationId);
+            Console.WriteLine($"\nIndividual Enrollment deleted with success.");
         }
     }
 }
