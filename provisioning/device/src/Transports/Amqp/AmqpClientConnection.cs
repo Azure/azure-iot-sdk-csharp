db9801a9bdb789369d267b05d520bbbf595d8c77
timtay-microsoft <timtay@microsoft.com>
2022-10-03 15:37:20 -0700

    fixup

--- provisioning/device/src/Transports/Amqp/AmqpClientConnection.cs
+++ provisioning/device/src/Transports/Amqp/AmqpClientConnection.cs
@@ -8,8 +8,6 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Amqp;
-using Microsoft.Azure.Amqp.Framing;
-using Microsoft.Azure.Amqp.Sasl;
 using Microsoft.Azure.Amqp.Transport;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client
@@ -18,28 +16,26 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
     {
         private const string Amqpwsb10 = "AMQPWSB10";
         private const string UriSuffix = "/$iothub/websocket";
-        private const string Scheme = "wss";
+        private const string Scheme = "wss://";
         private const string Version = "13";
         private const int WebSocketPort = 443;
         private const int TcpPort = 5671;
 
         private readonly AmqpSettings _amqpSettings;
-        private readonly Uri _uri;
+        private readonly string _host;
         private readonly Action _onConnectionClosed;
         private readonly SemaphoreSlim _connectionSemaphore = new(1, 1);
         private readonly ProvisioningClientAmqpSettings _clientSettings;
 
-        private TaskCompletionSource<TransportBase> _tcs;
         private TransportBase _transport;
-        private ProtocolHeader _sentHeader;
 
         internal AmqpClientConnection(
-            Uri uri,
+            string host,
             AmqpSettings amqpSettings,
             Action onConnectionClosed,
             ProvisioningClientAmqpSettings clientSettings)
         {
-            _uri = uri;
+            _host = host;
             _amqpSettings = amqpSettings;
             _onConnectionClosed = onConnectionClosed;
             _clientSettings = clientSettings;
@@ -47,7 +43,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
             AmqpConnectionSettings = new AmqpConnectionSettings
             {
                 ContainerId = Guid.NewGuid().ToString(),
-                HostName = _uri.Host,
+                HostName = _host,
                 IdleTimeOut = Convert.ToUInt32(_clientSettings.IdleTimeout.TotalMilliseconds),
             };
         }
@@ -84,28 +80,22 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
 
             try
             {
-                string hostName = _uri.Host;
-
                 var tcpSettings = new TcpTransportSettings
                 {
-                    Host = hostName,
-                    Port = _uri.Port != -1
-                    ? _uri.Port
-                    : AmqpConstants.DefaultSecurePort,
+                    Host = _host,
+                    Port = TcpPort,
                 };
 
                 TransportSettings = new TlsTransportSettings(tcpSettings)
                 {
-                    TargetHost = hostName,
-                    Certificate = clientCert,
+                    TargetHost = _host,
                     CertificateValidationCallback = remoteCerificateValidationCallback,
-                    Protocols = _clientSettings.SslProtocols,
                 };
 
                 AmqpTransportInitiator amqpTransportInitiator;
                 if (useWebSocket)
                 {
-                    var websocketUri = new Uri($"{Scheme}{_uri.Host}:{WebSocketPort}{UriSuffix}");
+                    var websocketUri = new Uri($"{Scheme}{_host}:{WebSocketPort}{UriSuffix}");
                     var websocketTransportSettings = new WebSocketTransportSettings
                     {
                         Uri = websocketUri,
@@ -113,21 +103,24 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
                         SubProtocol = Amqpwsb10,
                     };
 
+                    //TODO certificates can't be passed in, so this will fail with x509 auth.
+
                     amqpTransportInitiator = new AmqpTransportInitiator(_amqpSettings, websocketTransportSettings);
                 }
                 else
                 {
-                    var transportSettings = new TcpTransportSettings
+                    var tcpTransportSettings = new TcpTransportSettings
                     {
-                        Host = _uri.Host,
+                        Host = _host,
                         Port = TcpPort,
                     };
 
-                    var tlsTranpsortSettings = new TlsTransportSettings(transportSettings)
+                    var tlsTranpsortSettings = new TlsTransportSettings(tcpTransportSettings)
                     {
-                        TargetHost = _uri.Host,
-                        Certificate = null,
-                        CertificateValidationCallback = _clientSettings.RemoteCertificateValidationCallback
+                        TargetHost = _host,
+                        Certificate = clientCert,
+                        CertificateValidationCallback = _clientSettings.RemoteCertificateValidationCallback,
+                        Protocols = _clientSettings.SslProtocols,
                     };
 
                     amqpTransportInitiator = new AmqpTransportInitiator(_amqpSettings, tlsTranpsortSettings);
@@ -190,84 +183,6 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
                 Logging.Error(this, $"AMQP connection was lost.");
 
             _onConnectionClosed.Invoke();
-        }
-
-        private void OnWriteHeaderComplete(TransportAsyncCallbackArgs args)
-        {
-            if (Logging.IsEnabled)
-                Logging.Enter(this, $"{nameof(AmqpClientConnection)}.{nameof(OnWriteHeaderComplete)}");
-
-            if (args.Exception != null)
-            {
-                CompleteOnException(args);
-                return;
-            }
-
-            byte[] headerBuffer = new byte[AmqpConstants.ProtocolHeaderSize];
-            args.SetBuffer(headerBuffer, 0, headerBuffer.Length);
-            args.CompletedCallback = OnReadHeaderComplete;
-            bool operationPending = args.Transport.ReadAsync(args);
-
-            if (!operationPending)
-            {
-                args.CompletedCallback(args);
-            }
-        }
-
-        private void OnReadHeaderComplete(TransportAsyncCallbackArgs args)
-        {
-            if (Logging.IsEnabled)
-                Logging.Enter(this, $"{nameof(AmqpClientConnection)}.{nameof(OnReadHeaderComplete)}");
-
-            if (args.Exception != null)
-            {
-                CompleteOnException(args);
-                return;
-            }
-
-            try
-            {
-                var receivedHeader = new ProtocolHeader();
-
-                using var byteBuffer = new ByteBuffer(args.Buffer, args.Offset, args.Count);
-                receivedHeader.Decode(byteBuffer);
-
-                if (Logging.IsEnabled)
-                    Logging.Info(this, $"{nameof(AmqpClientConnection)}.{nameof(OnReadHeaderComplete)}: Received Protocol Header: {receivedHeader}");
-
-                if (!receivedHeader.Equals(_sentHeader))
-                {
-                    throw new AmqpException(AmqpErrorCode.NotImplemented, $"The requested protocol version {_sentHeader} is not supported. The supported version is {receivedHeader}");
-                }
-
-                SaslTransportProvider provider = _amqpSettings.GetTransportProvider<SaslTransportProvider>();
-                TransportBase transport = provider.CreateTransport(args.Transport, true);
-                if (Logging.IsEnabled)
-                    Logging.Info(this, $"{nameof(AmqpClientConnection)}.{nameof(OnReadHeaderComplete)}: Created SaslTransportHandler ");
-
-                _tcs.TrySetResult(transport);
-            }
-            catch (Exception ex)
-            {
-                args.Exception = ex;
-                CompleteOnException(args);
-            }
-        }
-
-        private void CompleteOnException(TransportAsyncCallbackArgs args)
-        {
-            if (Logging.IsEnabled)
-                Logging.Enter(this, $"{nameof(AmqpClientConnection)}.{nameof(CompleteOnException)}");
-
-            if (args.Exception != null && args.Transport != null)
-            {
-                if (Logging.IsEnabled)
-                    Logging.Error(this, $"{nameof(AmqpClientConnection)}.{nameof(CompleteOnException)}: Exception thrown {args.Exception.Message}");
-
-                args.Transport.SafeClose(args.Exception);
-                args.Transport = null;
-                _tcs.TrySetException(args.Exception);
-            }
         }
     }
 }
