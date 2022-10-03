d134cd3d2efe991bd93e210ed042c9112a29a842
Sophia JW Choi <113465132+schoims@users.noreply.github.com>
2022-10-03 12:00:51 -0700

    Update provisioning/service/samples/Getting Started/IndividualEnrollmentX509Sample/Parameters.cs
    Co-authored-by: David R. Williamson <drwill@microsoft.com>

--- provisioning/service/samples/Getting Started/IndividualEnrollmentX509Sample/Parameters.cs
+++ provisioning/service/samples/Getting Started/IndividualEnrollmentX509Sample/Parameters.cs
@@ -21,17 +21,6 @@ namespace IndividualEnrollmentX509Sample
             HelpText = "The connection string of device provisioning service. Not required when the PROVISIONING_CONNECTION_STRING environment variable is set.")]
         public string ProvisioningConnectionString { get; set; } = Environment.GetEnvironmentVariable("PROVISIONING_CONNECTION_STRING");
 
-        public static void ValidateProvisioningConnectionString(string provisioningConnectionString)
-        {
-            if (string.IsNullOrWhiteSpace(provisioningConnectionString))
-            {
-                Console.WriteLine("A provisioning connection string needs to be specified, " +
-                    "please set the environment variable \"PROVISIONING_CONNECTION_STRING\" " +
-                    "or pass in \"-c | --ProvisioningConnectionString\" through command line.");
-                Environment.Exit(1);
-            }
-        }
-
         [Option(
             'd',
             "DeviceId",
@@ -45,5 +34,16 @@ namespace IndividualEnrollmentX509Sample
             Required = false,
             HelpText = "The Id of registration.")]
         public string RegistrationId { get; set; } = $"my-registration-{Guid.NewGuid()}";
+        
+        public static void ValidateProvisioningConnectionString(string provisioningConnectionString)
+        {
+            if (string.IsNullOrWhiteSpace(provisioningConnectionString))
+            {
+                Console.WriteLine("A provisioning connection string needs to be specified, " +
+                    "please set the environment variable \"PROVISIONING_CONNECTION_STRING\" " +
+                    "or pass in \"-c | --ProvisioningConnectionString\" through command line.");
+                Environment.Exit(1);
+            }
+        }
     }
 }
