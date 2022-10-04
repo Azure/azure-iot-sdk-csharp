3b6f31e4577b16d809a6ac33f43321f379fad2e4
Nick Patilsen <110431552+patilsnr@users.noreply.github.com>
2022-10-04 16:01:09 -0700

    moved validation helper into utils (#2818)

--- iothub/device/src/Authentication/DeviceAuthenticationWithSharedAccessPolicyKey.cs
+++ iothub/device/src/Authentication/DeviceAuthenticationWithSharedAccessPolicyKey.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using Microsoft.Azure.Devices.Client.Utilities;
 
 namespace Microsoft.Azure.Devices.Client
 {
