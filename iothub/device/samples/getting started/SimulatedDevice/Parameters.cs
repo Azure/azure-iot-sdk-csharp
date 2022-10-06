7a78c3e7a90b942654c58741411f874235f3fa46
Sophia Choi <choisophia@microsoft.com>
2022-10-06 15:27:42 -0700

    Add samples

---
+++ iothub/device/samples/getting started/SimulatedDevice/Parameters.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using CommandLine;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    public enum Transport
+    {
+        Mqtt,
+        Amqp,
+        Http1,
+    };
+
+    /// <summary>
+    /// Command line parameters for the SimulatedDevice sample.
+    /// </summary>
+    internal class Parameters
+    {
+        [Option(
+            'c',
+            "DeviceConnectionString",
+            Required = true,
+            HelpText = "The IoT hub device connection string. This is available by clicking any existing device under the 'Devices' blade in the Azure portal." +
+            "\nDefaults to value of environment variable IOTHUB_DEVICE_CONNECTION_STRING.")]
+        public string DeviceConnectionString { get; set; } = Environment.GetEnvironmentVariable("IOTHUB_DEVICE_CONNECTION_STRING");
+
+        [Option(
+            't',
+            "Transport",
+            Default = Transport.Mqtt,
+            Required = false,
+            HelpText = "The transport to use for the connection.")]
+        public Transport Transport { get; set; }
+
+        [Option(
+            "TransportProtocol",
+            Default = IotHubClientTransportProtocol.Tcp,
+            HelpText = "The transport to use to communicate with the device provisioning instance.")]
+        public IotHubClientTransportProtocol TransportProtocol { get; set; }
+
+        internal IotHubClientTransportSettings GetHubTransportSettings()
+        {
+            return Transport switch
+            {
+                Transport.Mqtt => new IotHubClientMqttSettings(TransportProtocol),
+                Transport.Amqp => new IotHubClientAmqpSettings(TransportProtocol),
+                _ => throw new NotSupportedException($"Unsupported transport type {Transport}/{TransportProtocol}"),
+            };
+        }
+    }
+}
