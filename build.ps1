a7685afd6c4b0ce382794328d773c3d19101b97b
Nicholas Patilsen <npatilsen@microsoft.com>
2022-10-06 10:43:16 -0700

    added to build.ps1

--- build.ps1
+++ build.ps1
@@ -24,6 +24,7 @@ Parameters:
     -framework: Select which framework to run tests on. Allowed values examples include, but are not limited to, "netcoreapp3.1", "net47", "net451"
     -skipIotHubTests: Provide this flag if you want to skip all IoT Hub integration tests
     -skipDPSTests: Provide this flag if you want to skip all DPS integration tests
+    -runSamples: Ensures all SDK samples build and run with exit code 0.
 	
 
 Build will automatically detect if the machine is Windows vs Unix. On Windows development boxes, additional testing on .NET Framework will be performed.
@@ -70,7 +71,8 @@ Param(
     [string] $framework = "*",
     [switch] $skipIotHubTests,
     [switch] $skipDPSTests,
-    [switch] $noBuildBeforeTesting
+    [switch] $noBuildBeforeTesting,
+    [switch] $runSamples
 )
 
 Function IsWindows()
@@ -135,6 +137,84 @@ Function BuildProject($path, $message)
     if (DidBuildFail "./buildlog.txt" -or $LASTEXITCODE -ne 0)
     {
         throw "Build failed: $label"
+    }
+}
+
+Function RunSamples($path, $message)
+{
+    $label = "RUNNING SAMPLES: --- $message $configuration ---"
+
+    Write-Host
+    Write-Host -ForegroundColor Cyan $label
+
+    try {
+            if ($clean)
+            {
+                # Clear device and enrollment
+                RunApp provisioning\service\samples\Getting Started\CleanupEnrollmentsSample "Provisioning\Service\samples\Getting Started\CleanupEnrollmentsSample" "-c ""$env:IOTHUB_CONNECTION_STRING"""
+                RunApp iothub\service\samples\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+                RunApp iothub\service\samples\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:FAR_AWAY_IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+            }
+
+            $sampleRunningTimeInSeconds = 60
+
+            # Run cleanup first so the samples don't get overloaded with old devices
+            RunApp provisioning\service\samples\Getting Started\CleanupEnrollmentsSample "Provisioning\Service\CleanupEnrollmentsSample" "-c ""$env:IOTHUB_CONNECTION_STRING"""
+            RunApp iothub\service\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+            RunApp iothub\service\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:FAR_AWAY_IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+    
+            # Run the iot-hub\device samples
+            RunApp iothub\device\samples\how to guides\DeviceReconnectionSample "IoTHub\Device\DeviceReconnectionSample" "-c ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+            RunApp iothub\device\samples\getting started\FileUploadSample "IoTHub\Device\FileUploadSample" "-c ""$env:IOTHUB_DEVICE_CONN_STRING"""
+            RunApp iothub\device\samples\getting started\MessageReceiveSample "IoTHub\Device\MessageReceiveSample" "-c ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+            RunApp iothub\device\samples\getting started\MethodSample "IoTHub\Device\MethodSample" "-c ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+            RunApp iothub\device\samples\getting started\TwinSample "IoTHub\Device\TwinSample" "-c ""$env:IOTHUB_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+    
+            $pnpDeviceSecurityType = "connectionString"
+            RunApp iothub\device\samples\solutions\PnpDeviceSamples\TemperatureController "IoTHub\Device\PnpDeviceSamples\TemperatureController" "--DeviceSecurityType $pnpDeviceSecurityType -c ""$env:PNP_TC_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+            RunApp iothub\device\samples\solutions\PnpDeviceSamples\Thermostat "IoTHub\Device\PnpDeviceSamples\Thermostat" "--DeviceSecurityType $pnpDeviceSecurityType -c ""$env:PNP_THERMOSTAT_DEVICE_CONN_STRING"" -r $sampleRunningTimeInSeconds"
+    
+            # Run the iot-hub\service samples
+            $deviceId = ($Env:IOTHUB_DEVICE_CONN_STRING.Split(';') | Where-Object {$_ -like "DeviceId=*"}).Split("=")[1]
+            $iothubHost = ($Env:IOTHUB_CONNECTION_STRING.Split(';') | Where-Object {$_ -like "HostName=*"}).Split("=")[1]
+    
+            RunApp iothub\service\samples\how to guides\AutomaticDeviceManagementSample "IoTHub\Service\AutomaticDeviceManagementSample"
+    
+            Write-Warning "Using device $deviceId for the AzureSasCredentialAuthenticationSample."
+            RunApp iothub\service\samples\how to guides\AzureSasCredentialAuthenticationSample "IoTHub\Service\AzureSasCredentialAuthenticationSample" "-r $iothubHost -d $deviceId -s ""$env:IOT_HUB_SAS_KEY"" -n ""$env:IOT_HUB_SAS_KEY_NAME"""
+    
+            RunApp iothub\service\samples\getting started\EdgeDeploymentSample "IoTHub\Service\EdgeDeploymentSample"
+            RunApp iothub\service\samples\getting started\JobsSample "IoTHub\Service\JobsSample"
+            RunApp iothub\service\samples\getting started\RegistryManagerSample "IoTHub\Service\RegistryManagerSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -p ""$env:IOTHUB_X509_DEVICE_PFX_THUMBPRINT"""
+    
+            Write-Warning "Using device $deviceId for the RoleBasedAuthenticationSample."
+            RunApp iothub\service\samples\how to guides\RoleBasedAuthenticationSample "IoTHub\Service\RoleBasedAuthenticationSample" "-h $iothubHost -d $deviceId --ClientId ""$env:E2E_TEST_AAD_APP_CLIENT_ID"" --TenantId ""$env:MSFT_TENANT_ID"" --ClientSecret ""$env:E2E_TEST_AAD_APP_CLIENT_SECRET"""
+    
+            Write-Warning "Using device $deviceId for the ServiceClientSample."
+            RunApp iothub\service\samples\getting started\ServiceClientSample "IoTHub\Service\ServiceClientSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -d $deviceId -r $sampleRunningTimeInSeconds"
+    
+            # Run provisioning\device samples
+    
+            # ComputeDerivedSymmetricKeySample uses the supplied group enrollment key to compute the SHA256 based hash of the supplied device Id.
+            # For the sake of running this sample on the pipeline, we will only test the hash computation by passing in a base-64 string and a string to be hashed.
+            RunApp provisioning\device\samples\getting started\ComputeDerivedSymmetricKeySample "Provisioning\Device\ComputeDerivedSymmetricKeySample" "-d ""$env:DPS_SYMMETRIC_KEY_INDIVIDUAL_ENROLLMENT_REGISTRATION_ID"" -p ""$env:DPS_SYMMETRIC_KEY_INDIVIDUAL_ENROLLEMNT_PRIMARY_KEY"""
+    
+            RunApp provisioning\device\samples\how to\SymmetricKeySample "Provisioning\Device\SymmetricKeySample" "-s ""$env:DPS_IDSCOPE"" -i ""$env:DPS_SYMMETRIC_KEY_INDIVIDUAL_ENROLLMENT_REGISTRATION_ID"" -p ""$env:DPS_SYMMETRIC_KEY_INDIVIDUAL_ENROLLEMNT_PRIMARY_KEY"""
+    
+            # Run provisioning\service samples
+            RunApp provisioning\service\samples\how to\BulkOperationSample "Provisioning\Service\BulkOperationSample"
+            RunApp provisioning\service\samples\getting started\EnrollmentSample "Provisioning\Service\EnrollmentSample"
+    
+            # Run the cleanup again so that identities and enrollments created for the samples are cleaned up.
+            RunApp provisioning\service\samples\Getting Started\CleanupEnrollmentsSample "Provisioning\Service\CleanupEnrollmentsSample" "-c ""$env:IOTHUB_CONNECTION_STRING"""
+            RunApp iothub\service\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+            RunApp iothub\service\how to guides\CleanupDevicesSample "IoTHub\Service\CleanupDevicesSample" "-c ""$env:FAR_AWAY_IOTHUB_CONNECTION_STRING"" -a ""$env:STORAGE_ACCOUNT_CONNECTION_STRING"" --PathToDevicePrefixForDeletion ""$env:PATH_TO_DEVICE_PREFIX_FOR_DELETION_FILE"""
+    }  
+    catch [Exception]{
+        throw "Running Samples Failed: $label"
+    }
+    finally {
+        Set-Location $rootDir
     }
 }
 
@@ -267,6 +347,11 @@ try
         # SDK binaries
         BuildProject . "Azure IoT C# SDK Solution"
         $env:AZURE_IOT_LOCALPACKAGES = $packageTempPath
+    }
+
+    if ($runSamples)
+    {
+        RunSamples . "Azure IoT C# SDK Samples"
     }
 
     # Unit Tests require InternalsVisibleTo and can only run in Debug builds.
