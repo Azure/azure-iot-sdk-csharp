b9f6b55a2b7cc862c505a78c8e259d92ef68d5fa
tmahmood-microsoft <tmahmood@microsoft.com>
2022-10-06 14:40:58 -0700

    cr changes

--- provisioning/device/src/Transports/Amqp/ClientWebSocketTransport.cs
+++ provisioning/device/src/Transports/Amqp/ClientWebSocketTransport.cs
@@ -88,6 +88,10 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
             {
                 throw new IOException(webSocketException.Message, webSocketException);
             }
+            catch (HttpListenerException httpListenerException)
+            {
+                throw new IOException(httpListenerException.Message, httpListenerException);
+            }
             finally
             {
                 if (!succeeded)
@@ -135,6 +139,10 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
             catch (WebSocketException webSocketException)
             {
                 throw new IOException(webSocketException.Message, webSocketException);
+            }
+            catch (HttpListenerException httpListenerException)
+            {
+                throw new IOException(httpListenerException.Message, httpListenerException);
             }
             finally
             {
