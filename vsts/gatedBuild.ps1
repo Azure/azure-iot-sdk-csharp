dbc0ae5d5f491f7547761e107dcfaea1f69a27cd
Nathanael Gastelum <ngastelum@microsoft.com>
2022-10-27 16:55:44 -0700

    Remove TPM tests from PR gate

--- vsts/gatedBuild.ps1
+++ vsts/gatedBuild.ps1
@@ -33,6 +33,7 @@ if (IsPullRequestBuild)
 {
 	Write-Host "Pull request build detected, will run pr tests"
 	$runTestCmd += " -prtests"
+	$runTestCmd += " -skipTPMTests"
 
 	if (ShouldSkipDPSTests)
 	{
