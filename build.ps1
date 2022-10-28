d9b8c0f218059202e297b23dcf15a61e33377e1b
Nicholas Patilsen <npatilsen@microsoft.com>
2022-10-28 13:48:05 -0700

    test sample runs p2

--- build.ps1
+++ build.ps1
@@ -171,7 +171,7 @@ Function RunSamples($path, $message)
     
             RunSample 'iothub\service\samples\getting started\EdgeDeploymentSample' "IoTHub\Service\EdgeDeploymentSample"
             RunSample 'iothub\service\samples\getting started\JobsSample' "IoTHub\Service\JobsSample"
-            RunSample 'iothub\service\samples\getting started\RegistryManagerSample' "IoTHub\Service\RegistryManagerSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -p ""$env:IOTHUB_X509_DEVICE_PFX_THUMBPRINT"""
+            RunSample 'iothub\service\samples\how to guides\RegistryManagerSample' "IoTHub\Service\RegistryManagerSample" "-c ""$env:IOTHUB_CONNECTION_STRING"" -p ""$env:IOTHUB_X509_DEVICE_PFX_THUMBPRINT"""
 
             Write-Warning "Using device $deviceId for the RoleBasedAuthenticationSample."
             RunSample 'iothub\service\samples\how to guides\RoleBasedAuthenticationSample' "IoTHub\Service\RoleBasedAuthenticationSample" "-h $iothubHost -d $deviceId --ClientId ""$env:E2E_TEST_AAD_APP_CLIENT_ID"" --TenantId ""$env:MSFT_TENANT_ID"" --ClientSecret ""$env:E2E_TEST_AAD_APP_CLIENT_SECRET"""
