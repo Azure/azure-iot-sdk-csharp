f999749b3033d238514ea4376fdd76172d81b534
David R. Williamson <drwill@microsoft.com>
2022-11-22 10:58:22 -0800

    Fix logic in some new code

--- provisioning/device/src/Transports/Mqtt/ProvisioningTransportHandlerMqtt.cs
+++ provisioning/device/src/Transports/Mqtt/ProvisioningTransportHandlerMqtt.cs
@@ -476,12 +476,9 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
 
         private static bool ContainsAuthenticationException(Exception ex)
         {
-            while (ex != null)
-            {
-                return ex is AuthenticationException || ContainsAuthenticationException(ex.InnerException);
-            }
-
-            return false;
+            return ex != null &&
+                (ex is AuthenticationException
+                    || ContainsAuthenticationException(ex.InnerException));
         }
     }
 }
