4ae699295926b10f9dc803edb9ee5e53eb5d0cb3
ngastelum-ms <109701188+ngastelum-ms@users.noreply.github.com>
2022-12-15 10:02:51 -0800

    Fix error CA1854, Prefer a 'TryGetValue' call over a Dictionary indexer access guarded by a 'ContainsKey' check to avoid double lookup

--- iothub/device/src/Messaging/TelemetryMessage.cs
+++ iothub/device/src/Messaging/TelemetryMessage.cs
@@ -239,9 +239,7 @@ namespace Microsoft.Azure.Devices.Client
 
         private T GetSystemProperty<T>(string key)
         {
-            return SystemProperties.ContainsKey(key)
-                ? (T)SystemProperties[key]
-                : default;
+            return SystemProperties.TryGetValue(key, out object value) ? (T)value : default;
         }
     }
 }
