7f09ba384de8e5e1ee862ec309b13ef36ec71747
Sophia Choi <choisophia@microsoft.com>
2023-01-04 18:12:45 -0800

    remove unused import statements

--- iothub/service/tests/ConfigurationsClientTests.cs
+++ iothub/service/tests/ConfigurationsClientTests.cs
@@ -6,13 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Threading;
 using FluentAssertions;
-using System.Security.Cryptography;
 using Azure;
 
 namespace Microsoft.Azure.Devices.Tests
