4cc76051057e088eebd0eed54d38b3ec36c6b974
ngastelum-ms <109701188+ngastelum-ms@users.noreply.github.com>
2023-01-18 11:51:17 -0800

    DPS device coverage (#3068)
    * Add unit tests
    
    * Cleanup
    
    * Update tests
    
    * Fix default ctor test
    
    * Cleanup

---
+++ provisioning/device/tests/AuthenticationProviderX509Tests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class AuthenticationProviderX509Tests
+    {
+        [TestMethod]
+        public void AuthenticationProviderX509_ThrowsWhenMissingCert()
+        {
+            // arrange - act
+            Func<AuthenticationProviderX509> act = () => new AuthenticationProviderX509(null);
+
+            // assert
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ provisioning/device/tests/ProvisioningTransportRegisterRequestTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class ProvisioningTransportRegisterRequestTests
+    {
+
+        private readonly string _globalDeviceEndpoint = "endpoint";
+        private readonly string _idScope = "id";
+
+        [TestMethod]
+        public void ProvisioningTransportRegisterRequest_DefaultPayload()
+        {
+            // arrange - act
+            var auth = new Mock<AuthenticationProvider>();
+
+            var request = new ProvisioningTransportRegisterRequest(
+                _globalDeviceEndpoint,
+                _idScope,
+                auth.Object);
+
+            // assert
+            request.GlobalDeviceEndpoint.Should().Be(_globalDeviceEndpoint);
+            request.IdScope.Should().Be(_idScope);
+            request.Payload.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void ProvisioningTransportRegisterRequest_WithPayload()
+        {
+            // arrange - act
+            var auth = new Mock<AuthenticationProvider>();
+
+            var request = new ProvisioningTransportRegisterRequest(
+                _globalDeviceEndpoint, 
+                _idScope,
+                auth.Object,
+                new RegistrationRequestPayload());
+
+            // assert
+            request.GlobalDeviceEndpoint.Should().Be(_globalDeviceEndpoint);
+            request.IdScope.Should().Be(_idScope);
+            request.Payload.Should().BeEquivalentTo(new RegistrationRequestPayload());
+        }
+    }
+}
