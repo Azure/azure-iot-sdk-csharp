04d5790c08cb5aa71ea85aa6fb89f72630573ee3
Sophia Choi <choisophia@microsoft.com>
2023-01-25 14:20:37 -0800

    Added more tests

--- iothub/service/tests/DeserializationTests.cs
+++ iothub/service/tests/DeserializationTests.cs
@@ -287,11 +287,11 @@ namespace Microsoft.Azure.Devices.Tests
                                                 new TimeSpan(1, 0, 0));
             const string JsonString = @"
 {
-  ""originalMessageId"": ""1"",
-  ""deviceGenerationId"": ""2"",
+  ""originalMessageId"": 1,
+  ""deviceGenerationId"": 2,
   ""deviceId"": ""testDeviceId"",
   ""enqueuedTimeUtc"": ""1/20/2023 8:06:32 AM +01:00"",
-  ""statusCode"": ""0"",
+  ""statusCode"": 0,
   ""description"": ""Success"",
 }";
             FeedbackRecord feedbackRecord = JsonConvert.DeserializeObject<FeedbackRecord>(JsonString);
@@ -322,7 +322,7 @@ namespace Microsoft.Azure.Devices.Tests
   ""blobName"": ""testBlob"",
   ""lastUpdatedTime"": ""1/19/2023 8:07:32 AM +01:00"", 
   ""enqueuedTimeUtc"": ""1/20/2023 8:06:32 AM +01:00"",
-  ""blobSizeInBytes"": ""50""
+  ""blobSizeInBytes"": 50
 }";
             FileUploadNotification fileUploadNotification = JsonConvert.DeserializeObject<FileUploadNotification>(jsonString);
             fileUploadNotification.BlobUriPath.Should().Be(expectedBlobUri);
@@ -333,30 +333,148 @@ namespace Microsoft.Azure.Devices.Tests
             fileUploadNotification.LastUpdatedOnUtc.Should().Be(lastUpdatedOnUtc);
         }
 
-//        [TestMethod]
-//        public void BasicDigitalTwin_Deserialize_Ok()
-//        {
-//            const string ExpectedId = "twinId1234";
-//            var ExpectedMetaData = new DigitalTwinMetadata
-//            {
+        [TestMethod]
+        public void BasicDigitalTwin_Deserialize_Ok()
+        {
+            const string ExpectedTwinId = "twinId1234";
+            const string ExpectedModelId = "modelId1234";
+            var ExpectedMetaData = new DigitalTwinMetadata
+            {
+                ModelId = "modelId1234"
+            };
+            const string jsonString = @"
+            {
+              ""$dtId"": ""twinId1234"",
+              ""$metadata"": {
+                   ""$model"": ""modelId1234""
+                }
+            }";
+            BasicDigitalTwin basicDigitalTwin = JsonConvert.DeserializeObject<BasicDigitalTwin>(jsonString);
+            basicDigitalTwin.Id.Should().Be(ExpectedTwinId);
+            basicDigitalTwin.Metadata.Should().BeEquivalentTo(ExpectedMetaData);
+            basicDigitalTwin.Metadata.ModelId.Should().Be(ExpectedModelId);
+            basicDigitalTwin.Metadata.WritableProperties.Should().NotBeNull();
+        }
 
-//            }
-//            const long BlobSizeInBytes = 50;
-//            const string jsonString = @"
-//{
-//  ""$dtId"": ""twinId1234"",
-//  ""$metadata"": ""testBlob"",
-//  ""lastUpdatedTime"": ""1/19/2023 8:07:32 AM +01:00"", 
-//  ""enqueuedTimeUtc"": ""1/20/2023 8:06:32 AM +01:00"",
-//  ""blobSizeInBytes"": ""50""
-//}";
-//            FileUploadNotification fileUploadNotification = JsonConvert.DeserializeObject<FileUploadNotification>(jsonString);
-//            fileUploadNotification.BlobUriPath.Should().Be(BlobUri);
-//            fileUploadNotification.BlobName.Should().Be(BlobName);
-//            fileUploadNotification.DeviceId.Should().Be(DeviceId);
-//            fileUploadNotification.BlobSizeInBytes.Should().Be(BlobSizeInBytes);
-//            fileUploadNotification.EnqueuedOnUtc.Should().Be(enqueuedTimeUtc);
-//            fileUploadNotification.LastUpdatedOnUtc.Should().Be(lastUpdatedOnUtc);
-//        }
+        [TestMethod]
+        public void BasicDigitalTwin_WithCustomProperties_Deserialize_Ok()
+        {
+            const string ExpectedTwinId = "twinId1234";
+            const string ExpectedModelId = "modelId1234";
+
+            const string jsonString = @"
+            {
+              ""$dtId"": ""twinId1234"",
+              ""$metadata"": {
+                   ""$model"": ""modelId1234"",
+                   ""additionalKey"": ""value""
+                },
+              ""desiredValue"": ""sampleValue"",
+              ""desiredVersion"": 1,
+              ""ackVersion"": 1,
+              ""ackCode"": 200,
+              ""ackDescription"": ""Ack Description"",
+            }";
+
+            BasicDigitalTwin basicDigitalTwin = JsonConvert.DeserializeObject<BasicDigitalTwin>(jsonString);
+            basicDigitalTwin.Id.Should().Be(ExpectedTwinId);
+            basicDigitalTwin.CustomProperties.Should().HaveCount(5);
+            basicDigitalTwin.Metadata.WritableProperties.Should().HaveCount(1);
+            basicDigitalTwin.Metadata.ModelId.Should().Be(ExpectedModelId);
+        }
+
+        [TestMethod]
+        public void WritableProperty_Deserialize_Ok()
+        {
+            const string ExpectedDesiredValue = "sampleValue";
+            const int ExpectedDesiredVersion = 1;
+            const int ExpectedAckVersion = 1;
+            const int ExpectedAckCode = 200;
+            const string ExpectedAckDescription = "Ack Description";
+
+            var lastUpdateTime = new DateTimeOffset(2023, 1, 20, 8, 6, 32,
+                        new TimeSpan(1, 0, 0));
+
+            const string jsonMetadataString = @"
+            {
+                 ""$model"": ""modelId1234"",
+                 ""desiredValue"": ""sampleValue"",
+                 ""desiredVersion"": 1,
+                 ""ackVersion"": 1,
+                 ""ackCode"": 200,
+                 ""ackDescription"": ""Ack Description"",
+                 ""lastUpdateTime"": ""1/20/2023 8:06:32 AM +01:00""
+            }";
+            WritableProperty writableProperty = JsonConvert.DeserializeObject<WritableProperty>(jsonMetadataString);
+            writableProperty.DesiredValue.Should().Be(ExpectedDesiredValue);
+            writableProperty.DesiredVersion.Should().Be(ExpectedDesiredVersion);
+            writableProperty.AckVersion.Should().Be(ExpectedAckVersion);
+            writableProperty.AckCode.Should().Be(ExpectedAckCode);
+            writableProperty.AckDescription.Should().Be(ExpectedAckDescription);
+            writableProperty.LastUpdatedOnUtc.Should().Be(lastUpdateTime);
+        }
+
+        [TestMethod]
+        public void ComponentMetadata_Deserialize_Ok()
+        {
+            const string ExpectedKey1Value = "sampleValue";
+            const int ExpectedKey2Value = 1;
+            var ExpectedKey3Value = new DateTimeOffset(2023, 1, 20, 8, 6, 32,
+                        new TimeSpan(1, 0, 0));
+
+            const string jsonMetadataString = @"
+            {
+                 ""key1"": ""sampleValue"",
+                 ""key2"": 1,
+                 ""key3"": ""1/20/2023 8:06:32 AM +01:00""
+            }";
+            ComponentMetadata componentMetadata = JsonConvert.DeserializeObject<ComponentMetadata>(jsonMetadataString);
+            componentMetadata.WritableProperties["key1"].Should().Be(ExpectedKey1Value);
+            componentMetadata.WritableProperties["key2"].Should().Be(ExpectedKey2Value);
+            componentMetadata.WritableProperties["key3"].Should().Be(ExpectedKey3Value);
+        }
+
+        [TestMethod]
+        public void CloudToDeviceMethodScheduledJob_Deserialize_Ok()
+        {
+            const string ExpectedMethod = "testMethod";
+            const string ExpectedPayload = "testPayload";
+            const string jsonString = @"
+            {
+              ""cloudToDeviceMethod"": {
+                    ""methodName"": ""testMethod"",
+                    ""payload"": ""testPayload""
+                }
+            }";
+
+            CloudToDeviceMethodScheduledJob job = JsonConvert.DeserializeObject<CloudToDeviceMethodScheduledJob>(jsonString);
+            job.DirectMethodRequest.MethodName.Should().Be(ExpectedMethod);
+            job.DirectMethodRequest.Payload.Should().Be(ExpectedPayload);
+        }
+
+        [TestMethod]
+        public void DeviceJobStatistics_Deserialize_Ok()
+        {
+            const int ExpectedDeviceCount = 100;
+            const int ExpectedFailedCount = 50;
+            const int ExpectedRunningCount = 20;
+            const int ExpectedSucceededCount = 0;
+            const int ExpectedPendingCount = 30;
+            const string jsonString = @"
+            {
+              ""deviceCount"": 100,
+              ""failedCount"": 50,
+              ""SucceededCount"": 0,
+              ""runningCount"": 20,
+              ""pendingCount"": 30,
+            }";
+
+            DeviceJobStatistics statistics = JsonConvert.DeserializeObject<DeviceJobStatistics>(jsonString);
+            statistics.DeviceCount.Should().Be(ExpectedDeviceCount);
+            statistics.FailedCount.Should().Be(ExpectedFailedCount);
+            statistics.SucceededCount.Should().Be(ExpectedSucceededCount);
+            statistics.RunningCount.Should().Be(ExpectedRunningCount);
+            statistics.PendingCount.Should().Be(ExpectedPendingCount);
+        }
     }
 }
