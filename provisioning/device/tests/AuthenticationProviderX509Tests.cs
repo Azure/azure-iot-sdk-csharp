b8e78d2944b15cead96060c13e6cc801115a1e6f
ngastelum-ms <109701188+ngastelum-ms@users.noreply.github.com>
2023-01-30 13:43:14 -0800

    Cleanup and add more unit tests

--- provisioning/device/tests/AuthenticationProviderX509Tests.cs
+++ provisioning/device/tests/AuthenticationProviderX509Tests.cs
@@ -1,9 +1,11 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+﻿using Microsoft.Azure.Devices.Provisioning.Client;
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
 {
@@ -11,6 +13,11 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
     [TestCategory("Unit")]
     public class AuthenticationProviderX509Tests
     {
+#pragma warning disable SYSLIB0026 // Type or member is obsolete
+        private static readonly X509Certificate2 s_cert = new();
+#pragma warning restore SYSLIB0026 // Type or member is obsolete
+        private static readonly X509Certificate2Collection s_certs = new();
+
         [TestMethod]
         public void AuthenticationProviderX509_ThrowsWhenMissingCert()
         {
@@ -19,6 +26,19 @@ namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
 
             // assert
             act.Should().Throw<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void AuthenticationProviderX509_Works()
+        {
+            // arrange - act
+            var authProvider = new AuthenticationProviderX509(s_cert, s_certs);
+
+            // assert
+            authProvider.Should().NotBeNull();
+            authProvider.ClientCertificate.Should().Be(s_cert);
+            authProvider.CertificateChain.Should().BeEquivalentTo(s_certs);
+            authProvider.GetRegistrationId().Should().Be(s_cert.GetNameInfo(X509NameType.DnsName, false)); // TODO: fix or remove
         }
     }
 }
---
+++ provisioning/device/tests/X509RegistrationResultTests.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.UnitTests
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class X509RegistrationResultTests
+    {
+        private static readonly string s_enrollmentGroupId = "testing-enrollment-group-id";
+        private static readonly X509CertificateInfo s_certificateInfo = new();
+
+        [TestMethod]
+        public void X509RegistrationResult_CertificateInfo()
+        {
+            // arrange
+
+            var source = new X509RegistrationResult
+            {
+                CertificateInfo = s_certificateInfo,
+            };
+            string body = JsonConvert.SerializeObject(source);
+
+            // act
+            X509RegistrationResult result = JsonConvert.DeserializeObject<X509RegistrationResult>(body);
+
+            // assert
+            result.CertificateInfo.Should().BeEquivalentTo(s_certificateInfo);
+        }
+
+        [TestMethod]
+        public void X509RegistrationResult_EnrollmentGroupId()
+        {
+            // arrange
+
+            var source = new X509RegistrationResult
+            {
+                EnrollmentGroupId = s_enrollmentGroupId,
+            };
+            string body = JsonConvert.SerializeObject(source);
+
+            // act
+            X509RegistrationResult result = JsonConvert.DeserializeObject<X509RegistrationResult>(body);
+
+            // assert
+            result.EnrollmentGroupId.Should().Be(s_enrollmentGroupId);
+        }
+
+        [TestMethod]
+        public void X509RegistrationResult_SigningCertificateInfo()
+        {
+            // arrange
+
+            var source = new X509RegistrationResult
+            {
+                SigningCertificateInfo = s_certificateInfo,
+            };
+            string body = JsonConvert.SerializeObject(source);
+
+            // act
+            X509RegistrationResult result = JsonConvert.DeserializeObject<X509RegistrationResult>(body);
+
+            // assert
+            result.SigningCertificateInfo.Should().BeEquivalentTo(s_certificateInfo);
+        }
+    }
+}
