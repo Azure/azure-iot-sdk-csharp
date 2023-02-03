be143966fe03d57fc6f2d1b9668f6f2158f2a6a0
David R. Williamson <drwill@microsoft.com>
2023-02-03 15:08:04 -0800

    Speed up the loading of environment variables for E2E tests

--- e2e/test/prerequisites/E2ETestsSetup/LoadEnvironmentVariablesFromKeyVault.ps1
+++ e2e/test/prerequisites/E2ETestsSetup/LoadEnvironmentVariablesFromKeyVault.ps1
@@ -40,14 +40,14 @@ Connect-AzureSubscription | Out-Null
 # Load all secrets from KeyVault and set the appropriate environment variable
 $ids = az keyvault secret list --subscription $SubscriptionId --vault-name $KeyVaultName --query '[*].id' --output tsv
 Write-Host "`nSetting environment variables:"
-foreach ($id in $ids)
-{
+$ids | ForEach-Object -Parallel {
     # az keyvault secret show does not return a name in its properties so we need to extract it from the id
-    # The ids have parts seperated by / and extracting the 5th part gets us the name of the Key
+    $value = az keyvault secret show --subscription $using:SubscriptionId --id $_ --query 'value' --output tsv
+
+    # The ids have parts separated by a '/' and extracting the 5th part gets us the name of the Key
     # After we extract the name of the Key, we also want to replace dashes with underscores so that we convert it to the correct Environment Vairable name to be set
     # Ex: https://test-kv.vault.azure.net/secrets/IOTHUB-NAME/f7953b248a3e46cfa6fc59651794db2e - Extract IOTHUB-NAME and convert to IOTHUB_NAME
-    $name = $id.split('/')[4].Replace('-', '_')
-    Write-Host "`t$name"
-    $value = az keyvault secret show --subscription $SubscriptionId --id $id --query 'value' --output tsv
-    Set-Item -Path Env:$name -Value "$value"
+    $envVarName = $_.split('/')[4].Replace('-', '_')
+    Write-Host "`t$envVarName"
+    Set-Item -Path Env:$envVarName -Value "$value"
 }
