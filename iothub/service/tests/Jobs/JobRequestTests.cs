bd1022dd87c4d543185b9a63a0296f0dcd872452
Nick Patilsen <npatilsen@microsoft.com>
2023-02-03 10:10:54 -0800

    new classes p1

---
+++ iothub/service/tests/Jobs/JobRequestTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Devices.Tests.Jobs
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class JobRequestTests
+    {
+        [TestMethod]
+        public void JobRequest_FieldInitialization()
+        {
+            // arrange
+            var request = new JobRequest();
+
+            // act
+            long? maxExecutionTimeInSeconds = request.MaxExecutionTimeInSeconds;
+
+            // assert
+            maxExecutionTimeInSeconds.Should().Be(null);
+
+            // rearrange
+            request.MaxExecutionTimeInSeconds = 5L;
+
+            // assert
+            request.MaxExecutionTimeInSeconds.Should().Be(5L);
+        }
+    }
+}
---
+++ iothub/service/tests/Jobs/ScheduledJobsOptionsTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Devices.Tests.Jobs
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class ScheduledJobsOptionsTests
+    {
+        [TestMethod]
+        public void ScheduledJobsOptions_FieldInitialization()
+        {
+            // arrange
+            var options = new ScheduledJobsOptions();
+
+            // act
+            options.MaxExecutionTime = TimeSpan.FromSeconds(1);
+
+            // assert
+            options.MaxExecutionTimeInSeconds.Should().Be(1);
+        }
+    }
+}
