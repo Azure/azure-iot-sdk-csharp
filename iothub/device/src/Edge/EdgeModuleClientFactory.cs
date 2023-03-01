09c6f87f2bb3e949dc6f956bcfb62206ecaa6d5b
David R. Williamson <drwill@microsoft.com>
2023-02-28 17:35:28 -0800

    Edge module creation variable name changes

--- iothub/device/src/Edge/EdgeModuleClientFactory.cs
+++ iothub/device/src/Edge/EdgeModuleClientFactory.cs
@@ -19,16 +19,16 @@ namespace Microsoft.Azure.Devices.Client.Edge
     internal class EdgeModuleClientFactory
     {
         private const string DefaultApiVersion = "2018-06-28";
-        private const string IotEdgedUriVariableName = "IOTEDGE_WORKLOADURI";
-        private const string IotHubHostnameVariableName = "IOTEDGE_IOTHUBHOSTNAME";
-        private const string GatewayHostnameVariableName = "IOTEDGE_GATEWAYHOSTNAME";
+        private const string IotEdgeWorkloadUriVariableName = "IOTEDGE_WORKLOADURI";
+        private const string IotHubHostNameVariableName = "IOTEDGE_IOTHUBHOSTNAME";
+        private const string GatewayHostNameVariableName = "IOTEDGE_GATEWAYHOSTNAME";
         private const string DeviceIdVariableName = "IOTEDGE_DEVICEID";
         private const string ModuleIdVariableName = "IOTEDGE_MODULEID";
         private const string ModuleGenerationIdVariableName = "IOTEDGE_MODULEGENERATIONID";
         private const string AuthSchemeVariableName = "IOTEDGE_AUTHSCHEME";
         private const string SasTokenAuthScheme = "SasToken";
-        private const string EdgehubConnectionstringVariableName = "EdgeHubConnectionString";
-        private const string IothubConnectionstringVariableName = "IotHubConnectionString";
+        private const string EdgeHubConnectionStringVariableName = "EdgeHubConnectionString";
+        private const string IotHubConnectionStringVariableName = "IotHubConnectionString";
         private const string EdgeCaCertificateFileVariableName = "EdgeModuleCACertificateFile";
 
         private readonly ITransportSettings[] _transportSettings;
@@ -60,14 +60,17 @@ namespace Microsoft.Azure.Devices.Client.Edge
         {
             IDictionary envVariables = Environment.GetEnvironmentVariables();
 
-            string connectionString = GetValueFromEnvironment(envVariables, EdgehubConnectionstringVariableName) ?? GetValueFromEnvironment(envVariables, IothubConnectionstringVariableName);
+            string connectionString = GetValueFromEnvironment(envVariables, EdgeHubConnectionStringVariableName)
+                ?? GetValueFromEnvironment(envVariables, IotHubConnectionStringVariableName);
 
-            // First try to create from connection string and if env variable for connection string is not found try to create from edgedUri
+            ICertificateValidator certificateValidator = NullCertificateValidator.Instance;
+
+            // First try to create from connection string and if env variable for connection string is not found try to
+            // create from IOTEDGE_WORKLOADURI.
             if (!string.IsNullOrWhiteSpace(connectionString))
             {
                 string certPath = Environment.GetEnvironmentVariable(EdgeCaCertificateFileVariableName);
 
-                ICertificateValidator certificateValidator = NullCertificateValidator.Instance;
                 if (!string.IsNullOrWhiteSpace(certPath))
                 {
                     Debug.WriteLine("EdgeModuleClientFactory setupTrustBundle from file");
@@ -77,42 +80,39 @@ namespace Microsoft.Azure.Devices.Client.Edge
 
                 return new ModuleClient(CreateInternalClientFromConnectionString(connectionString, _options), certificateValidator);
             }
-            else
+
+            string edgeWorkloadUri = GetValueFromEnvironment(envVariables, IotEdgeWorkloadUriVariableName) ?? throw new InvalidOperationException($"Environment variable {IotEdgeWorkloadUriVariableName} is required.");
+            string deviceId = GetValueFromEnvironment(envVariables, DeviceIdVariableName) ?? throw new InvalidOperationException($"Environment variable {DeviceIdVariableName} is required.");
+            string moduleId = GetValueFromEnvironment(envVariables, ModuleIdVariableName) ?? throw new InvalidOperationException($"Environment variable {ModuleIdVariableName} is required.");
+            string hostname = GetValueFromEnvironment(envVariables, IotHubHostNameVariableName) ?? throw new InvalidOperationException($"Environment variable {IotHubHostNameVariableName} is required.");
+            string authScheme = GetValueFromEnvironment(envVariables, AuthSchemeVariableName) ?? throw new InvalidOperationException($"Environment variable {AuthSchemeVariableName} is required.");
+            string generationId = GetValueFromEnvironment(envVariables, ModuleGenerationIdVariableName) ?? throw new InvalidOperationException($"Environment variable {ModuleGenerationIdVariableName} is required.");
+            string gateway = GetValueFromEnvironment(envVariables, GatewayHostNameVariableName);
+
+            if (!StringComparer.OrdinalIgnoreCase.Equals(authScheme, SasTokenAuthScheme))
             {
-                string edgedUri = GetValueFromEnvironment(envVariables, IotEdgedUriVariableName) ?? throw new InvalidOperationException($"Environment variable {IotEdgedUriVariableName} is required.");
-                string deviceId = GetValueFromEnvironment(envVariables, DeviceIdVariableName) ?? throw new InvalidOperationException($"Environment variable {DeviceIdVariableName} is required.");
-                string moduleId = GetValueFromEnvironment(envVariables, ModuleIdVariableName) ?? throw new InvalidOperationException($"Environment variable {ModuleIdVariableName} is required.");
-                string hostname = GetValueFromEnvironment(envVariables, IotHubHostnameVariableName) ?? throw new InvalidOperationException($"Environment variable {IotHubHostnameVariableName} is required.");
-                string authScheme = GetValueFromEnvironment(envVariables, AuthSchemeVariableName) ?? throw new InvalidOperationException($"Environment variable {AuthSchemeVariableName} is required.");
-                string generationId = GetValueFromEnvironment(envVariables, ModuleGenerationIdVariableName) ?? throw new InvalidOperationException($"Environment variable {ModuleGenerationIdVariableName} is required.");
-                string gateway = GetValueFromEnvironment(envVariables, GatewayHostnameVariableName);
+                throw new InvalidOperationException($"Unsupported authentication scheme. Supported scheme is {SasTokenAuthScheme}.");
+            }
 
-                if (!string.Equals(authScheme, SasTokenAuthScheme, StringComparison.OrdinalIgnoreCase))
-                {
-                    throw new InvalidOperationException($"Unsupported authentication scheme. Supported scheme is {SasTokenAuthScheme}.");
-                }
+            ISignatureProvider signatureProvider = new HttpHsmSignatureProvider(edgeWorkloadUri, DefaultApiVersion);
 
-                ISignatureProvider signatureProvider = new HttpHsmSignatureProvider(edgedUri, DefaultApiVersion);
-
-                TimeSpan sasTokenTimeToLive = _options?.SasTokenTimeToLive ?? default;
-                int sasTokenRenewalBuffer = _options?.SasTokenRenewalBuffer ?? default;
+            TimeSpan sasTokenTimeToLive = _options?.SasTokenTimeToLive ?? default;
+            int sasTokenRenewalBuffer = _options?.SasTokenRenewalBuffer ?? default;
 
 #pragma warning disable CA2000 // Dispose objects before losing scope - IDisposable ModuleAuthenticationWithHsm is disposed when the client is disposed.
-                // Since the sdk creates the instance of disposable ModuleAuthenticationWithHsm, the sdk needs to dispose it once the client is disposed.
-                var authMethod = new ModuleAuthenticationWithHsm(signatureProvider, deviceId, moduleId, generationId, sasTokenTimeToLive, sasTokenRenewalBuffer, disposeWithClient: true);
+            // Since the sdk creates the instance of disposable ModuleAuthenticationWithHsm, the sdk needs to dispose it once the client is disposed.
+            var authMethod = new ModuleAuthenticationWithHsm(signatureProvider, deviceId, moduleId, generationId, sasTokenTimeToLive, sasTokenRenewalBuffer, disposeWithClient: true);
 #pragma warning restore CA2000 // Dispose objects before losing scope - IDisposable ModuleAuthenticationWithHsm is disposed when the client is disposed.
 
-                Debug.WriteLine("EdgeModuleClientFactory setupTrustBundle from service");
+            Debug.WriteLine("EdgeModuleClientFactory setupTrustBundle from service");
 
-                ICertificateValidator certificateValidator = NullCertificateValidator.Instance;
-                if (!string.IsNullOrEmpty(gateway))
-                {
-                    IList<X509Certificate2> certs = await _trustBundleProvider.GetTrustBundleAsync(new Uri(edgedUri), DefaultApiVersion).ConfigureAwait(false);
-                    certificateValidator = GetCertificateValidator(certs);
-                }
-
-                return new ModuleClient(CreateInternalClientFromAuthenticationMethod(hostname, gateway, authMethod, _options), certificateValidator);
+            if (!string.IsNullOrEmpty(gateway))
+            {
+                IList<X509Certificate2> certs = await _trustBundleProvider.GetTrustBundleAsync(new Uri(edgeWorkloadUri), DefaultApiVersion).ConfigureAwait(false);
+                certificateValidator = GetCertificateValidator(certs);
             }
+
+            return new ModuleClient(CreateInternalClientFromAuthenticationMethod(hostname, gateway, authMethod, _options), certificateValidator);
         }
 
         private ICertificateValidator GetCertificateValidator(IList<X509Certificate2> certs)
