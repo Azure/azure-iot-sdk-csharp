0628b7f7b4e244ddbaa6ddd96b4facb990e17a43
Abhipsa Misra <abhipsa.misra@microsoft.com>
2023-04-07 11:04:09 -0700

    hashcode

--- iothub/device/src/Pipeline/ClientTransportStateMachine.cs
+++ iothub/device/src/Pipeline/ClientTransportStateMachine.cs
@@ -46,7 +46,20 @@ namespace Microsoft.Azure.Devices.Client.Transport
 
             public override int GetHashCode()
             {
-                return _currentState.GetHashCode() + _nextAction.GetHashCode();
+                return ShiftAndWrap(_currentState.GetHashCode(), 2) ^ _nextAction.GetHashCode();
+            }
+
+            // Hashcode evalution example: https://learn.microsoft.com/dotnet/api/system.object.gethashcode?view=net-8.0#examples
+            private static int ShiftAndWrap(int value, int positions)
+            {
+                positions &= 0x1F;
+
+                // Save the existing bit pattern, but interpret it as an unsigned integer.
+                uint number = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+                // Preserve the bits to be discarded.
+                uint wrapped = number >> (32 - positions);
+                // Shift and wrap the discarded bits.
+                return BitConverter.ToInt32(BitConverter.GetBytes((number << positions) | wrapped), 0);
             }
         }
 
