d6ea8eebee9293fcf431a4c35b4cb78031ae88cc
Abhipsa Misra <abhipsa.misra@microsoft.com>
2023-05-01 14:13:48 -0700

    minor

--- iothub/device/src/Transport/Http/HttpClientHelper.cs
+++ iothub/device/src/Transport/Http/HttpClientHelper.cs
@@ -158,7 +158,7 @@ namespace Microsoft.Azure.Devices.Client.Transport
                 if (entity != null)
                 {
                     // Set the complete entity object into the HttpRequestMessage content. This includes the user-defined payload (if applicable)
-                    // and all associated metadata. The content is set to as per service-defined contract, i.e. UTF-8 encoded json string.
+                    // and all associated metadata. The content is set as per service-defined contract, i.e. UTF-8 encoded json string.
                     msg.Content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
                 }
 
