581388a7e86e49a21e560087c49e9f0837130f57
David R. Williamson <drwill@microsoft.com>
2023-05-01 10:35:26 -0700

    E2E test for close or dispose multiple times

--- e2e/Tests/iothub/device/IotHubDeviceClientTests.cs
+++ e2e/Tests/iothub/device/IotHubDeviceClientTests.cs
@@ -37,5 +37,55 @@ namespace Microsoft.Azure.Devices.E2ETests
             // assert
             await act.Should().NotThrowAsync().ConfigureAwait(false);
         }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task IotHubDeviceClient_CloseAsync_CanBeCalledTwice(bool useMqtt)
+        {
+            // arrange
+
+            await using TestDevice testDevice = await TestDevice.GetTestDeviceAsync(nameof(IotHubDeviceClient_CloseAsync_CanBeCalledTwice));
+
+            IotHubClientTransportSettings transport = useMqtt
+                ? new IotHubClientMqttSettings()
+                : new IotHubClientAmqpSettings();
+
+            IotHubDeviceClient client = testDevice.CreateDeviceClient(new IotHubClientOptions(transport));
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            await testDevice.OpenWithRetryAsync(cts.Token).ConfigureAwait(false);
+            await client.CloseAsync().ConfigureAwait(false);
+
+            // act
+            Func<Task> act = () => client.CloseAsync();
+
+            // assert
+            await act.Should().NotThrowAsync().ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task IotHubDeviceClient_DisposeAsync_CanBeCalledTwice(bool useMqtt)
+        {
+            // arrange
+
+            await using TestDevice testDevice = await TestDevice.GetTestDeviceAsync(nameof(IotHubDeviceClient_DisposeAsync_CanBeCalledTwice));
+
+            IotHubClientTransportSettings transport = useMqtt
+                ? new IotHubClientMqttSettings()
+                : new IotHubClientAmqpSettings();
+
+            IotHubDeviceClient client = testDevice.CreateDeviceClient(new IotHubClientOptions(transport));
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            await testDevice.OpenWithRetryAsync(cts.Token).ConfigureAwait(false);
+            await client.DisposeAsync().ConfigureAwait(false);
+
+            // act
+            Func<Task> act = () => client.DisposeAsync().AsTask();
+
+            // assert
+            await act.Should().NotThrowAsync().ConfigureAwait(false);
+        }
     }
 }
