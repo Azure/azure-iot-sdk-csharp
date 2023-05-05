d1b1385fa3b94be8f09d487f55c37728694e4f97
Tim Taylor <timtay@microsoft.com>
2023-05-04 17:38:16 -0700

    Fix .NET 472 specific E2E test bug (#3328)
    MQTTNET does not allows older .NET frameworks to set the certificate validation handler when connecting over websocket, and that caused our MQTT_WS provisioning tests to fail consistently on .NET 472
    
    Now we deliberately opt-out of setting this handler in the above situation

--- e2e/Tests/provisioning/ProvisioningE2ETests.cs
+++ e2e/Tests/provisioning/ProvisioningE2ETests.cs
@@ -901,7 +901,14 @@ namespace Microsoft.Azure.Devices.E2ETests.Provisioning
 
                 IotHubClientMqttSettings => transportSettings.Protocol == IotHubClientTransportProtocol.Tcp
                     ? new ProvisioningClientOptions(new ProvisioningClientMqttSettings(ProvisioningClientTransportProtocol.Tcp))
-                    : new ProvisioningClientOptions(new ProvisioningClientMqttSettings(ProvisioningClientTransportProtocol.WebSocket)),
+                    : new ProvisioningClientOptions(new ProvisioningClientMqttSettings(ProvisioningClientTransportProtocol.WebSocket)
+                    {
+#if NET472
+                        // MQTTNET doesn't support setting this callback on older .NET framework versions
+                        // https://github.com/dotnet/MQTTnet/blob/99f4f46062611cd08e18b82515962b69e8c619e4/Source/MQTTnet/Implementations/MqttWebSocketChannel.cs#L222
+                        RemoteCertificateValidationCallback = null,
+#endif
+                    }),
 
                 _ => throw new NotSupportedException($"Unknown transport: '{transportSettings}'.")
             };
