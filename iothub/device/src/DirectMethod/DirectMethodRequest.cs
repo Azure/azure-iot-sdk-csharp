9451955f56cf9acf3340d5ba70f0ab4b9850cc1a
Nick Patilsen <npatilsen@microsoft.com>
2023-07-19 14:49:24 -0700

    misc

--- iothub/device/src/DirectMethod/DirectMethodRequest.cs
+++ iothub/device/src/DirectMethod/DirectMethodRequest.cs
@@ -105,9 +105,8 @@ namespace Microsoft.Azure.Devices.Client
                 }
 
                 // If not deserializing into byte[], an extra layer of decoding is needed
-                var bytes = PayloadConvention.GetObject<byte[]>(_payload);
-                var test3 = Encoding.UTF8.GetString(bytes);
-                payload = JsonConvert.DeserializeObject<T>(test3);
+
+                payload = JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(PayloadConvention.GetObject<byte[]>(_payload)));
 
                 return true;
             }
@@ -145,6 +144,11 @@ namespace Microsoft.Azure.Devices.Client
         public byte[] GetPayload()
         {
             return TryGetPayload(out byte[] payload) ? payload : (byte[])_payload.Clone();
+        }
+
+        internal static string MultipleDecode(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(JsonConvert.DeserializeObject<byte[]>(Encoding.UTF8.GetString(bytes)));
         }
     }
 }
