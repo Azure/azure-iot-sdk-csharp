cb3aea537cc28fddafb7904463458492337f611c
Nick Patilsen <npatilsen@microsoft.com>
2023-07-24 14:36:51 -0700

    added test file

--- iothub/device/src/Authentication/ClientAuthenticationForEdgeHubOnBehalfOf.cs
+++ iothub/device/src/Authentication/ClientAuthenticationForEdgeHubOnBehalfOf.cs
@@ -15,9 +15,9 @@ namespace Microsoft.Azure.Devices.Client.Authentication
         /// <summary>
         /// Creates an instance of this class.
         /// </summary>
-        /// <param name="sharedAccessKey">Shared access key value.</param>
-        /// <param name="parentDeviceId">Identifier of the parent device.</param>
-        /// <param name="deviceId">Device identifier.</param>
+        /// <param name="sharedAccessKey">Shared access key value for the higher-layer parent device.</param>
+        /// <param name="parentDeviceId">Identifier of the higher-layer parent device that connects directly to IoT Hub.</param>
+        /// <param name="deviceId">Device identifier of the lower-layer device that authenticates through the parent IoT device.</param>
         /// <param name="moduleId">Module identifier.</param>
         /// <param name="sasTokenTimeToLive">
         /// The suggested time to live value for the generated SAS tokens.
---
+++ iothub/device/tests/Authentication/EdgeDeviceOnBehalfOfTests.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace Microsoft.Azure.Devices.Client.Tests.OnBehalfOf
+{
+    public class EdgeDeviceOnBehalfOfTests
+    {
+        private static string _testKey => Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.Empty.ToString("N")));
+
+        [TestMethod]
+        public async Task ConnectDeviceOnBehalfOf()
+        {
+            string leafDeviceId = "test-leaf-device";
+
+            DeviceConnectionSettings edgeDevice = new DeviceConnectionSettings("e4k-hub.azure-devices.net", "test-edge-device")
+            {
+                SharedAccessKey = _testKey
+            };
+
+            var edgeHubCs = await EdgeHubConnectionSettingsBuilder.Build(edgeDevice);
+
+            IAuthenticationMethod leafAuth = new ClientAuthenticationWithSharedAccessKeyRefreshBehalfOf(
+                edgeHubCs.SharedAccessKey!,
+                edgeHubCs.DeviceId!,
+                leafDeviceId,
+                null,
+                TimeSpan.FromMinutes(10),
+                5);
+
+            IotHubDeviceClient leafClient = new(edgeHubCs.HostName, leafAuth,
+                new IotHubClientOptions(
+                    new IotHubClientAmqpSettings
+                    {
+                        ConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                        {
+                            UsePooling = true,
+                            MaxPoolSize = 10
+                        }
+                    }));
+
+            await leafClient.OpenAsync();
+            long tick = Environment.TickCount;
+            await leafClient.UpdateReportedPropertiesAsync(new ReportedProperties { ["tick"] = tick });
+            var twin = await leafClient.GetTwinPropertiesAsync();
+            Assert.NotNull(twin);
+            Assert.NotNull(twin.Reported);
+            Assert.NotNull(twin.Reported["tick"]);
+            Assert.Equal(tick, twin.Reported["tick"]);
+            await leafClient.CloseAsync();
+        }
+
+        [TestMethod]
+        public async Task ConnectLeafModuleOnBehalfOf()
+        {
+            string leafDeviceId = "test-leaf-device";
+            string edgeModuleId = "test-leaf-device-module";
+
+            DeviceConnectionSettings edgeDevice = new DeviceConnectionSettings("e4k-hub.azure-devices.net", "test-edge-device")
+            {
+                SharedAccessKey = _testKey
+            };
+
+            var edgeHubCs = await EdgeHubConnectionSettingsBuilder.Build(edgeDevice);
+
+            IAuthenticationMethod leafAuth = new ClientAuthenticationWithSharedAccessKeyRefreshBehalfOf(
+                edgeHubCs.SharedAccessKey!,
+                edgeHubCs.DeviceId!,
+                leafDeviceId,
+                edgeModuleId,
+                TimeSpan.FromMinutes(10),
+                5);
+
+            IotHubModuleClient leafClient = new(edgeHubCs.HostName, leafAuth,
+                new IotHubClientOptions(
+                    new IotHubClientAmqpSettings
+                    {
+                        ConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                        {
+                            UsePooling = true,
+                            MaxPoolSize = 10
+                        }
+                    }));
+
+            await leafClient.OpenAsync();
+            long tick = Environment.TickCount;
+            await leafClient.UpdateReportedPropertiesAsync(new ReportedProperties { ["tick"] = tick });
+            var twin = await leafClient.GetTwinPropertiesAsync();
+            Assert.NotNull(twin);
+            Assert.NotNull(twin.Reported);
+            Assert.NotNull(twin.Reported["tick"]);
+            Assert.Equal(tick, twin.Reported["tick"]);
+            await leafClient.CloseAsync();
+        }
+
+        [TestMethod]
+        public async Task ConnectEdgeModuleOnBehalfOf()
+        {
+            string edgeDeviceId = "test-edge-device";
+            string edgeModuleId = "test-edge-module";
+
+            DeviceConnectionSettings edgeDevice = new DeviceConnectionSettings("e4k-hub.azure-devices.net", edgeDeviceId)
+            {
+                SharedAccessKey = _testKey
+            };
+
+            var edgeHubCs = await EdgeHubConnectionSettingsBuilder.Build(edgeDevice);
+
+            IAuthenticationMethod leafAuth = new ClientAuthenticationWithSharedAccessKeyRefreshBehalfOf(
+                edgeHubCs.SharedAccessKey!,
+                edgeHubCs.DeviceId!,
+                edgeDeviceId,
+                edgeModuleId,
+                TimeSpan.FromMinutes(10),
+                5);
+
+            IotHubModuleClient leafClient = new(edgeHubCs.HostName, leafAuth,
+                new IotHubClientOptions(
+                    new IotHubClientAmqpSettings
+                    {
+                        ConnectionPoolSettings = new AmqpConnectionPoolSettings()
+                        {
+                            UsePooling = true,
+                            MaxPoolSize = 10
+                        }
+                    }));
+
+            await leafClient.OpenAsync();
+            long tick = Environment.TickCount;
+            await leafClient.UpdateReportedPropertiesAsync(new ReportedProperties { ["tick"] = tick });
+            var twin = await leafClient.GetTwinPropertiesAsync();
+            Assert.NotNull(twin);
+            Assert.NotNull(twin.Reported);
+            Assert.NotNull(twin.Reported["tick"]);
+            Assert.Equal(tick, twin.Reported["tick"]);
+            await leafClient.CloseAsync();
+        }
+    }
+}
