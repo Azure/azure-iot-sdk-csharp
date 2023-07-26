e16fb442072e9363d434cf39007cb5b9e0e8cdaf
Nick Patilsen <npatilsen@microsoft.com>
2023-07-26 13:09:09 -0700

    fixed doc comment

--- provisioning/device/src/RetryPolicies/IProvisioningClientRetryPolicy.cs
+++ provisioning/device/src/RetryPolicies/IProvisioningClientRetryPolicy.cs
@@ -14,7 +14,8 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         /// Method called by the client when an operation fails to determine if a retry should be attempted,
         /// and how long to wait until retrying the operation.
         /// </summary>
-        /// <param name="currentRetryCount">The number of times the current operation has been attempted.</param>
+        /// <param name="
+        /// currentRetryCount">The number of times the current operation has been attempted.</param>
         /// <param name="lastException">The exception that prompted this retry policy check.</param>
         /// <param name="retryDelay">Set this to the desired time to delay before the next attempt.</param>
         /// <returns>True if the operation should be retried; otherwise false.</returns>
