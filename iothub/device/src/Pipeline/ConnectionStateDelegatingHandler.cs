dc9ae3a6916f1c991e53765d24c5c157890f0d3c
Nick Patilsen <110431552+patilsnr@users.noreply.github.com>
2023-07-28 10:55:54 -0700

    Make currentRetryCount consistent across IotHub and DPS (#3355)
    * fixed doc comment
    
    * change default should retry in csdh to 1

--- iothub/device/src/Pipeline/ConnectionStateDelegatingHandler.cs
+++ iothub/device/src/Pipeline/ConnectionStateDelegatingHandler.cs
@@ -580,7 +580,7 @@ namespace Microsoft.Azure.Devices.Client.Transport
                 // This is used to ensure that when IotHubServiceNoRetry() policy is enabled, we should not be retrying.
                 // This exception is not returned to the user.
                 var networkException = new IotHubClientException("", IotHubClientErrorCode.NetworkErrors);
-                if (!_retryPolicy.ShouldRetry(0, networkException, out TimeSpan delay))
+                if (!_retryPolicy.ShouldRetry(1, networkException, out TimeSpan delay))
                 {
                     if (Logging.IsEnabled)
                         Logging.Info(this, "Transport disconnected: closed by application.", nameof(HandleDisconnectAsync));
