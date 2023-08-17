9b65775b1442fafff8917b5eb49ba0917aa1fb0f
Tim Stewart <timstewart@microsoft.com>
2023-08-17 13:05:53 -0700

    create sample project for discovery code

---
+++ discovery/device/samples/how to guides/TpmSample/DiscoveryDeviceClientSample.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.Devices.Discovery.Client;
+using Microsoft.Azure.Devices.Discovery.Client.Transport;
+using Microsoft.Azure.Devices.Provisioning.Security;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
+{
+    /// <summary>
+    /// Demonstrates how to register a device with the device provisioning service using a certificate, and then
+    /// use the registration information to authenticate to IoT Hub.
+    /// </summary>
+    internal class DiscoveryDeviceClientSample
+    {
+        private readonly Parameters _parameters;
+
+        public DiscoveryDeviceClientSample(Parameters parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public async Task RunSampleAsync()
+        {
+            Console.WriteLine("Initializing security using the local TPM...");
+            using SecurityProviderTpm security = new SecurityProviderTpmHsm(_parameters.RegistrationId);
+
+            Console.WriteLine("Initializing transport");
+            using DiscoveryTransportHandler transport = new DiscoveryTransportHandlerHttp();
+
+            var client = DiscoveryDeviceClient.Create(
+                _parameters.GlobalDeviceEndpoint,
+                security,
+                transport);
+
+            Console.WriteLine($"Initialized for registration Id {security.GetRegistrationID()}.");
+
+            Console.WriteLine("Getting nonce for challenge... ");
+            string nonce = await client.IssueChallengeAsync();
+
+            Console.WriteLine($"Received nonce");
+
+            string cert = await client.GetOnboardingInfoAsync(nonce);
+
+            Console.WriteLine($"Received cert: {cert}");
+        }
+    }
+}
---
+++ discovery/device/samples/how to guides/TpmSample/Parameters.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
+
+namespace Microsoft.Azure.Devices.Provisioning.Client.Samples
+{
+    /// <summary>
+    /// Parameters for the application
+    /// </summary>
+    internal class Parameters
+    {
+        [Option(
+            'e',
+            "GetTpmEndorsementKey",
+            HelpText = "Gets the TPM endorsement key. Use this option by itself to get the EK needed to create a DPS individual enrollment.")]
+        public bool GetTpmEndorsementKey { get; set; }
+
+        [Option(
+            'r',
+            "RegistrationId",
+            HelpText = "The registration Id from the individual enrollment. For normal runs, this is required.")]
+        public string RegistrationId { get; set; }
+
+        [Option(
+            'g',
+            "GlobalDeviceEndpoint",
+            Default = "dev1.eastus.device.discovery.edgeprov-dev.azure.net",
+            HelpText = "The global endpoint for devices to connect to.")]
+        public string GlobalDeviceEndpoint { get; set; }
+    }
+}
