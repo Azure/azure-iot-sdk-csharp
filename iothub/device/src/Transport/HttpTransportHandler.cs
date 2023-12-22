64eb3b64c34a8681da5e9cea4e7adc602650838f
tmahmood-microsoft <tmahmood@microsoft.com>
2023-12-22 11:33:17 -0800

    build fix

--- iothub/device/src/Transport/HttpTransportHandler.cs
+++ iothub/device/src/Transport/HttpTransportHandler.cs
@@ -16,13 +16,8 @@ using Microsoft.Azure.Devices.Client.Exceptions;
 using Microsoft.Azure.Devices.Client.Extensions;
 using Microsoft.Azure.Devices.Shared;
 using Newtonsoft.Json;
-
-#if !NET451
-
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
-
-#endif
 
 namespace Microsoft.Azure.Devices.Client.Transport
 {
@@ -191,17 +186,6 @@ namespace Microsoft.Azure.Devices.Client.Transport
             try
             {
                 // 2. Use SAS URI to send data to Azure Storage Blob (PUT)
-#if NET451
-                var blob = new WindowsAzure.Storage.Blob.CloudBlockBlob(new Uri(putString));
-                Task uploadTask = blob.UploadFromStreamAsync(source, null, null, null, cancellationToken);
-                await uploadTask.ConfigureAwait(false);
-
-                notification.CorrelationId = fileUploadResponse.CorrelationId;
-                notification.IsSuccess = uploadTask.IsCompleted;
-                notification.StatusCode = uploadTask.IsCompleted ? 0 : -1;
-                notification.StatusDescription = uploadTask.IsCompleted ? null : "Failed to upload to storage.";
-#endif
-#if !NET451
                 var blockBlobClient = new BlockBlobClient(new Uri(putString));
                 await blockBlobClient.UploadAsync(source, new BlobUploadOptions(), cancellationToken).ConfigureAwait(false);
 
@@ -209,7 +193,6 @@ namespace Microsoft.Azure.Devices.Client.Transport
                 notification.IsSuccess = true;
                 notification.StatusCode = 0;
                 notification.StatusDescription = null;
-#endif
 
                 // 3. POST to IoTHub with upload status
                 await _httpClientHelper.PostAsync(
