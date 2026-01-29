91c5e05932ba32ff79fb1c2f7cff7518558e1b80
Tim Taylor <timtay@microsoft.com>
2026-01-29 11:27:49 -0800

    more

--- e2e/test/provisioning/ProvisioningE2ETests.cs
+++ e2e/test/provisioning/ProvisioningE2ETests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
