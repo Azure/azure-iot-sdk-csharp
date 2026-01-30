fa3c207143727de3690300550b8a05f4aabf1cab
Tim Taylor <timtay@microsoft.com>
2026-01-29 16:33:31 -0800

    ctor

--- e2e/test/prerequisites/E2ETestsSetup/e2eTestsSetup.ps1
+++ e2e/test/prerequisites/E2ETestsSetup/e2eTestsSetup.ps1
@@ -146,7 +146,7 @@ Write-Host "`nGenerating self signed certs."
 # This chain will be combined with the certificates that are signed by Intermediate2 to test X509 CA-chained devices for IoT Hub and DPS (group enrollment) tests.
 # Chain: Root->Intermediate1->Intermediate2, device cert: Intermediate2->deviceCert
 $rootCACert = New-SelfSignedCertificateEx `
-    -Subject "CN=$rootCommonName" `
+    -Subject New-Object -TypeName System.Security.Cryptography.X509Certificates.X500DistinguishedName -ArgumentList "CN=$rootCommonName" `
     -KeyUsage 4 ` # KeyCertSign
     #-TextExtension @("2.5.29.19={text}ca=TRUE&pathlength=12") `
     -SignatureAlgorithm "$certificateHashAlgorithm" `
@@ -154,7 +154,7 @@ $rootCACert = New-SelfSignedCertificateEx `
     -NotAfter (Get-Date).AddYears(2)
 
 $intermediateCert1 = New-SelfSignedCertificateEx `
-    -Subject "CN=$intermediateCert1CommonName" `
+    -Subject New-Object -TypeName System.Security.Cryptography.X509Certificates.X500DistinguishedName -ArgumentList "CN=$intermediateCert1CommonName" `
     -KeyUsage 4 ` # KeyCertSign
     #-TextExtension @("2.5.29.19={text}ca=TRUE&pathlength=12") `
     -SignatureAlgorithm "$certificateHashAlgorithm" `
@@ -163,7 +163,7 @@ $intermediateCert1 = New-SelfSignedCertificateEx `
     -Issuer $rootCACert
 
 $intermediateCert2 = New-SelfSignedCertificateEx `
-    -Subject "CN=$intermediateCert2CommonName" `
+    -Subject New-Object -TypeName System.Security.Cryptography.X509Certificates.X500DistinguishedName -ArgumentList "CN=$intermediateCert2CommonName" `
     -KeyUsage 4 ` # KeyCertSign
     #-TextExtension @("2.5.29.19={text}ca=TRUE&pathlength=12") `
     -SignatureAlgorithm "$certificateHashAlgorithm" `
@@ -191,7 +191,7 @@ $x509ChainIntermediate2PfxBase64 = [Convert]::ToBase64String((Get-Content $inter
 # Generate an X509 self-signed certificate. This certificate will be used by test device identities that test X509 self-signed certificate device authentication.
 # Leaf certificates are not used for signing so don't specify KeyUsage and TestExtension - ca=TRUE&pathlength=12
 $iotHubX509SelfSignedDeviceCert = New-SelfSignedCertificateEx `
-    -Subject "CN=$iotHubX509DeviceCertCommonName" `
+    -Subject New-Object -TypeName System.Security.Cryptography.X509Certificates.X500DistinguishedName -ArgumentList "CN=$iotHubX509DeviceCertCommonName" `
     -KeySpec Signature `
     #-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") `
     -SignatureAlgorithm "$certificateHashAlgorithm" `
@@ -206,7 +206,7 @@ $iothubX509DevicePfxThumbprint = $iotHubX509SelfSignedDeviceCert.Thumbprint
 # Generate the leaf device certificate signed by Intermediate2. This certificate will be used by test device identities that test X509 CA-signed certificate device authentication.
 # Leaf certificates are not used for signing so don't specify KeyUsage and TestExtension - ca=TRUE&pathlength=12
 $iotHubX509ChainDeviceCert = New-SelfSignedCertificateEx `
-    -Subject "CN=$iotHubX509CertChainDeviceCommonName" `
+    -Subject New-Object -TypeName System.Security.Cryptography.X509Certificates.X500DistinguishedName -ArgumentList "CN=$iotHubX509CertChainDeviceCommonName" `
     -KeySpec Signature `
     #-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2") `
     -SignatureAlgorithm "$certificateHashAlgorithm" `
