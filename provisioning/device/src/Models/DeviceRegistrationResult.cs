8a60af121d6e6469ea998964948f18c09854dcf5
Tim Taylor <timtay@microsoft.com>
2026-03-10 14:00:24 -0700

    barely/not used azure.core dependency

--- provisioning/device/src/Models/DeviceRegistrationResult.cs
+++ provisioning/device/src/Models/DeviceRegistrationResult.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Azure;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -77,9 +76,7 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         /// The entity tag associated with the resource.
         /// </summary>
         [JsonProperty("etag")]
-        // NewtonsoftJsonETagConverter is used here because otherwise the ETag isn't serialized properly.
-        [JsonConverter(typeof(NewtonsoftJsonETagConverter))]
-        public ETag ETag { get; protected internal set; }
+        public string ETag { get; protected internal set; }
 
         /// <summary>
         /// The custom data returned from the webhook to the device.
