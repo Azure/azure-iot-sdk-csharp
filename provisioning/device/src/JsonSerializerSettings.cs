19c8b228d4f43eaded28a36c11af0b8354e8c331
Tim Taylor <timtay@microsoft.com>
2026-03-24 11:55:06 -0700

    settings

--- provisioning/device/src/JsonSerializerSettings.cs
+++ provisioning/device/src/JsonSerializerSettings.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using Azure;
 
 namespace Microsoft.Azure.Devices.Provisioning.Client
 {
@@ -17,7 +20,37 @@ namespace Microsoft.Azure.Devices.Provisioning.Client
         public static JsonSerializerOptions Options { get; set; } = new()
         {
             DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+            AllowTrailingCommas = true,
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+            TypeInfoResolver = new DefaultJsonTypeInfoResolver
+            {
+                Modifiers =
+                {
+                    IgnoreEmptyCollectionsAndUnsetEtags,
+                }
+            }
         };
+
+        private static void IgnoreEmptyCollectionsAndUnsetEtags(JsonTypeInfo typeInfo)
+        {
+            if (typeInfo.Kind != JsonTypeInfoKind.Object) return;
+
+            foreach (var prop in typeInfo.Properties)
+            {
+                if (typeof(IEnumerable).IsAssignableFrom(prop.PropertyType) && prop.PropertyType != typeof(string))
+                {
+                    // Only serialize a collection if it has at least one element
+                    prop.ShouldSerialize = (_, value) =>
+                    {
+                        if (value == null)
+                        {
+                            return false;
+                        }
+
+                        return value is ICollection c ? c.Count > 0 : ((IEnumerable)value).GetEnumerator().MoveNext();
+                    };
+                }
+            }
+        }
     }
 }
