24ef6a6a71dd18f3dfda83c61f97bfa904764bda
Tim Taylor <timtay@microsoft.com>
2026-03-23 20:26:29 -0700

    initial twin

--- provisioning/service/src/Twin/InitialTwin.cs
+++ provisioning/service/src/Twin/InitialTwin.cs
@@ -19,46 +19,23 @@ namespace Microsoft.Azure.Devices.Provisioning.Service
     /// </remarks>
     public class InitialTwin
     {
-        [JsonIgnore]
-        private InitialTwinProperties _properties;
-
         /// <summary>
         /// Creates an instance of this class.
         /// </summary>
         public InitialTwin()
         {
-            _properties ??= new InitialTwinProperties();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="tags"></param>
-        /// <param name="properties"></param>
-        [JsonConstructor]
-        public InitialTwin(IDictionary<string, object> tags, InitialTwinProperties properties)
-        {
-            Tags = tags;
-            DesiredProperties = properties?.Desired;
         }
 
         /// <summary>
         /// Getter and setter the for tags.
         /// </summary>
         [JsonPropertyName("tags")]
-        public IDictionary<string, object> Tags { get; set; } = new Dictionary<string, object>();
+        public JsonDictionary Tags { get; set; } = new();
 
         /// <summary>
         /// Getter and setter the desired properties.
         /// </summary>
         [JsonPropertyName("properties")]
-        public InitialTwinPropertyCollection DesiredProperties
-        {
-            get => _properties?.Desired;
-
-            set => _properties = value == null
-                ? null
-                : new InitialTwinProperties { Desired = value };
-        }
+        public InitialTwinPropertyCollection DesiredProperties { get; set; }
     }
 }
